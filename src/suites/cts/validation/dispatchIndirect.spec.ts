9115639d62a9751c0421c450015d011a8794bd36
François Beaufort <beaufort.francois@gmail.com>
2019-09-16 11:15:59 +0200

    Address @kainino0x feedback

--- src/suites/cts/validation/dispatchIndirect.spec.ts
+++ src/suites/cts/validation/dispatchIndirect.spec.ts
@@ -3,61 +3,55 @@ dispatchIndirect validation tests.
 `;
 
 import { TestGroup } from '../../../framework/index.js';
-
-import { ValidationTest } from './validation_test.js';
 import GLSL from '../../../tools/glsl.macro.js';
 
-export class F extends ValidationTest {
-  async testIndirectOffset(data: number[], offset: number, success: boolean): Promise<void> {
-    const module = this.device.createShaderModule({
-      code: GLSL(
-        'compute',
-        `#version 450
-          void main() {
-          }
-        `
-      ),
-    });
+import { ValidationTest } from './validation_test.js';
 
-    const pipelineLayout = this.device.createPipelineLayout({
-      bindGroupLayouts: [],
-    });
-
-    const pipeline = this.device.createComputePipeline({
-      computeStage: { module, entryPoint: 'main' },
-      layout: pipelineLayout,
-    });
-
-    const [indirectBuffer, arrayBuffer] = await this.device.createBufferMappedAsync({
-      size: data.length * Uint32Array.BYTES_PER_ELEMENT,
-      usage: GPUBufferUsage.INDIRECT,
-    });
-    new Uint32Array(arrayBuffer).set(data);
-
-    const commandEncoder = this.device.createCommandEncoder();
-    const computePass = commandEncoder.beginComputePass();
-    computePass.setPipeline(pipeline);
-    computePass.dispatchIndirect(indirectBuffer, offset * Uint32Array.BYTES_PER_ELEMENT);
-    computePass.endPass();
-
-    if (success) {
-      // Control case
-      commandEncoder.finish();
-    } else {
-      // Out of bounds indirect calls are caught
-      await this.expectValidationError(() => {
-        commandEncoder.finish();
-      });
-    }
-  }
-}
-
-export const g = new TestGroup(F);
+export const g = new TestGroup(ValidationTest);
 
 g.test('out of bounds indirect dispatch calls are caught early', async t => {
   const { data, offset, success } = t.params;
 
-  await t.testIndirectOffset(data, offset, success);
+  const module = t.device.createShaderModule({
+    code: GLSL(
+      'compute',
+      `#version 450
+        void main() {
+        }
+      `
+    ),
+  });
+
+  const pipelineLayout = t.device.createPipelineLayout({
+    bindGroupLayouts: [],
+  });
+
+  const pipeline = t.device.createComputePipeline({
+    computeStage: { module, entryPoint: 'main' },
+    layout: pipelineLayout,
+  });
+
+  const [indirectBuffer, arrayBuffer] = await t.device.createBufferMappedAsync({
+    size: data.length * Uint32Array.BYTES_PER_ELEMENT,
+    usage: GPUBufferUsage.INDIRECT,
+  });
+  new Uint32Array(arrayBuffer).set(data);
+
+  const commandEncoder = t.device.createCommandEncoder();
+  const computePass = commandEncoder.beginComputePass();
+  computePass.setPipeline(pipeline);
+  computePass.dispatchIndirect(indirectBuffer, offset * Uint32Array.BYTES_PER_ELEMENT);
+  computePass.endPass();
+
+  if (success) {
+    // Control case
+    commandEncoder.finish();
+  } else {
+    // Out of bounds indirect calls are caught
+    await t.expectValidationError(() => {
+      commandEncoder.finish();
+    });
+  }
 }).params([
   { data: [1, 2, 3], offset: 0, success: true }, // In bounds
   { data: [1, 2, 3, 4, 5, 6], offset: 0, success: true }, // In bounds, bigger buffer
@@ -65,5 +59,5 @@ g.test('out of bounds indirect dispatch calls are caught early', async t => {
   { data: [1, 2], offset: 0, success: false }, //Out of bounds, buffer too small
   { data: [1, 2, 3], offset: 1, success: false }, //Out of bounds, index too big
   { data: [1, 2, 3], offset: 4, success: false }, //Out of bounds, index past buffer
-  { data: [1, 2, 3, 4, 5, 6], offset: Number.MAX_SAFE_INTEGER, success: false }, // In bounds,index + size of command overflows
+  { data: [1, 2, 3, 4, 5, 6], offset: Number.MAX_SAFE_INTEGER, success: false }, // Out of bounds, offset is very large
 ]);
