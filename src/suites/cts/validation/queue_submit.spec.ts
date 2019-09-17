fb9b9e8f71c99dd1e59ce09bc1746134f4521880
François Beaufort <beaufort.francois@gmail.com>
2019-09-17 11:49:19 +0200

    Add function to create command buffer

--- src/suites/cts/validation/queue_submit.spec.ts
+++ src/suites/cts/validation/queue_submit.spec.ts
@@ -9,34 +9,34 @@ import { ValidationTest } from './validation_test.js';
 export const g = new TestGroup(ValidationTest);
 
 g.test('submitting with a mapped buffer is disallowed', async t => {
-  // Create a map-write buffer
   const buffer = t.device.createBuffer({
     size: 4,
     usage: GPUBufferUsage.MAP_WRITE | GPUBufferUsage.COPY_SRC,
   });
 
-  // Create a fake copy destination buffer
   const targetBuffer = t.device.createBuffer({
     size: 4,
     usage: GPUBufferUsage.COPY_DST,
   });
 
-  const commandEncoder = t.device.createCommandEncoder();
-  commandEncoder.copyBufferToBuffer(buffer, 0, targetBuffer, 0, 4);
+  const getCommandBuffer = (): GPUCommandBuffer => {
+    const commandEncoder = t.device.createCommandEncoder();
+    commandEncoder.copyBufferToBuffer(buffer, 0, targetBuffer, 0, 4);
+    return commandEncoder.finish();
+  };
 
   // Submitting when the buffer has never been mapped should succeed
-  const commandBuffer = commandEncoder.finish();
-  t.queue.submit([commandBuffer]);
+  t.queue.submit([getCommandBuffer()]);
 
   // Map the buffer, submitting when the buffer is mapped should fail
   await buffer.mapWriteAsync();
   t.queue.submit([]);
 
   await t.expectValidationError(() => {
-    t.queue.submit([commandBuffer]);
+    t.queue.submit([getCommandBuffer()]);
   });
 
   // Unmap the buffer, queue submit should succeed
   buffer.unmap();
-  t.queue.submit([commandBuffer]);
+  t.queue.submit([getCommandBuffer()]);
 });
