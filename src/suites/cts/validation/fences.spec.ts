74c82a1386bb42671ce6c4a1cf1cf28621685174
François Beaufort <beaufort.francois@gmail.com>
2019-09-17 11:35:40 +0200

    Split functional and validation tests

--- src/suites/cts/validation/fences.spec.ts
+++ src/suites/cts/validation/fences.spec.ts
@@ -8,50 +8,36 @@ import { ValidationTest } from './validation_test.js';
 
 export const g = new TestGroup(ValidationTest);
 
-g.test('fence creation succeeds', t => {
-  t.queue.createFence();
-});
+// TODO: Remove if https://github.com/gpuweb/gpuweb/issues/377 is decided
+g.test('wait on a fence without signaling the value is invalid', async t => {
+  const fence = t.queue.createFence();
+  const promise = fence.onCompletion(2);
 
-g.test('fence starts at initial value', t => {
-  const fence = t.queue.createFence({ initialValue: 1 });
-  t.expect(fence.getCompletedValue() === 1);
-});
-
-g.test('onCompletion resolves immediately for completed fence values', async t => {
-  const fence = t.queue.createFence({ initialValue: 1 });
-
-  await Promise.all([fence.onCompletion(0), fence.onCompletion(1)]);
+  await t.shouldReject('OperationError', promise);
 });
 
 // TODO: Remove if https://github.com/gpuweb/gpuweb/issues/377 is decided
-g.test('set onCompletion for values greater than signaled value', async t => {
-  const fence = t.queue.createFence({ initialValue: 1 });
+g.test('wait on a fence before it is signaled is invalid', async t => {
+  const fence = t.queue.createFence();
+  const promise = fence.onCompletion(2);
 
-  await t.shouldReject('OperationError', fence.onCompletion(2));
+  await t.expectValidationError(() => {
+    t.queue.signal(fence, 2);
+  });
 
+  await t.shouldReject('OperationError', promise);
+});
+
+// TODO: Remove if https://github.com/gpuweb/gpuweb/issues/377 is decided
+g.test('wait on a fence with a value greater than signaled value is invalid', async t => {
+  const fence = t.queue.createFence();
   t.queue.signal(fence, 2);
-  await fence.onCompletion(2);
+
+  const promise = fence.onCompletion(3);
+  await t.shouldReject('OperationError', promise);
 });
 
-g.test('get completed value inside callback', async t => {
-  const fence = t.queue.createFence({ initialValue: 1 });
-
-  t.queue.signal(fence, 3);
-  await fence.onCompletion(2);
-
-  t.expect(fence.getCompletedValue() === 3);
-});
-
-g.test('get completed value after callback', async t => {
-  const fence = t.queue.createFence({ initialValue: 1 });
-
-  t.queue.signal(fence, 2);
-  await fence.onCompletion(2);
-
-  t.expect(fence.getCompletedValue() === 2);
-});
-
-g.test('value lower than signaled value raises error', async t => {
+g.test('signal a value lower than signaled value is invalid', async t => {
   const fence = t.queue.createFence({ initialValue: 1 });
 
   await t.expectValidationError(() => {
@@ -59,7 +45,7 @@ g.test('value lower than signaled value raises error', async t => {
   });
 });
 
-g.test('value equal to signaled value raises error', async t => {
+g.test('signal a value equal to signaled value is invalid', async t => {
   const fence = t.queue.createFence({ initialValue: 1 });
 
   await t.expectValidationError(() => {
@@ -68,7 +54,7 @@ g.test('value equal to signaled value raises error', async t => {
 });
 
 g.test('increasing fence value by more than 1 succeeds', async t => {
-  const fence = t.queue.createFence({ initialValue: 1 });
+  const fence = t.queue.createFence();
 
   t.queue.signal(fence, 2);
   await fence.onCompletion(2);
@@ -77,8 +63,8 @@ g.test('increasing fence value by more than 1 succeeds', async t => {
   await fence.onCompletion(6);
 });
 
-g.test('signal a fence on a different queue than it was created on raises error', async t => {
-  const fence = t.queue.createFence({ initialValue: 1 });
+g.test('signal a fence on a different queue than it was created on is invalid', async t => {
+  const fence = t.queue.createFence();
 
   const anotherDevice = await t.device.adapter.requestDevice();
   const anotherQueue = anotherDevice.getQueue();
@@ -89,7 +75,7 @@ g.test('signal a fence on a different queue than it was created on raises error'
 });
 
 g.test('signal a fence on a wrong queue does not update fence signaled value', async t => {
-  const fence = t.queue.createFence({ initialValue: 1 });
+  const fence = t.queue.createFence();
 
   const anotherDevice = await t.device.adapter.requestDevice();
   const anotherQueue = anotherDevice.getQueue();
