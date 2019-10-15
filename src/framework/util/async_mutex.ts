6d14b74fcfd61893fc2b60135934092df77686dc
Kai Ninomiya <kainino@chromium.org>
2019-10-15 14:24:54 -0700

    renames

--- src/framework/util/async_mutex.ts
+++ src/framework/util/async_mutex.ts
@@ -1,5 +1,7 @@
 export class AsyncMutex {
-  private lastToRun: Promise<unknown> | undefined;
+  // The newest item currently waiting for the mutex. This promise is chained so
+  // that it implicitly defines a FIFO queue where this is the "last-in" item.
+  private newestQueueItem: Promise<unknown> | undefined;
 
   // Run an async function with a lock on this mutex.
   // Waits until the mutex is available, locks it, runs the function, then releases it.
@@ -7,14 +9,14 @@ export class AsyncMutex {
     const p = (async () => {
       // If the mutex is locked, wait for the last thing in the queue before running.
       // (Everything in the queue runs in order, so this is after everything currently enqueued.)
-      if (this.lastToRun) {
-        await this.lastToRun;
+      if (this.newestQueueItem) {
+        await this.newestQueueItem;
       }
       return fn();
     })();
 
-    // Push the newly-created Promise onto the queue.
-    this.lastToRun = p;
+    // Push the newly-created Promise onto the queue by replacing the old "newest" item.
+    this.newestQueueItem = p;
     // And return so the caller can wait on the result.
     return p;
   }
