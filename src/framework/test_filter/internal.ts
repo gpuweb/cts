8611baabff0d899cada1d2a99bd6ec508d69a5db
Kai Ninomiya <kainino@chromium.org>
2019-11-11 15:10:44 +0000

    Use a list of expectations to generate a minimal list of WPT variants
    See tools/gen_wpt_cts_html for documentation.

--- src/framework/test_filter/internal.ts
+++ src/framework/test_filter/internal.ts
@@ -1,3 +1,4 @@
+import { TestSpecOrTestOrCaseID } from '../id.js';
 import { TestFileLoader } from '../loader.js';
 
 import { TestFilterResult } from './index.js';
@@ -5,4 +6,9 @@ import { TestFilterResult } from './index.js';
 export interface TestFilter {
   // Iterates over the test cases matched by the filter.
   iterate(loader: TestFileLoader): Promise<TestFilterResult[]>;
+
+  // If the filter can accept one spec, one test, or one case, returns its ID.
+  idIfSingle(): TestSpecOrTestOrCaseID | undefined;
+
+  matches(id: TestSpecOrTestOrCaseID): boolean;
 }
