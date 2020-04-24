a80df0dc05ad5f25c62e3f843a37fbc19ed483d0
Austin Eng <2154796+austinEng@users.noreply.github.com>
2020-04-24 16:25:18 -0700

    Introduce expectSingleColor helper function (#176)
    This function checks that the entire subresource region
    of some texture is entirely filled by a single color.

--- src/webgpu/api/operation/resource_init/copied_texture_clear.spec.ts
+++ src/webgpu/api/operation/resource_init/copied_texture_clear.spec.ts
@@ -3,76 +3,26 @@ export const description = 'Test uninitialized textures are initialized to zero 
 import * as C from '../../../../common/constants.js';
 import { TestGroup } from '../../../../common/framework/test_group.js';
 import { assert, unreachable } from '../../../../common/framework/util/util.js';
-import {
-  fillTextureDataWithTexelValue,
-  getTextureCopyLayout,
-} from '../../../util/texture/layout.js';
 import { SubresourceRange } from '../../../util/texture/subresource.js';
-import { getTexelDataRepresentation } from '../../../util/texture/texelData.js';
 
 import { InitializedState, ReadMethod, TextureZeroInitTest } from './texture_zero_init_test.js';
 
 class CopiedTextureClearTest extends TextureZeroInitTest {
-  private checkTextureSliceByBufferCopy(
-    texture: GPUTexture,
-    state: InitializedState,
-    width: number,
-    height: number,
-    level: number,
-    layer: number
-  ): void {
-    const { byteLength, bytesPerRow, bytesPerBlock, rowsPerImage } = getTextureCopyLayout(
-      this.params.format,
-      this.params.dimension,
-      [width, height, 1]
-    );
-
-    const buffer = this.device.createBuffer({
-      size: byteLength,
-      usage: C.BufferUsage.CopySrc | C.BufferUsage.CopyDst,
-    });
-
-    const commandEncoder = this.device.createCommandEncoder();
-    commandEncoder.copyTextureToBuffer(
-      { texture, mipLevel: level, arrayLayer: layer },
-      {
-        buffer,
-        bytesPerRow,
-        rowsPerImage,
-      },
-      [width, height, 1]
-    );
-    this.queue.submit([commandEncoder.finish()]);
-
-    const expectedTexelData = new Uint8Array(
-      getTexelDataRepresentation(this.params.format).getBytes(this.stateToTexelComponents[state])
-    );
-    assert(expectedTexelData.byteLength === bytesPerBlock);
-
-    const arrayBuffer = new ArrayBuffer(byteLength);
-    fillTextureDataWithTexelValue(
-      expectedTexelData,
-      this.params.format,
-      this.params.dimension,
-      arrayBuffer,
-      [width, height, 1]
-    );
-
-    this.expectContents(buffer, new Uint8Array(arrayBuffer));
-  }
-
   private checkContentsByBufferCopy(
     texture: GPUTexture,
     state: InitializedState,
     subresourceRange: SubresourceRange
   ): void {
-    for (const { level, slice } of subresourceRange.each()) {
+    for (const { level: mipLevel, slice } of subresourceRange.each()) {
       assert(this.params.dimension === '2d');
 
-      const width = this.textureWidth >> level;
-      const height = this.textureHeight >> level;
-
-      this.checkTextureSliceByBufferCopy(texture, state, width, height, level, slice);
+      this.expectSingleColor(texture, this.params.format, {
+        size: [this.textureWidth, this.textureHeight, 1],
+        dimension: this.params.dimension,
+        slice,
+        layout: { mipLevel },
+        exp: this.stateToTexelComponents[state],
+      });
     }
   }
 
@@ -101,7 +51,10 @@ class CopiedTextureClearTest extends TextureZeroInitTest {
       );
       this.queue.submit([commandEncoder.finish()]);
 
-      this.checkTextureSliceByBufferCopy(dst, state, width, height, 0, 0);
+      this.expectSingleColor(dst, this.params.format, {
+        size: [width, height, 1],
+        exp: this.stateToTexelComponents[state],
+      });
     }
   }
 
