ca4f613e2e7f718cb6e95f952218121eea8cc59e
Kai Ninomiya <kainino@chromium.org>
2020-04-29 15:10:51 -0700

    Handle device loss and popErrorScope hangs in test finalization

--- src/webgpu/gpu_test.ts
+++ src/webgpu/gpu_test.ts
@@ -1,6 +1,12 @@
 import { Fixture } from '../common/framework/fixture.js';
 import { getGPU } from '../common/framework/gpu/implementation.js';
-import { assert, unreachable } from '../common/framework/util/util.js';
+import {
+  assert,
+  unreachable,
+  raceWithRejectOnTimeout,
+  assertReject,
+  PromiseTimeoutError,
+} from '../common/framework/util/util.js';
 
 import {
   fillTextureDataWithTexelValue,
@@ -29,15 +35,35 @@ type TypedArrayBufferViewConstructor =
   | Float32ArrayConstructor
   | Float64ArrayConstructor;
 
+interface DeviceHolder {
+  device: GPUDevice;
+  lostReason?: string;
+}
+
+const kPopErrorScopeTimeoutMS = 5000;
+
 class DevicePool {
-  device: GPUDevice | undefined = undefined;
-  state: 'free' | 'acquired' | 'uninitialized' | 'failed' = 'uninitialized';
+  holder?: DeviceHolder = undefined; // undefined means "uninitialized"
+  state: 'free' | 'acquired' | 'failed' = 'free';
 
   private async initialize(): Promise<void> {
+    if (this.holder !== undefined) {
+      return;
+    }
     try {
       const gpu = getGPU();
       const adapter = await gpu.requestAdapter();
-      this.device = await adapter.requestDevice();
+      const holder: DeviceHolder = {
+        device: await adapter.requestDevice(),
+        lostReason: undefined,
+      };
+
+      holder.device.lost.then(ev => {
+        holder.lostReason = ev.message;
+      });
+
+      this.holder = holder;
+      this.state = 'free';
     } catch (ex) {
       this.state = 'failed';
       throw ex;
@@ -46,22 +72,87 @@ class DevicePool {
 
   async acquire(): Promise<GPUDevice> {
     assert(this.state !== 'acquired', 'Device was in use');
-    assert(this.state !== 'failed', 'Failed to initialize WebGPU device');
+    assert(this.state !== 'failed', 'WebGPU device previously failed to initialize');
 
-    const state = this.state;
+    await this.initialize();
     this.state = 'acquired';
-    if (state === 'uninitialized') {
-      await this.initialize();
-    }
 
-    assert(!!this.device);
-    return this.device;
+    assert(this.holder !== undefined);
+
+    await assertReject(
+      this.holder.device.popErrorScope(),
+      'There was an error scope on the stack before a test'
+    );
+    this.beginErrorScopes();
+
+    return this.holder.device;
   }
 
-  release(device: GPUDevice): void {
+  beginErrorScopes(): void {
+    assert(this.holder !== undefined);
+    this.holder.device.pushErrorScope('out-of-memory');
+    this.holder.device.pushErrorScope('validation');
+  }
+
+  async endErrorScopes(): Promise<void> {
+    assert(this.holder !== undefined);
+    let gpuValidationError: GPUValidationError | GPUOutOfMemoryError | null;
+    let gpuOutOfMemoryError: GPUValidationError | GPUOutOfMemoryError | null;
+
+    try {
+      // May reject if the device was lost.
+      gpuValidationError = await this.holder.device.popErrorScope();
+      gpuOutOfMemoryError = await this.holder.device.popErrorScope();
+    } catch (ex) {
+      assert(
+        this.holder.lostReason !== undefined,
+        'popErrorScope failed, but device not (yet) lost'
+      );
+      throw ex;
+    }
+
+    if (gpuValidationError !== null) {
+      assert(gpuValidationError instanceof GPUValidationError);
+      unreachable(`Unexpected validation error occurred: ${gpuValidationError.message}`);
+    }
+    if (gpuOutOfMemoryError !== null) {
+      assert(gpuOutOfMemoryError instanceof GPUOutOfMemoryError);
+      unreachable('Unexpected out-of-memory error occurred');
+    }
+  }
+
+  async release(device: GPUDevice): Promise<void> {
     assert(this.state === 'acquired');
-    assert(device === this.device, 'Released device was the wrong device');
-    this.state = 'free';
+    assert(this.holder !== undefined);
+    assert(device === this.holder.device, 'Released device was the wrong device');
+
+    try {
+      // A timeout probably isn't supposed to happen, but could due to a browser bug
+      // (e.g. as of this writing, on Chrome GPU process crash, popErrorScope just hangs).
+      await raceWithRejectOnTimeout(
+        this.endErrorScopes(),
+        kPopErrorScopeTimeoutMS,
+        'finalization popErrorScope timed out'
+      );
+    } catch (ex) {
+      if (ex instanceof PromiseTimeoutError) {
+        this.holder = undefined;
+      }
+      throw ex;
+    } finally {
+      this.state = 'free';
+
+      // Hopefully if the device was lost, it has been reported by the time endErrorScopes()
+      // has finished (or timed out). If not, it could cause a finite number of extra test
+      // failures following this one (but should recover eventually).
+      if (this.holder) {
+        const lostReason = this.holder.lostReason;
+        if (lostReason !== undefined) {
+          this.holder = undefined;
+          unreachable(`Device was lost: ${lostReason}`);
+        }
+      }
+    }
   }
 }
 
@@ -87,39 +178,14 @@ export class GPUTest extends Fixture {
     const device = await devicePool.acquire();
     const queue = device.defaultQueue;
     this.objects = { device, queue };
-
-    try {
-      await device.popErrorScope();
-      unreachable('There was an error scope on the stack at the beginning of the test');
-      /* eslint-disable-next-line no-empty */
-    } catch (ex) {}
-
-    device.pushErrorScope('out-of-memory');
-    device.pushErrorScope('validation');
-
-    this.initialized = true;
   }
 
+  // Note: finalize is called even if init was unsuccessful.
   async finalize(): Promise<void> {
-    // Note: finalize is called even if init was unsuccessful.
     await super.finalize();
 
-    if (this.initialized) {
-      const gpuValidationError = await this.device.popErrorScope();
-      if (gpuValidationError !== null) {
-        assert(gpuValidationError instanceof GPUValidationError);
-        this.fail(`Unexpected validation error occurred: ${gpuValidationError.message}`);
-      }
-
-      const gpuOutOfMemoryError = await this.device.popErrorScope();
-      if (gpuOutOfMemoryError !== null) {
-        assert(gpuOutOfMemoryError instanceof GPUOutOfMemoryError);
-        this.fail('Unexpected out-of-memory error occurred');
-      }
-    }
-
     if (this.objects) {
-      devicePool.release(this.objects.device);
+      await devicePool.release(this.objects.device);
     }
   }
 
