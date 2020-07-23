6055ffec892ffa82e90ae1291c731ac68575a8f1
Kai Ninomiya <kainino@chromium.org>
2020-07-23 16:10:59 -0700

    Represent undefined with a magic value in JSON

---
+++ src/common/framework/query/json_param_value.ts
@@ -0,0 +1,16 @@
+import { ParamArgument } from '../params_utils.js';
+import { assert } from '../util/util.js';
+
+// JSON can't represent `undefined` and by default stores it as `null`.
+// Instead, store `undefined` as this magic string value in JSON.
+const jsUndefinedMagicValue = '✗undefined';
+
+export function stringifyParamValue(v: ParamArgument): string {
+  assert(v !== jsUndefinedMagicValue);
+
+  return JSON.stringify(v, (k, v) => (v === undefined ? jsUndefinedMagicValue : v));
+}
+
+export function parseParamValue(s: string): ParamArgument {
+  return JSON.parse(s, (k, v) => (v === jsUndefinedMagicValue ? undefined : v));
+}
