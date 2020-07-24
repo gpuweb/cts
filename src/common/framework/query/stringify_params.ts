29ab7a15203fcc625fadb766d7d2203ca95f1f2e
Kai Ninomiya <kainino@chromium.org>
2020-07-24 16:23:43 -0700

    Represent undefined with a magic value in JSON (#247)
    * Represent undefined with a magic value in JSON
    
    * lint

--- src/common/framework/query/stringify_params.ts
+++ src/common/framework/query/stringify_params.ts
@@ -6,6 +6,7 @@ import {
 } from '../params_utils.js';
 import { assert } from '../util/util.js';
 
+import { stringifyParamValue } from './json_param_value.js';
 import { kParamKVSeparator } from './separators.js';
 
 export function stringifyPublicParams(p: CaseParams): string[] {
@@ -19,7 +20,7 @@ export function stringifySingleParam(k: string, v: ParamArgument) {
 }
 
 function stringifySingleParamValue(v: ParamArgument): string {
-  const s = v === undefined ? 'undefined' : JSON.stringify(v);
+  const s = stringifyParamValue(v);
   assert(
     !badParamValueChars.test(s),
     `JSON.stringified param value must not match ${badParamValueChars} - was ${s}`
