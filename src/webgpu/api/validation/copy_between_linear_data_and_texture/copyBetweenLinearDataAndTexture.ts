4a373709d8ee7003199d09ef243e627a2e8c01b0
ponitka <tommek@google.com>
2020-08-11 00:48:07 +0200

    Copy between linear data and texture validation tests (#255)
    * Some tests
    
    * Suggestions from the review
    
    * Separating files
    
    * Fixes including aligning texture size and divisibility testing
    
    * Running npx grunt fix
    
    * Removing writeTexture specific tests and moving files to a directory
    
    * Test for offset + requiredBytesInCopy overflow
    
    * Clearing up the code
    
    Moving the expander functions to separate helper functions,
    making order of tests match the test plan, small fixes.
    
    * Moving format filtering to a helper function
    
    * Changing texture usage test parameters
    
    * Suggestions from the review
    
    * kTextureFormatInfo.copyable -> copySrc and copyDst
    
    * Apply suggestions from code review

---
+++ src/webgpu/api/validation/copy_between_linear_data_and_texture/copyBetweenLinearDataAndTexture.ts
@@ -0,0 +1,197 @@
+import { poptions } from '../../../../common/framework/params_builder.js';
+import { assert } from '../../../../common/framework/util/util.js';
+import { kTextureFormatInfo } from '../../../capability_info.js';
+import { ValidationTest } from '../validation_test.js';
+
+export const kAllTestMethods = [
+  'WriteTexture',
+  'CopyBufferToTexture',
+  'CopyTextureToBuffer',
+] as const;
+
+export class CopyBetweenLinearDataAndTextureTest extends ValidationTest {
+  bytesInACompleteRow(copyWidth: number, format: GPUTextureFormat): number {
+    assert(copyWidth % kTextureFormatInfo[format].blockWidth! === 0);
+    return (
+      (kTextureFormatInfo[format].bytesPerBlock! * copyWidth) /
+      kTextureFormatInfo[format].blockWidth!
+    );
+  }
+
+  requiredBytesInCopy(
+    layout: GPUTextureDataLayout,
+    format: GPUTextureFormat,
+    copyExtent: GPUExtent3DDict
+  ): number {
+    assert(layout.rowsPerImage! % kTextureFormatInfo[format].blockHeight! === 0);
+    assert(copyExtent.height % kTextureFormatInfo[format].blockHeight! === 0);
+    assert(copyExtent.width % kTextureFormatInfo[format].blockWidth! === 0);
+    if (copyExtent.width === 0 || copyExtent.height === 0 || copyExtent.depth === 0) {
+      return 0;
+    } else {
+      const texelBlockRowsPerImage = layout.rowsPerImage! / kTextureFormatInfo[format].blockHeight!;
+      const bytesPerImage = layout.bytesPerRow * texelBlockRowsPerImage;
+      const bytesInLastSlice =
+        layout.bytesPerRow * (copyExtent.height / kTextureFormatInfo[format].blockHeight! - 1) +
+        (copyExtent.width / kTextureFormatInfo[format].blockWidth!) *
+          kTextureFormatInfo[format].bytesPerBlock!;
+      return bytesPerImage * (copyExtent.depth - 1) + bytesInLastSlice;
+    }
+  }
+
+  testRun(
+    textureCopyView: GPUTextureCopyView,
+    textureDataLayout: GPUTextureDataLayout,
+    size: GPUExtent3D,
+    {
+      dataSize,
+      method,
+      success,
+      submit = false, // If submit is true, the validaton error is expected to come from the submit and encoding should succeed.
+    }: { dataSize: number; method: string; success: boolean; submit?: boolean }
+  ): void {
+    switch (method) {
+      case 'WriteTexture': {
+        const data = new Uint8Array(dataSize);
+
+        this.expectValidationError(() => {
+          this.device.defaultQueue.writeTexture(textureCopyView, data, textureDataLayout, size);
+        }, !success);
+
+        break;
+      }
+      case 'CopyBufferToTexture': {
+        const buffer = this.device.createBuffer({
+          size: dataSize,
+          usage: GPUBufferUsage.COPY_SRC,
+        });
+
+        const encoder = this.device.createCommandEncoder();
+        encoder.copyBufferToTexture({ buffer, ...textureDataLayout }, textureCopyView, size);
+
+        if (submit) {
+          const cmd = encoder.finish();
+          this.expectValidationError(() => {
+            this.device.defaultQueue.submit([cmd]);
+          }, !success);
+        } else {
+          this.expectValidationError(() => {
+            encoder.finish();
+          }, !success);
+        }
+
+        break;
+      }
+      case 'CopyTextureToBuffer': {
+        const buffer = this.device.createBuffer({
+          size: dataSize,
+          usage: GPUBufferUsage.COPY_DST,
+        });
+
+        const encoder = this.device.createCommandEncoder();
+        encoder.copyTextureToBuffer(textureCopyView, { buffer, ...textureDataLayout }, size);
+
+        if (submit) {
+          const cmd = encoder.finish();
+          this.expectValidationError(() => {
+            this.device.defaultQueue.submit([cmd]);
+          }, !success);
+        } else {
+          this.expectValidationError(() => {
+            encoder.finish();
+          }, !success);
+        }
+
+        break;
+      }
+    }
+  }
+
+  // This is a helper function used for creating a texture when we don't have to be very
+  // precise about its size as long as it's big enough and properly aligned.
+  createAlignedTexture(
+    format: GPUTextureFormat,
+    copySize: GPUExtent3DDict = { width: 1, height: 1, depth: 1 },
+    origin: Required<GPUOrigin3DDict> = { x: 0, y: 0, z: 0 }
+  ): GPUTexture {
+    return this.device.createTexture({
+      size: {
+        width: Math.max(1, copySize.width + origin.x) * kTextureFormatInfo[format].blockWidth!,
+        height: Math.max(1, copySize.height + origin.y) * kTextureFormatInfo[format].blockHeight!,
+        depth: Math.max(1, copySize.depth + origin.z),
+      },
+      format,
+      usage: GPUTextureUsage.COPY_SRC | GPUTextureUsage.COPY_DST,
+    });
+  }
+}
+
+// For testing divisibility by a number we test all the values returned by this function:
+function valuesToTestDivisibilityBy(number: number): Iterable<number> {
+  const values = [];
+  for (let i = 0; i <= 2 * number; ++i) {
+    values.push(i);
+  }
+  values.push(3 * number);
+  return values;
+}
+
+interface WithFormat {
+  format: GPUTextureFormat;
+}
+
+interface WithFormatAndCoordinate extends WithFormat {
+  coordinateToTest: keyof GPUOrigin3DDict | keyof GPUExtent3DDict;
+}
+
+interface WithFormatAndMethod extends WithFormat {
+  method: string;
+}
+
+// This is a helper function used for expanding test parameters for texel block alignment tests on offset
+export function texelBlockAlignmentTestExpanderForOffset({ format }: WithFormat) {
+  return poptions('offset', valuesToTestDivisibilityBy(kTextureFormatInfo[format].bytesPerBlock!));
+}
+
+// This is a helper function used for expanding test parameters for texel block alignment tests on rowsPerImage
+export function texelBlockAlignmentTestExpanderForRowsPerImage({ format }: WithFormat) {
+  return poptions(
+    'rowsPerImage',
+    valuesToTestDivisibilityBy(kTextureFormatInfo[format].blockHeight!)
+  );
+}
+
+// This is a helper function used for expanding test parameters for texel block alignment tests on origin and size
+export function texelBlockAlignmentTestExpanderForValueToCoordinate({
+  format,
+  coordinateToTest,
+}: WithFormatAndCoordinate) {
+  switch (coordinateToTest) {
+    case 'x':
+    case 'width':
+      return poptions(
+        'valueToCoordinate',
+        valuesToTestDivisibilityBy(kTextureFormatInfo[format].blockWidth!)
+      );
+
+    case 'y':
+    case 'height':
+      return poptions(
+        'valueToCoordinate',
+        valuesToTestDivisibilityBy(kTextureFormatInfo[format].blockHeight!)
+      );
+
+    case 'z':
+    case 'depth':
+      return poptions('valueToCoordinate', valuesToTestDivisibilityBy(1));
+  }
+}
+
+// This is a helper function used for filtering test parameters
+export function formatCopyableWithMethod({ format, method }: WithFormatAndMethod): boolean {
+  if (method === 'CopyTextureToBuffer') {
+    return kTextureFormatInfo[format].copySrc;
+  } else {
+    return kTextureFormatInfo[format].copyDst;
+  }
+}
