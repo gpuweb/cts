f40e51656f08652ec8286ada01589e3652a786df
Kai Ninomiya <kainino@chromium.org>
2020-09-03 17:54:29 -0700

    Add a very lightweight preprocessor using JS template strings

---
+++ src/common/framework/preprocessor.ts
@@ -0,0 +1,101 @@
+import { assert } from './util/util.js';
+
+// The state of the preprocessor is a stack of States.
+// The transitions in the state space are:
+// - Sibling elif
+// - Sibling else
+// - Sibling endif
+// - Child if
+const enum State {
+  Seeking, // Still looking for a passing condition
+  Passing, // Currently inside a passing condition
+  Skipping, // Have already seen a passing condition; now skipping the rest
+}
+
+export function pp(parts: TemplateStringsArray, ...predicates: readonly boolean[]): string {
+  let result = '';
+
+  const stateStack: State[] = [State.Passing];
+
+  for (let partIndex = 0; partIndex < parts.length; ++partIndex) {
+    const lines = parts[partIndex].split('\n');
+
+    checkDirectiveComment(partIndex, lines[0]);
+    for (let lineIndex = 1; lineIndex < lines.length; ++lineIndex) {
+      const line = lines[lineIndex];
+      if (line.startsWith('%')) {
+        const match = matchDirective(line);
+
+        if (match[2] === 'if' || match[2] === 'elif') {
+          assert(lineIndex === lines.length - 1);
+          assert(partIndex < predicates.length);
+          const predicate = predicates[partIndex];
+
+          if (match[2] === 'if') {
+            checkDepth(stateStack.length, line);
+            const parentState = stateStack[stateStack.length - 1];
+            stateStack.push(
+              parentState !== State.Passing
+                ? State.Skipping
+                : predicate
+                ? State.Passing
+                : State.Seeking
+            );
+          } else {
+            assert(match[2] === 'elif', 'expected %elif');
+            const siblingState = stateStack.pop()!;
+            checkDepth(stateStack.length, line);
+            const parentState = stateStack[stateStack.length - 1];
+            stateStack.push(
+              siblingState !== State.Seeking || parentState !== State.Passing
+                ? State.Skipping
+                : predicate
+                ? State.Passing
+                : State.Seeking
+            );
+          }
+        } else {
+          const siblingState = stateStack.pop()!;
+          checkDepth(stateStack.length, line);
+          if (match[3] === 'else') {
+            stateStack.push(siblingState === State.Seeking ? State.Passing : State.Skipping);
+          } else {
+            assert(match[3] === 'endif', 'expected %endif');
+          }
+        }
+      } else {
+        if (stateStack[stateStack.length - 1] === State.Passing) {
+          result += line + '\n';
+        }
+      }
+    }
+  }
+  assert(stateStack.length === 1, 'Unterminated preprocessor condition at end of file');
+
+  return result.trim();
+}
+
+function checkDirectiveComment(partIndex: number, line: string) {
+  const directiveComment = /^ *(#.*)?$/;
+
+  const explanation = partIndex === 0 ? 'on first line' : 'at end of preprocessor directive line';
+  assert(
+    directiveComment.test(line),
+    () => `Text ${explanation} must match ${directiveComment}, in:\n${line}`
+  );
+}
+
+function matchDirective(line: string): RegExpExecArray {
+  const directive = /^%+((if|elif) |(else|endif) *(#.*)?)$/;
+
+  const match = directive.exec(line);
+  assert(match !== null, () => `Preprocessor directive must match ${directive}, in:\n${line}`);
+  return match;
+}
+
+function checkDepth(depth: number, line: string) {
+  assert(
+    RegExp(`^%{${depth}}[^%]`).test(line),
+    `Number of "%"s must match nesting depth, currently ${depth} (e.g. %if x  %%if y  %%endif  %endif), in:\n${line}`
+  );
+}
---
+++ src/unittests/preprocessor.spec.ts
@@ -0,0 +1,183 @@
+export const description = `
+Test for "pp" preprocessor.
+`;
+
+import { pp } from '../common/framework/preprocessor.js';
+import { makeTestGroup } from '../common/framework/test_group.js';
+
+import { UnitTest } from './unit_test.js';
+
+class F extends UnitTest {
+  test(act: string, exp: string): void {
+    this.expect(act === exp, 'got: ' + act.replace('\n', '⏎'));
+  }
+}
+
+export const g = makeTestGroup(F);
+
+g.test('empty').fn(t => {
+  const act = pp``;
+  const exp = '';
+  t.test(act, exp);
+});
+
+g.test('basic,true').fn(t => {
+  const act = pp`
+a
+%if ${true} #comment
+c
+%endif
+d
+`;
+  const exp = 'a\nc\nd';
+  t.test(act, exp);
+});
+
+g.test('basic,false').fn(t => {
+  const act = pp`#start-of-file comment
+a
+%if ${false} #end-of-directive comment
+c
+%endif
+d
+`;
+  const exp = 'a\nd';
+  t.test(act, exp);
+});
+
+g.test('else,1').fn(t => {
+  const act = pp`
+a
+%if ${true} # comment
+b
+%else
+c
+%endif
+d
+`;
+  const exp = 'a\nb\nd';
+  t.test(act, exp);
+});
+
+g.test('else,2').fn(t => {
+  const act = pp`
+a
+%if ${false} #comment
+b
+%else
+c
+%endif
+d
+`;
+  const exp = 'a\nc\nd';
+  t.test(act, exp);
+});
+
+g.test('else,3').fn(t => {
+  const act = pp`
+a
+%if ${false} #comment
+b
+%else
+c
+%else
+e
+%endif
+d
+`;
+  const exp = 'a\nc\nd';
+  t.test(act, exp);
+});
+
+g.test('elif,1').fn(t => {
+  const act = pp`
+a
+%if ${false} #comment
+b
+%elif ${true} #comment
+e
+%else #comment
+c
+%endif #comment
+d
+`;
+  const exp = 'a\ne\nd';
+  t.test(act, exp);
+});
+
+g.test('elif,2').fn(t => {
+  const act = pp`
+a
+%if ${true} #comment
+b
+%elif ${true} #comment
+e
+%else #comment
+c
+%endif #comment
+d
+`;
+  const exp = 'a\nb\nd';
+  t.test(act, exp);
+});
+
+g.test('nested,1').fn(t => {
+  const act = pp`
+a
+%if ${false} #comment
+b
+%%if ${true} #comment
+e
+%%endif
+c
+%endif #comment
+d
+`;
+  const exp = 'a\nd';
+  t.test(act, exp);
+});
+
+g.test('nested,2').fn(t => {
+  const act = pp`
+a
+%if ${false} #comment
+b
+%else
+h
+%%if ${false} #comment
+e
+%%elif ${true}
+f
+%%else
+g
+%%endif
+c
+%endif #comment
+d
+`;
+  const exp = 'a\nh\nf\nc\nd';
+  t.test(act, exp);
+});
+
+g.test('errors,pass').fn(() => {
+  pp`\n%if ${true}\n%endif`;
+  pp`\n%if ${true}\n%endif\n`;
+  pp`\n%if ${true}\n%else\n%endif\n`;
+  pp`\n%if ${true}\n%%if ${true}\n%%endif\n%endif`;
+  pp`\n%if ${true}\n%%if ${true}\n%%else\n%%else\n%%endif\n%endif`;
+});
+
+g.test('errors,fail').fn(t => {
+  const e = (fn: () => void) => t.shouldThrow('Error', fn);
+  e(() => pp`\n%if true\n%endif`);
+  e(() => pp`\n%if${true}`);
+  e(() => pp`\n%if ${true}`);
+  e(() => pp`\n%if ${true}\n%else`);
+  e(() => pp`\n%if ${true}\n%elif ${true}`);
+  e(() => pp`\n%if ${true}\n%elif ${true}\n%else`);
+  e(() => pp`\n%if ${true}\n%if ${true}\n%endif\n%endif`);
+  e(() => pp`\n%endif\n`);
+  e(() => pp`\n%endif\n%endif\n`);
+  e(() => pp`\n%else\n%endif\n`);
+  e(() => pp`\n%%if ${true}\n%%endif`);
+});
