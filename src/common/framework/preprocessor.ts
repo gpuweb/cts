ec0879779192bd7009779ff32f563c20a78ba2b3
Kai Ninomiya <kainino@chromium.org>
2020-09-10 16:16:19 -0700

    Allow string/number substitutions on non-directive lines

--- src/common/framework/preprocessor.ts
+++ src/common/framework/preprocessor.ts
@@ -1,5 +1,83 @@
 import { assert } from './util/util.js';
 
+export function pp(
+  strings: TemplateStringsArray,
+  ...values: ReadonlyArray<boolean | string | number>
+): string {
+  let result = '';
+  const stateStack = new StateStack();
+  let lastStringEndedWithDirective = false;
+
+  for (let stringIndex = 0; stringIndex < strings.length; ++stringIndex) {
+    const lines = strings[stringIndex].split(/(?<=\n)/); // include trailing \n on each line
+
+    if (stringIndex === 0) {
+      assert(
+        strings[0].length === 0 || strings[0][0] === '\n',
+        () => `First line should always be empty, in:\n${lines[0]}`
+      );
+    }
+
+    let lineIndex = 0;
+    if (lastStringEndedWithDirective) {
+      checkDirectiveComment(lines[0]);
+      lineIndex = 1;
+      lastStringEndedWithDirective = false;
+    } else if (stateStack.passing && stringIndex > 0) {
+      const subst = values[stringIndex - 1];
+      assert(
+        typeof subst === 'string' || typeof subst === 'number',
+        '${value} not following %if/%elif must be string or number'
+      );
+      result += subst;
+    }
+    for (; lineIndex < lines.length; ++lineIndex) {
+      const line = lines[lineIndex];
+      if (!line.startsWith('%')) {
+        if (stateStack.passing) {
+          result += line;
+          // TODO: need to substitute non-predicate values
+        }
+        continue;
+      }
+
+      try {
+        const match = matchDirective(line);
+        const directiveDepth = match[1].length;
+
+        if (match[4] === 'endif') {
+          stateStack.endif(directiveDepth);
+        } else if (match[4] === 'else') {
+          stateStack.else(directiveDepth);
+        } else {
+          const valueIndex = stringIndex; // Index of the value right after this string
+          assert(
+            lineIndex === lines.length - 1 && valueIndex < values.length,
+            `%${match[4]} must be followed immediately by \${boolean} on the same line`
+          );
+          lastStringEndedWithDirective = true;
+
+          const predicate = values[valueIndex];
+          assert(typeof predicate === 'boolean', '${value} following %if/%elif must be boolean');
+
+          if (match[3] === 'if') {
+            stateStack.if(predicate, directiveDepth);
+          } else {
+            assert(match[3] === 'elif', 'expected %elif');
+            stateStack.elif(predicate, directiveDepth);
+          }
+        }
+      } catch (ex) {
+        if (ex instanceof Error) ex.message += `, in:\n${line.trimRight()}`;
+        throw ex;
+      }
+    }
+  }
+  assert(stateStack.length === 1, 'Unterminated preprocessor condition at end of file');
+
+  return result.trim();
+}
+
 // The state of the preprocessor is a stack of States.
 // The transitions in the state space are:
 // - Sibling elif
@@ -12,93 +90,67 @@ const enum State {
   Skipping, // Have already seen a passing condition; now skipping the rest
 }
 
-export function pp(parts: TemplateStringsArray, ...predicates: readonly boolean[]): string {
-  let result = '';
+class StateStack {
+  private stack: State[] = [State.Passing];
 
-  const stateStack: State[] = [State.Passing];
+  get length(): number {
+    return this.stack.length;
+  }
 
-  for (let partIndex = 0; partIndex < parts.length; ++partIndex) {
-    const lines = parts[partIndex].split('\n');
+  get passing(): boolean {
+    return this.stack[this.stack.length - 1] === State.Passing;
+  }
 
-    checkDirectiveComment(partIndex, lines[0]);
-    for (let lineIndex = 1; lineIndex < lines.length; ++lineIndex) {
-      const line = lines[lineIndex];
-      if (line.startsWith('%')) {
-        const match = matchDirective(line);
+  if(predicate: boolean, directiveDepth: number): void {
+    const parentState = this.stack[this.stack.length - 1];
+    this.checkDepth(directiveDepth);
+    this.stack.push(
+      parentState !== State.Passing ? State.Skipping : predicate ? State.Passing : State.Seeking
+    );
+  }
 
-        if (match[2] === 'if' || match[2] === 'elif') {
-          assert(
-            lineIndex === lines.length - 1,
-            `%${match[2]} must be immediately followed by \${...}`
-          );
-          assert(partIndex < predicates.length, `File ended while parsing %${match[2]}`);
-          const predicate = predicates[partIndex];
-
-          if (match[2] === 'if') {
-            checkDepth(stateStack.length, line);
-            const parentState = stateStack[stateStack.length - 1];
-            stateStack.push(
-              parentState !== State.Passing
-                ? State.Skipping
-                : predicate
-                ? State.Passing
-                : State.Seeking
-            );
-          } else {
-            assert(match[2] === 'elif', 'expected %elif');
-            const siblingState = stateStack.pop()!;
-            checkDepth(stateStack.length, line);
-            const parentState = stateStack[stateStack.length - 1];
-            stateStack.push(
-              siblingState !== State.Seeking || parentState !== State.Passing
-                ? State.Skipping
-                : predicate
-                ? State.Passing
-                : State.Seeking
-            );
-          }
-        } else {
-          const siblingState = stateStack.pop()!;
-          checkDepth(stateStack.length, line);
-          if (match[3] === 'else') {
-            stateStack.push(siblingState === State.Seeking ? State.Passing : State.Skipping);
-          } else {
-            assert(match[3] === 'endif', 'expected %endif');
-          }
-        }
-      } else {
-        if (stateStack[stateStack.length - 1] === State.Passing) {
-          result += line + '\n';
-        }
-      }
+  elif(predicate: boolean, directiveDepth: number): void {
+    const siblingState = this.stack.pop()!;
+    if (siblingState !== State.Seeking) {
+      this.checkDepth(directiveDepth);
+      this.stack.push(State.Skipping);
+    } else {
+      this.if(predicate, directiveDepth);
     }
   }
-  assert(stateStack.length === 1, 'Unterminated preprocessor condition at end of file');
 
-  return result.trim();
+  else(directiveDepth: number): void {
+    const siblingState = this.stack.pop()!;
+    this.checkDepth(directiveDepth);
+    this.stack.push(siblingState === State.Seeking ? State.Passing : State.Skipping);
+  }
+
+  endif(directiveDepth: number): void {
+    this.stack.pop();
+    this.checkDepth(directiveDepth);
+  }
+
+  private checkDepth(directiveDepth: number) {
+    assert(
+      this.stack.length === directiveDepth,
+      `Number of "%"s must match nesting depth, currently ${this.stack.length} (e.g. %if x  %%if y  %%endif  %endif)`
+    );
+  }
 }
 
-function checkDirectiveComment(partIndex: number, line: string) {
-  const directiveComment = /^ *(#.*)?$/;
+function checkDirectiveComment(line: string) {
+  const directiveComment = /^ *(#.*)?\n$/;
 
-  const explanation = partIndex === 0 ? 'on first line' : 'at end of preprocessor directive line';
   assert(
     directiveComment.test(line),
-    () => `Text ${explanation} must match ${directiveComment}, in:\n${line}`
+    () => `Only comments allowed at the end of a directive line ${directiveComment}, in:\n${line}`
   );
 }
 
 function matchDirective(line: string): RegExpExecArray {
-  const directive = /^%+((if|elif) |(else|endif) *(#.*)?)$/;
+  const directive = /^(%+)((if|elif) |(else|endif) *(#.*)?\n)$/;
 
   const match = directive.exec(line);
-  assert(match !== null, () => `Preprocessor directive must match ${directive}, in:\n${line}`);
+  assert(match !== null, () => `Preprocessor directive must match ${directive}`);
   return match;
-}
-
-function checkDepth(depth: number, line: string) {
-  assert(
-    RegExp(`^%{${depth}}[^%]`).test(line),
-    `Number of "%"s must match nesting depth, currently ${depth} (e.g. %if x  %%if y  %%endif  %endif), in:\n${line}`
-  );
 }
--- src/unittests/preprocessor.spec.ts
+++ src/unittests/preprocessor.spec.ts
@@ -16,12 +16,51 @@ class F extends UnitTest {
 export const g = makeTestGroup(F);
 
 g.test('empty').fn(t => {
-  const act = pp``;
-  const exp = '';
+  t.test(pp``, '');
+  t.test(pp`\n`, '');
+  t.test(pp`\n\n`, '');
+});
+
+g.test('plain').fn(t => {
+  t.test(pp`\na`, 'a');
+  t.test(pp`\n\na`, 'a');
+  t.test(pp`\na\n`, 'a');
+  t.test(pp`\na\n\n`, 'a');
+});
+
+g.test('substitutions,1').fn(t => {
+  const act = pp`
+a ${3} b`;
+  const exp = 'a 3 b';
   t.test(act, exp);
 });
 
-g.test('basic,true').fn(t => {
+g.test('substitutions,2').fn(t => {
+  const act = pp`
+a ${'x'}`;
+  const exp = 'a x';
+  t.test(act, exp);
+});
+
+g.test('substitutions,3').fn(t => {
+  const act = pp`
+a ${'x'} b`;
+  const exp = 'a x b';
+  t.test(act, exp);
+});
+
+g.test('substitutions,4').fn(t => {
+  const act = pp`
+a
+%if ${false}
+${'x'}
+%endif
+b`;
+  const exp = 'a\nb';
+  t.test(act, exp);
+});
+
+g.test('if,true').fn(t => {
   const act = pp`
 a
 %if ${true} #comment
@@ -33,8 +72,8 @@ d
   t.test(act, exp);
 });
 
-g.test('basic,false').fn(t => {
-  const act = pp`#start-of-file comment
+g.test('if,false').fn(t => {
+  const act = pp`
 a
 %if ${false} #end-of-directive comment
 c
@@ -126,7 +165,7 @@ g.test('nested,1').fn(t => {
 a
 %if ${false} #comment
 b
-%%if ${true} #comment
+%%if ${true}#comment
 e
 %%endif
 c
@@ -140,11 +179,11 @@ d
 g.test('nested,2').fn(t => {
   const act = pp`
 a
-%if ${false} #comment
+%if ${false}#comment
 b
 %else
 h
-%%if ${false} #comment
+%%if ${false} # c o m m e n t \n\
 e
 %%elif ${true}
 f
@@ -160,30 +199,38 @@ d
 });
 
 g.test('errors,pass').fn(() => {
-  pp`\n%if ${true}\n%endif`;
   pp`\n%if ${true}\n%endif\n`;
   pp`\n%if ${true}\n%else\n%endif\n`;
-  pp`\n%if ${true}\n%%if ${true}\n%%endif\n%endif`;
-  pp`\n%if ${true}\n%%if ${true}\n%%else\n%%else\n%%endif\n%endif`;
+  pp`\n%if ${true}\n%%if ${true}\n%%endif\n%endif\n`;
+  pp`\n%if ${true}\n%%if ${true}\n%%else\n%%else\n%%endif\n%endif\n`;
 });
 
 g.test('errors,fail').fn(t => {
   const e = (fn: () => void) => t.shouldThrow('Error', fn);
+  e(() => pp`a`);
+  e(() => pp`a\n`);
   e(() => pp`\n%if`);
   e(() => pp`\n%if `);
-  e(() => pp`\n%if true\n%endif`);
+  e(() => pp`\n%if ${true}\n%endif`);
+  e(() => pp`\n%if true\n%endif\n`);
+  e(() => pp`\n%if \n%endif\n`);
+  e(() => pp`\n%if \n${true}%endif\n`);
+  e(() => pp`\n%if \n${1}%endif\n`);
   e(() => pp`\n%if true\n${true}\n%endif`);
   e(() => pp`\n%if true\n%endif\n${true}`);
   e(() => pp`\n%if \n${true}\n%endif`);
   e(() => pp`\n%if \n%endif\n${true}`);
   e(() => pp`\n%if${true}`);
   e(() => pp`\n%if ${true}`);
-  e(() => pp`\n%if ${true}\n%else`);
-  e(() => pp`\n%if ${true}\n%elif ${true}`);
-  e(() => pp`\n%if ${true}\n%elif ${true}\n%else`);
-  e(() => pp`\n%if ${true}\n%if ${true}\n%endif\n%endif`);
+  e(() => pp`\n%if ${true}\n%else\n`);
+  e(() => pp`\n%if ${true}\n%elif ${true}\n`);
+  e(() => pp`\n%if ${true}\n%elif ${true}\n%else\n`);
+  e(() => pp`\n%if ${true}\n%if ${true}\n%endif\n%endif\n`);
   e(() => pp`\n%endif\n`);
   e(() => pp`\n%endif\n%endif\n`);
   e(() => pp`\n%else\n%endif\n`);
-  e(() => pp`\n%%if ${true}\n%%endif`);
+  e(() => pp`\n%%if ${true}\n%%endif\n`);
+  e(() => pp`\n%if ${true} ${3}\n%endif\n`);
+  e(() => pp`\n%if ${true} # ${3}\n%endif\n`);
+  e(() => pp`\n%if ${true}\n%endif ${3}\n`);
 });
