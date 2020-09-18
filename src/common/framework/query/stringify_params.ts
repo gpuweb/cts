ccbca39656e763bc95156b472885abfc9b349396
Kai Ninomiya <kainino@chromium.org>
2020-09-18 10:56:35 -0700

    Significantly speed up build by optimizing n^2 code (#289)
    * Significantly speed up build by optimizing n^2 code
    
    * Update src/common/framework/util/util.ts
    
    Co-authored-by: Austin Eng <2154796+austinEng@users.noreply.github.com>

--- src/common/framework/query/stringify_params.ts
+++ src/common/framework/query/stringify_params.ts
@@ -6,8 +6,8 @@ import {
 } from '../params_utils.js';
 import { assert } from '../util/util.js';
 
-import { stringifyParamValue } from './json_param_value.js';
-import { kParamKVSeparator } from './separators.js';
+import { stringifyParamValue, stringifyParamValueUniquely } from './json_param_value.js';
+import { kBigSeparator, kParamKVSeparator } from './separators.js';
 
 export function stringifyPublicParams(p: CaseParams): string[] {
   return Object.keys(p)
@@ -15,8 +15,23 @@ export function stringifyPublicParams(p: CaseParams): string[] {
     .map(k => stringifySingleParam(k, p[k]));
 }
 
+/**
+ * An _approximately_ unique string representing a CaseParams value.
+ */
+export function stringifyPublicParamsUniquely(p: CaseParams): string {
+  const keys = Object.keys(p).sort();
+  return keys
+    .filter(k => paramKeyIsPublic(k))
+    .map(k => stringifySingleParamUniquely(k, p[k]))
+    .join(kBigSeparator);
+}
+
 export function stringifySingleParam(k: string, v: ParamArgument) {
   return `${k}${kParamKVSeparator}${stringifySingleParamValue(v)}`;
+}
+
+function stringifySingleParamUniquely(k: string, v: ParamArgument) {
+  return `${k}${kParamKVSeparator}${stringifyParamValueUniquely(v)}`;
 }
 
 function stringifySingleParamValue(v: ParamArgument): string {
