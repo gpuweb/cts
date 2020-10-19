4b4198b4a7fa3f9f12b9f9af80439edff90628d4
Kai Ninomiya <kainino@chromium.org>
2020-10-19 16:57:40 -0700

    Use logical assignment operators

--- src/webgpu/api/validation/createBindGroupLayout.spec.ts
+++ src/webgpu/api/validation/createBindGroupLayout.spec.ts
@@ -97,28 +97,22 @@ g.test('bindingTypeSpecific_optional_members')
 
     let success = true;
     if (!(type in kBufferBindingTypeInfo)) {
-      if (hasDynamicOffset !== undefined) success = false;
-      if (minBufferBindingSize !== undefined) success = false;
+      success &&= hasDynamicOffset === undefined;
+      success &&= minBufferBindingSize === undefined;
     }
     if (!(type in kTextureBindingTypeInfo)) {
-      if (viewDimension !== undefined) success = false;
+      success &&= viewDimension === undefined;
     }
     if (kBindingTypeInfo[type].resource !== 'sampledTex') {
-      if (textureComponentType !== undefined) success = false;
-      if (multisampled !== undefined) success = false;
+      success &&= textureComponentType === undefined;
+      success &&= multisampled === undefined;
     }
     if (kBindingTypeInfo[type].resource !== 'storageTex') {
-      if (storageTextureFormat !== undefined) success = false;
+      success &&= storageTextureFormat === undefined;
     } else {
-      if (viewDimension !== undefined && !kTextureViewDimensionInfo[viewDimension].storage) {
-        success = false;
-      }
-      if (
-        storageTextureFormat !== undefined &&
-        !kAllTextureFormatInfo[storageTextureFormat].storage
-      ) {
-        success = false;
-      }
+      success &&= viewDimension === undefined || kTextureViewDimensionInfo[viewDimension].storage;
+      success &&=
+        storageTextureFormat === undefined || kAllTextureFormatInfo[storageTextureFormat].storage;
     }
 
     t.expectValidationError(() => {
