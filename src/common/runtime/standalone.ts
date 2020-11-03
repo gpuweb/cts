21a476f22740552db847af896b4a81c9bb11792e
Kai Ninomiya <kainino@chromium.org>
2020-11-03 11:30:21 -0800

    standalone: Replace results JSON textarea with copy button (#322)
    * standalone: Replace results JSON textarea with copy button
    
    In Chromium, this textarea causes a massive relayout whenever it gets
    updated (40s on my machine). Remove it, and replace it with a button
    that copies the JSON to the clipboard (fast).
    
    * move event listener setup
    
    * nit

--- src/common/runtime/standalone.ts
+++ src/common/runtime/standalone.ts
@@ -25,7 +25,6 @@ const logger = new Logger(debug);
 const worker = optionEnabled('worker') ? new TestWorker(debug) : undefined;
 
 const resultsVis = document.getElementById('resultsVis')!;
-const resultsJSON = document.getElementById('resultsJSON')!;
 
 type RunSubtree = () => Promise<void>;
 
@@ -190,7 +189,6 @@ function makeTreeNodeHeaderHTML(
     .attr('title', runtext)
     .on('click', async () => {
       await runSubtree();
-      updateJSON();
     })
     .appendTo(div);
   $('<a>')
@@ -220,10 +218,6 @@ function makeTreeNodeHeaderHTML(
       .appendTo(nodetitle);
   }
   return div[0];
-}
-
-function updateJSON(): void {
-  resultsJSON.textContent = logger.asJSON(2);
 }
 
 // Collapse s:f:t:* or s:f:t:c by default.
@@ -267,6 +261,10 @@ let lastQueryLevelToExpand: TestQueryLevel = 2;
   $('#expandall').change(function (this) {
     const checked = (this as HTMLInputElement).checked;
     $('.collapsebtn').prop('checked', checked).trigger('change');
+  });
+
+  document.getElementById('copyResultsJSON')!.addEventListener('click', () => {
+    navigator.clipboard.writeText(logger.asJSON(2));
   });
 
   if (runnow) {
