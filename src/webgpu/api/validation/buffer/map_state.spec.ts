7e3c1ee46229dc4b15834b292c01435f757db77c
Kai Ninomiya <kainino@chromium.org>
2020-12-02 14:58:49 -0800

    address comments

--- src/webgpu/api/validation/buffer/map_state.spec.ts
+++
@@ -1,8 +0,0 @@
-export const description = `
-Validation tests for GPUBuffer.mapAsync, GPUBuffer.unmap and GPUBuffer.getMappedRange.
-`;
-
-import { makeTestGroup } from '../../../../common/framework/test_group.js';
-import { ValidationTest } from '../validation_test.js';
-
-export const g = makeTestGroup(ValidationTest);
