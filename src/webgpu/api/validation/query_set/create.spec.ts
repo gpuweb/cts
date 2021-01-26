d4229be980d7430b6efb12ca01923078cdf35f71
Hao Li <hao.x.li@intel.com>
2021-01-26 19:43:01 +0800

    Group tests by count and pipelineStatistics

--- src/webgpu/api/validation/query_set/create.spec.ts
+++ src/webgpu/api/validation/query_set/create.spec.ts
@@ -2,140 +2,110 @@ export const description = `
 Tests for validation in createQuerySet.
 `;
 
-import { poptions } from '../../../../common/framework/params_builder.js';
+import { params, poptions } from '../../../../common/framework/params_builder.js';
 import { makeTestGroup } from '../../../../common/framework/test_group.js';
 import { ValidationTest } from '../validation_test.js';
 
 export const g = makeTestGroup(ValidationTest);
 
 const kMaxQueryCount = 8192;
+const kQuerytype = ['occlusion', 'pipeline-statistics', 'timestamp'] as const;
 
-// Test with these query counts.
-const kQueryCounts: number[] = [
-  // valid
-  0,
-  kMaxQueryCount,
-  // invalid
-  kMaxQueryCount + 1,
-];
-
-g.test('occlusion')
+g.test('count')
   .desc(
     `
-Tests that create query set with the type of occlusion.
-- control cases:
-  - count <= kMaxQueryCount
-  - pipelineStatistics is undefined or empty
-- invalid cases:
-  - count > kMaxQueryCount
-  - pipelineStatistics is set with pipeline statistic names
+Tests that create query set with the count for all query types:
+- count {<, =, >} kMaxQueryCount
+- x= {occlusion, pipeline-statistics, timestamp} query
   `
   )
-  .params([
-    ...poptions('count', kQueryCounts),
-    ...poptions('pipelineStatistics', [
-      // valid
-      undefined,
-      [],
-      // invalid
-      ['clipper-invocations'],
-    ]),
-  ])
-  .fn(t => {
-    const { count = 1, pipelineStatistics } = t.params as {
-      count?: number;
-      pipelineStatistics?: GPUPipelineStatisticName[];
-    };
-
-    t.expectValidationError(() => {
-      t.device.createQuerySet({ type: 'occlusion', count, pipelineStatistics });
-    }, count > kMaxQueryCount || (pipelineStatistics !== undefined && pipelineStatistics.length > 0));
-  });
-
-g.test('pipeline_statistics')
-  .desc(
-    `
-Tests that create query set with the type of pipeline-statistics.
-- control cases:
-  - count <= kMaxQueryCount
-  - pipelineStatistics is set with pipeline statistic names
-- invalid cases:
-  - count > kMaxQueryCount
-  - pipelineStatistics is undefined, empty or duplicated
-  `
+  .params(
+    params()
+      .combine(poptions('type', kQuerytype))
+      .combine(poptions('count', [0, kMaxQueryCount, kMaxQueryCount + 1]))
   )
-  .params([
-    ...poptions('count', kQueryCounts),
-    ...poptions('pipelineStatistics', [
-      // valid
-      ['clipper-invocations'],
-      // invalid
-      undefined,
-      [],
-      ['clipper-invocations', 'clipper-invocations'],
-    ]),
-  ])
   .fn(async t => {
-    await t.selectDeviceOrSkipTestCase({
-      extensions: ['pipeline-statistics-query'],
-    });
+    const { type, count } = t.params;
 
-    const { count = 1, pipelineStatistics = ['vertex-shader-invocations'] } = t.params as {
-      count?: number;
-      pipelineStatistics?: GPUPipelineStatisticName[];
-    };
-
-    // If the pipelineStatistics is passed as undefined, the default value 'vertex-shader-invocations'
-    // is got here, test it with undefined seperately.
-    if (count === 1 && pipelineStatistics[0] === 'vertex-shader-invocations') {
-      t.expectValidationError(() => {
-        t.device.createQuerySet({ type: 'pipeline-statistics', count });
+    if (type === 'pipeline-statistics') {
+      await t.selectDeviceOrSkipTestCase({
+        extensions: ['pipeline-statistics-query'],
       });
-    } else {
-      const shouldError =
-        count > kMaxQueryCount ||
-        pipelineStatistics.length === 0 ||
-        pipelineStatistics.length !== Array.from(new Set(pipelineStatistics)).length;
-
-      t.expectValidationError(() => {
-        t.device.createQuerySet({ type: 'pipeline-statistics', count, pipelineStatistics });
-      }, shouldError);
+    } else if (type === 'timestamp') {
+      await t.selectDeviceOrSkipTestCase({
+        extensions: ['timestamp-query'],
+      });
     }
-  });
 
-g.test('timestamp')
-  .desc(
-    `
-Tests that create query set with the type of timestamp.
-- control cases:
-  - count <= kMaxQueryCount
-  - pipelineStatistics is undefined or empty
-- invalid cases:
-  - count > kMaxQueryCount
-  - pipelineStatistics is set with pipeline statistic names
-  `
-  )
-  .params([
-    ...poptions('count', kQueryCounts),
-    ...poptions('pipelineStatistics', [
-      // valid
-      undefined,
-      [],
-      // invalid
-      ['clipper-invocations'],
-    ]),
-  ])
-  .fn(async t => {
-    await t.selectDeviceOrSkipTestCase({
-      extensions: ['timestamp-query'],
-    });
-
-    const { count = 1, pipelineStatistics } = t.params as {
-      count?: number;
-      pipelineStatistics?: GPUPipelineStatisticName[];
-    };
+    const pipelineStatistics =
+      type === 'pipeline-statistics' ? (['clipper-invocations'] as const) : ([] as const);
 
     t.expectValidationError(() => {
-      t.device.createQuerySet({ type: 'timestamp', count, pipelineStatistics });
-    }, count > kMaxQueryCount || (pipelineStatistics !== undefined && pipelineStatistics.length > 0));
+      t.device.createQuerySet({ type, count, pipelineStatistics });
+    }, count > kMaxQueryCount);
+  });
+
+g.test('pipelineStatistics')
+  .desc(
+    `
+Tests that create query set with the GPUPipelineStatisticName for all query types:
+- pipelineStatistics is undefined or empty
+- pipelineStatistics is a sequence of valid values
+- pipelineStatistics is a sequence of duplicate values
+- x= {occlusion, pipeline-statistics, timestamp} query
+  `
+  )
+  .params(
+    params()
+      .combine(poptions('type', kQuerytype))
+      .combine(
+        poptions('pipelineStatistics', [
+          undefined,
+          [] as const,
+          ['clipper-invocations'] as const,
+          ['clipper-invocations', 'clipper-invocations'] as const,
+          [
+            'clipper-invocations',
+            'clipper-primitives-out',
+            'compute-shader-invocations',
+            'fragment-shader-invocations',
+            'vertex-shader-invocations',
+          ] as const,
+        ])
+      )
+      // Except pipeline statistics query, there is no need to test duplicate values and all values of GPUPipelineStatisticName
+      .unless(
+        ({ type, pipelineStatistics }) =>
+          type !== 'pipeline-statistics' &&
+          pipelineStatistics !== undefined &&
+          pipelineStatistics.length >= 2
+      )
+  )
+  .fn(async t => {
+    const { type, pipelineStatistics } = t.params;
+
+    if (type === 'pipeline-statistics') {
+      await t.selectDeviceOrSkipTestCase({
+        extensions: ['pipeline-statistics-query'],
+      });
+    } else if (type === 'timestamp') {
+      await t.selectDeviceOrSkipTestCase({
+        extensions: ['timestamp-query'],
+      });
+    }
+
+    const count = 1;
+
+    const shouldError =
+      (type !== 'pipeline-statistics' &&
+        pipelineStatistics !== undefined &&
+        pipelineStatistics.length > 0) ||
+      (type === 'pipeline-statistics' &&
+        (pipelineStatistics === undefined ||
+          pipelineStatistics.length === 0 ||
+          pipelineStatistics.length !== Array.from(new Set(pipelineStatistics)).length));
+
+    t.expectValidationError(() => {
+      t.device.createQuerySet({ type, count, pipelineStatistics });
+    }, shouldError);
   });
