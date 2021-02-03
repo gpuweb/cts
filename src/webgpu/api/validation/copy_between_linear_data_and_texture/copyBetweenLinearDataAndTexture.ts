280e44df9c83e9776d3f2b603d392b9624406cd0
Kai Ninomiya <kainino@chromium.org>
2021-02-02 20:18:20 -0800

    Update _dataRelated for new bytesPerRow/rowsPerImage rules

--- src/webgpu/api/validation/copy_between_linear_data_and_texture/copyBetweenLinearDataAndTexture.ts
+++ src/webgpu/api/validation/copy_between_linear_data_and_texture/copyBetweenLinearDataAndTexture.ts
@@ -1,9 +1,11 @@
 import { poptions } from '../../../../common/framework/params_builder.js';
 import { assert } from '../../../../common/framework/util/util.js';
 import { kSizedTextureFormatInfo, SizedTextureFormat } from '../../../capability_info.js';
+import { align } from '../../../util/math.js';
 import { ValidationTest } from '../validation_test.js';
 
-export const kAllTestMethods = [
+type TestMethod = 'WriteTexture' | 'CopyBufferToTexture' | 'CopyTextureToBuffer';
+export const kAllTestMethods: readonly TestMethod[] = [
   'WriteTexture',
   'CopyBufferToTexture',
   'CopyTextureToBuffer',
@@ -16,25 +18,60 @@ export class CopyBetweenLinearDataAndTextureTest extends ValidationTest {
     return (info.bytesPerBlock * copyWidth) / info.blockWidth;
   }
 
-  requiredBytesInCopy(
-    layout: Required<GPUTextureDataLayout>,
+  /**
+   * Validate a copy and compute the number of bytes it needs. If the copy is invalid, computes a
+   * guess assuming `bytesPerRow` and `rowsPerImage` should be optimal.
+   */
+  dataBytesForCopy(
+    layout: GPUTextureDataLayout,
     format: SizedTextureFormat,
-    copyExtent: GPUExtent3DDict
-  ): number {
+    copyExtent: GPUExtent3DDict,
+    { method }: { method: TestMethod }
+  ): { minDataSize: number; valid: boolean } {
     const info = kSizedTextureFormatInfo[format];
-    assert(layout.rowsPerImage % info.blockHeight === 0);
-    assert(copyExtent.height % info.blockHeight === 0);
     assert(copyExtent.width % info.blockWidth === 0);
-    if (copyExtent.width === 0 || copyExtent.height === 0 || copyExtent.depth === 0) {
-      return 0;
-    } else {
-      const texelBlockRowsPerImage = layout.rowsPerImage / info.blockHeight;
-      const bytesPerImage = layout.bytesPerRow * texelBlockRowsPerImage;
-      const bytesInLastSlice =
-        layout.bytesPerRow * (copyExtent.height / info.blockHeight - 1) +
-        (copyExtent.width / info.blockWidth) * info.bytesPerBlock;
-      return bytesPerImage * (copyExtent.depth - 1) + bytesInLastSlice;
+    const widthInBlocks = copyExtent.width / info.blockWidth;
+    assert(copyExtent.height % info.blockHeight === 0);
+    const heightInBlocks = copyExtent.height / info.blockHeight;
+    const bytesInLastRow = widthInBlocks * info.bytesPerBlock;
+
+    let valid = true;
+    const offset = layout.offset ?? 0;
+    if (method !== 'WriteTexture') {
+      if (offset % info.bytesPerBlock !== 0) valid = false;
+      if (layout.bytesPerRow && layout.bytesPerRow % 256 !== 0) valid = false;
     }
+    if (layout.bytesPerRow !== undefined && bytesInLastRow > layout.bytesPerRow) valid = false;
+    if (layout.rowsPerImage !== undefined && heightInBlocks > layout.rowsPerImage) valid = false;
+
+    let requiredBytesInCopy = 0;
+    {
+      let { bytesPerRow, rowsPerImage } = layout;
+
+      // If heightInBlocks > 1, layout.bytesPerRow must be specified.
+      if (heightInBlocks > 1 && bytesPerRow === undefined) valid = false;
+      // If copyExtent.depth > 1, layout.bytesPerRow and layout.rowsPerImage must be specified.
+      if (copyExtent.depth > 1 && rowsPerImage === undefined) valid = false;
+      // If specified, layout.bytesPerRow must be greater than or equal to bytesInLastRow.
+      if (bytesPerRow !== undefined && bytesPerRow < bytesInLastRow) valid = false;
+      // If specified, layout.rowsPerImage must be greater than or equal to heightInBlocks.
+      if (rowsPerImage !== undefined && rowsPerImage < heightInBlocks) valid = false;
+
+      bytesPerRow ??= align(info.bytesPerBlock * widthInBlocks, 256);
+      rowsPerImage ??= heightInBlocks;
+
+      if (copyExtent.depth > 1) {
+        const bytesPerImage = bytesPerRow * rowsPerImage;
+        const bytesBeforeLastImage = bytesPerImage * (copyExtent.depth - 1);
+        requiredBytesInCopy += bytesBeforeLastImage;
+      }
+      if (copyExtent.depth > 0) {
+        if (heightInBlocks > 1) requiredBytesInCopy += bytesPerRow * (heightInBlocks - 1);
+        if (heightInBlocks > 0) requiredBytesInCopy += bytesInLastRow;
+      }
+    }
+
+    return { minDataSize: offset + requiredBytesInCopy, valid };
   }
 
   testRun(
@@ -42,11 +79,18 @@ export class CopyBetweenLinearDataAndTextureTest extends ValidationTest {
     textureDataLayout: GPUTextureDataLayout,
     size: GPUExtent3D,
     {
-      dataSize,
       method,
+      dataSize,
       success,
-      submit = false, // If submit is true, the validaton error is expected to come from the submit and encoding should succeed.
-    }: { dataSize: number; method: string; success: boolean; submit?: boolean }
+      submit = false,
+    }: {
+      method: TestMethod;
+      dataSize: number;
+      success: boolean;
+      /** If submit is true, the validaton error is expected to come from the submit and encoding
+       * should succeed. */
+      submit?: boolean;
+    }
   ): void {
     switch (method) {
       case 'WriteTexture': {
