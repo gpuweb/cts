80a4a15c642e365a8ffb1151a94db0171c1e24c0
shrekshao <shrekshao@google.com>
2021-02-18 13:30:31 -0800

    some more tests (indirect?)

--- src/webgpu/api/validation/encoding/cmds/index_access.spec.ts
+++ src/webgpu/api/validation/encoding/cmds/index_access.spec.ts
@@ -138,6 +138,9 @@ g.test('out_of_bounds')
         { indexCount: 6, firstIndex: 10000 }, // firstIndex much larger than the bound
         { indexCount: 7, firstIndex: 0 }, // only indexCount out of bound
         { indexCount: 10000, firstIndex: 0 }, // indexCount much larger than the bound
+        { indexCount: 0xffffffff, firstIndex: 0xffffffff }, // max uint32 value
+        { indexCount: 0xffffffff, firstIndex: 2 }, // max uint32 indexCount and small firstIndex
+        { indexCount: 2, firstIndex: 0xffffffff }, // small indexCount and max uint32 firstIndex
       ] as const)
       .combine(poptions('instanceCount', [1, 10000])) // normal and large instanceCount
   )
