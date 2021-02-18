77e64a027d640689bfc7a8a393e95c9b4562a5be
Yunchao He <yunchao.he@intel.com>
2021-02-17 16:15:10 -0800

    Addressed feedback from Kai

--- src/webgpu/api/validation/createTexture.spec.ts
+++ src/webgpu/api/validation/createTexture.spec.ts
@@ -71,9 +71,11 @@ export const g = makeTestGroup(F);
 g.test('zero_size')
   .desc(
     `Test texture creation with zero or nonzero size of
-   width, height, depthOrArrayLayers and mipLevelCount for every dimension, and representative formats.`
+    width, height, depthOrArrayLayers and mipLevelCount for every dimension, and representative formats.
+    TODO: add tests for depth/stencil format if depth/stencil format can support mipmaps.`
   )
-  .params(
+  // .cases(poptions('dimension', ['1d', '2d', '3d'] as const))
+  .subcases(() =>
     params()
       .combine(poptions('dimension', ['1d', '2d', '3d'] as const))
       .combine(
@@ -85,19 +87,13 @@ g.test('zero_size')
           'mipLevelCount',
         ] as const)
       )
-      .combine(
-        poptions('format', [
-          'rgba8unorm',
-          'rgb10a2unorm',
-          'depth24plus-stencil8',
-          'bc1-rgba-unorm',
-        ] as const)
-      )
+      .combine(poptions('format', ['rgba8unorm', 'rgb10a2unorm', 'bc1-rgba-unorm'] as const))
+      .unless(({ format, dimension }) => format === 'bc1-rgba-unorm' && dimension !== '2d')
   )
   .fn(async t => {
     const { dimension, zeroArgument, format } = t.params;
 
-    const size = dimension === '1d' ? [31, 1, 1] : dimension === '2d' ? [31, 31, 1] : [31, 31, 31];
+    const size = dimension === '1d' ? [32, 1, 1] : dimension === '2d' ? [32, 32, 1] : [32, 32, 32];
     let mipLevelCount = 1;
 
     switch (zeroArgument) {
@@ -136,28 +132,27 @@ g.test('zero_size')
 g.test('mipLevelCount,format')
   .desc(
     `Test texture creation with no mipmap chain, partial mipmap chain, full mipmap chain, out-of-bounds mipmap chain
-    for every format with different texture dimension types.`
+    for every format with different texture dimension types.
+    TODO: test 1D and 3D dimensions. Note that it is invalid for some formats with 1D/3D and/or mipmapping.`
   )
-  .params(
+  .subcases(() =>
     params()
-      .combine(poptions('dimension', ['1d', '2d', '3d'] as const))
       .combine(poptions('format', kAllTextureFormats))
-      .combine(poptions('mipLevelCount', [1, 3, 5, 6]))
+      .combine(poptions('mipLevelCount', [1, 3, 6, 7]))
   )
   .fn(async t => {
-    const { format, dimension, mipLevelCount } = t.params;
-    const size = dimension === '1d' ? [31, 1, 1] : dimension === '2d' ? [31, 31, 1] : [31, 31, 31];
+    const { format, mipLevelCount } = t.params;
+    const size = [32, 32, 1];
     const descriptor = {
       size,
       mipLevelCount,
-      dimension,
       format,
       usage: GPUTextureUsage.SAMPLED,
     };
 
     await t.selectDeviceOrSkipTestCase(kAllTextureFormatInfo[format].extension);
 
-    const success = mipLevelCount <= 5;
+    const success = mipLevelCount <= 6;
     t.expectValidationError(() => {
       t.device.createTexture(descriptor);
     }, !success);
@@ -165,34 +160,37 @@ g.test('mipLevelCount,format')
 
 g.test('mipLevelCount,bound_check')
   .desc(
-    `Test mip level count bound check upon different texture size and different texture dimension types.`
+    `Test mip level count bound check upon different texture size and different texture dimension types.
+    The cases below test: 1) there must be no mip levels after a 1 level (1D texture), or 1x1 level (2D texture), or 1x1x1 level (3D texture), 2) array layers are not mip-mapped, 3) power-of-two, non-power-of-two, and non-square sizes.
+    TODO: test compressed texture`
   )
-  .params([
-    { size: [32, 32], mipLevelCount: 6, _success: true }, // full mip chains for 2D texture are allowed (Mip level sizes: 32x32, 16x32, 8x8, 4x4, 2x2, 1x1)
-    { size: [31, 32], mipLevelCount: 6, _success: true }, // full mip chains for 2D texture are allowed (Mip level sizes: 31x32, 15x16, 7x8, 3x4, 1x2, 1x1)
-    { size: [32, 31], mipLevelCount: 6, _success: true }, // full mip chains for 2D texture are allowed (Mip level sizes: 32x31, 16x15, 8x7, 4x3, 2x1, 1x1)
-    { size: [31, 32], mipLevelCount: 7, _success: false }, // too big mip chains on width for 2D texture are disallowed (Mip level sizes: 31x32, 15x16, 7x8, 3x4, 1x2, 1x1, ?x?)
-    { size: [32, 31], mipLevelCount: 7, _success: false }, // too big mip chains on height for 2D texture are disallowed (Mip level sizes: 32x31, 16x15, 8x7, 4x3, 2x1, 1x1, ?x?)
-    { size: [31, 31], mipLevelCount: 5, _success: true }, // full mip chains for non-power-of-two 2D texture are allowed (Mip level sizes: 31x31, 15x15, 7x7, 3x3, 1x1)
-    { size: [31, 31], mipLevelCount: 6, _success: false }, // too big mip chains for non-power-of-two 2D texture are disallowed (Mip level sizes: 31x31, 15x15, 7x7, 3x3, 1x1, ?x?)
-    { size: [32, 1, 1], dimension: '1d' as const, mipLevelCount: 6, _success: true }, // full mip chains for 1D texture are allowed (Mip level sizes: 32, 16, 8, 4, 2, 1)
-    { size: [32, 1, 1], dimension: '1d' as const, mipLevelCount: 7, _success: false }, // too big mip chains for 1D texture are not allowed (Mip level sizes: 32, 16, 8, 4, 2, 1)
-    { size: [31, 1, 1], dimension: '1d' as const, mipLevelCount: 5, _success: true }, // full mip chains for non-power-of-two 1D texture are allowed (Mip level sizes: 31, 15, 7, 3, 1)
-    { size: [31, 1, 1], dimension: '1d' as const, mipLevelCount: 6, _success: false }, // too big mip chains for non-power-of-two 1D texture are not allowed (Mip level sizes: 31, 15, 7, 3, 1)
-    { size: [32, 32, 32], dimension: '3d' as const, mipLevelCount: 6, _success: true }, // full mip chains for 3D texture are allowed (Mip level sizes: 32x32x32, 16x16x16, 8x8x8, 4x4x4, 2x2x2, 1x1x1)
-    { size: [32, 32, 32], dimension: '3d' as const, mipLevelCount: 7, _success: false }, // too big mip chains for 3D texture are not allowed (Mip level sizes: 32x32x32, 16x16x16, 8x8x8, 4x4x4, 2x2x2, 1x1x1)
-    { size: [31, 31, 31], dimension: '3d' as const, mipLevelCount: 5, _success: true }, // full mip chains for non-power-of-two 3D texture are allowed (Mip level sizes: 31x31x31, 15x15x15, 7x7x7, 3x3x3, 1x1x1)
-    { size: [31, 31, 31], dimension: '3d' as const, mipLevelCount: 6, _success: false }, // too big mip chains for non-power-of-two 3D texture are not allowed (Mip level sizes: 31x31x31, 15x15x15, 7x7x7, 3x3x3, 1x1x1)
+  .subcases(() => [
+    { size: [32, 32], mipLevelCount: 6, _success: true }, // Mip level sizes: 32x32, 16x16, 8x8, 4x4, 2x2, 1x1
+    { size: [31, 32], mipLevelCount: 6, _success: true }, // Mip level sizes: 31x32, 15x16, 7x8, 3x4, 1x2, 1x1
+    { size: [32, 31], mipLevelCount: 6, _success: true }, // Mip level sizes: 32x31, 16x15, 8x7, 4x3, 2x1, 1x1
+    { size: [31, 32], mipLevelCount: 7, _success: false }, // Mip level sizes: 31x32, 15x16, 7x8, 3x4, 1x2, 1x1, ?x?
+    { size: [32, 31], mipLevelCount: 7, _success: false }, // Mip level sizes: 32x31, 16x15, 8x7, 4x3, 2x1, 1x1, ?x?
+    { size: [31, 31], mipLevelCount: 5, _success: true }, // Mip level sizes: 31x31, 15x15, 7x7, 3x3, 1x1
+    { size: [31, 31], mipLevelCount: 6, _success: false }, // Mip level sizes: 31x31, 15x15, 7x7, 3x3, 1x1, ?x?
+    { size: [32], dimension: '1d' as const, mipLevelCount: 6, _success: true }, // Mip level sizes: 32, 16, 8, 4, 2, 1
+    { size: [32], dimension: '1d' as const, mipLevelCount: 7, _success: false }, // Mip level sizes: 32, 16, 8, 4, 2, 1, ?
+    { size: [31], dimension: '1d' as const, mipLevelCount: 5, _success: true }, // Mip level sizes: 31, 15, 7, 3, 1
+    { size: [31], dimension: '1d' as const, mipLevelCount: 6, _success: false }, // Mip level sizes: 31, 15, 7, 3, 1, ?
+    { size: [32, 32, 32], dimension: '3d' as const, mipLevelCount: 6, _success: true }, // Mip level sizes: 32x32x32, 16x16x16, 8x8x8, 4x4x4, 2x2x2, 1x1x1
+    { size: [32, 32, 32], dimension: '3d' as const, mipLevelCount: 7, _success: false }, // Mip level sizes: 32x32x32, 16x16x16, 8x8x8, 4x4x4, 2x2x2, 1x1x1, ?x?x?
+    { size: [32, 31, 31], dimension: '3d' as const, mipLevelCount: 6, _success: true }, // Mip level sizes: 32x31x31, 16x15x15, 8x7x7, 4x3x3, 2x1x1, 1x1x1
+    { size: [32, 31, 31], dimension: '3d' as const, mipLevelCount: 7, _success: false }, // Mip level sizes: 32x31x31, 16x15x15, 8x7x7, 4x3x3, 2x1x1, 1x1x1, ?x?x?
+    { size: [31, 32, 31], dimension: '3d' as const, mipLevelCount: 6, _success: true }, // Mip level sizes: 31x32x31, 15x16x15, 7x8x7, 3x4x3, 1x2x1, 1x1x1
+    { size: [31, 32, 31], dimension: '3d' as const, mipLevelCount: 7, _success: false }, // Mip level sizes: 31x32x31, 15x16x15, 7x8x7, 3x4x3, 1x2x1, 1x1x1, ?x?x?
+    { size: [31, 31, 32], dimension: '3d' as const, mipLevelCount: 6, _success: true }, // Mip level sizes: 31x31x32, 15x15x16, 7x7x8, 3x3x4, 1x1x2, 1x1x1
+    { size: [31, 31, 32], dimension: '3d' as const, mipLevelCount: 7, _success: false }, // Mip level sizes: 31x31x32, 15x15x16, 7x7x8, 3x3x4, 1x1x2, 1x1x1, ?x?x?
+    { size: [31, 31, 31], dimension: '3d' as const, mipLevelCount: 5, _success: true }, // Mip level sizes: 31x31x31, 15x15x15, 7x7x7, 3x3x3, 1x1x1
+    { size: [31, 31, 31], dimension: '3d' as const, mipLevelCount: 6, _success: false }, // Mip level sizes: 31x31x31, 15x15x15, 7x7x7, 3x3x3, 1x1x1, ?x?x?
     { size: [32, 32], mipLevelCount: 100, _success: false }, // undefined shift check if miplevel is bigger than the integer bit width
-    { size: [32, 8], mipLevelCount: 6, _success: true }, // non square mip map halves the resolution until a 1x1 dimension. (Mip maps: 32x8, 16x4, 8x2, 4x1, 2x1, 1x1)
-    { size: [32, 8], mipLevelCount: 7, _success: true }, // too big mip chains for non square mip map halves are disallowed (Mip maps: 32x8, 16x4, 8x2, 4x1, 2x1, 1x1)
-    { size: [32, 32, 64], mipLevelCount: 7, _success: false }, // array layer count for 2D texture should not be taken account to calculate mip levels. (Mip maps: 32x32x64, 16x16x64, 8x8x64, 4x4x64, 2x2x64, 1x1x64)
-    {
-      size: [32, 32, 64],
-      dimension: '3d' as const,
-      mipLevelCount: 7,
-      _success: true,
-    }, // depth of 3D texture should be taken account to calculate mip levels. (Mip maps: 32x32x64, 16x16x32, 8x8x16, 4x4x8, 2x2x4, 1x1x2, 1x1x1)
+    { size: [32, 8], mipLevelCount: 6, _success: true }, // Mip levels: 32x8, 16x4, 8x2, 4x1, 2x1, 1x1
+    { size: [32, 8], mipLevelCount: 7, _success: true }, // Mip levels: 32x8, 16x4, 8x2, 4x1, 2x1, 1x1
+    { size: [32, 32, 64], mipLevelCount: 7, _success: false }, // Mip levels: 32x32x64, 16x16x64, 8x8x64, 4x4x64, 2x2x64, 1x1x64, ?x?x64
+    { size: [32, 32, 64], dimension: '3d' as const, mipLevelCount: 7, _success: true }, // Mip levels: 32x32x64, 16x16x32, 8x8x16, 4x4x8, 2x2x4, 1x1x2, 1x1x1
   ])
   .fn(async t => {
     const { size, mipLevelCount, dimension, _success } = t.params;
@@ -212,16 +210,15 @@ g.test('mipLevelCount,bound_check')
 
 g.test('sampleCount')
   .params([
-    // TODO: Consider making a list of "valid"+"invalid" texture descriptors in capability_info.
-    { sampleCount: 0, _success: false }, // sampleCount of 0 is not allowed
-    { sampleCount: 1, _success: true }, // sampleCount of 1 is allowed
-    { sampleCount: 2, _success: false }, // sampleCount of 2 is not allowed
-    { sampleCount: 3, _success: false }, // sampleCount of 3 is not allowed
-    { sampleCount: 4, _success: true }, // sampleCount of 4 is allowed
-    { sampleCount: 8, _success: false }, // sampleCount of 8 is not allowed
-    { sampleCount: 16, _success: false }, // sampleCount of 16 is not allowed
-    { sampleCount: 4, mipLevelCount: 2, _success: false }, // multisampled multi-level is not allowed
-    { sampleCount: 4, arrayLayerCount: 2, _success: false }, // multisampled multi-layer is not allowed
+    { sampleCount: 0, _success: false },
+    { sampleCount: 1, _success: true },
+    { sampleCount: 2, _success: false },
+    { sampleCount: 3, _success: false },
+    { sampleCount: 4, _success: true },
+    { sampleCount: 8, _success: false },
+    { sampleCount: 16, _success: false },
+    { sampleCount: 4, mipLevelCount: 2, _success: false },
+    { sampleCount: 4, arrayLayerCount: 2, _success: false },
   ])
   .fn(async t => {
     const { sampleCount, mipLevelCount, arrayLayerCount, _success } = t.params;
