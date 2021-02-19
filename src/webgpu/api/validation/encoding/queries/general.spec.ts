d021d027e7108739250a1854173dd555d7efc11d
Hao Li <hao.x.li@intel.com>
2021-02-19 16:43:52 +0800

    Add validation tests for Timestamp Query
    - Test writeTimestamp in all possible encoders.
    - Test resolveQuerySet for all types of queries.
    - Test destoryed query set in writeTimestamp and resolveQuerySet.

--- src/webgpu/api/validation/encoding/queries/general.spec.ts
+++ src/webgpu/api/validation/encoding/queries/general.spec.ts
@@ -5,10 +5,100 @@ TODO:
     - queryIndex {in, out of} range for GPUQuerySet
     - GPUQuerySet {valid, invalid}
         - or {undefined}, for occlusionQuerySet
-    - ?
+    - x = {occlusion, pipeline statistics, timestamp} query
 `;
 
+import { params, poptions } from '../../../../../common/framework/params_builder.js';
 import { makeTestGroup } from '../../../../../common/framework/test_group.js';
+import { kQueryTypes } from '../../../../capability_info.js';
 import { ValidationTest } from '../../validation_test.js';
 
+async function selectDeviceForQueryType(t: ValidationTest, type: GPUQueryType): Promise<void> {
+  const extensions: GPUExtensionName[] = [];
+  if (type === 'pipeline-statistics') {
+    extensions.push('pipeline-statistics-query');
+  } else if (type === 'timestamp') {
+    extensions.push('timestamp-query');
+  }
+
+  await t.selectDeviceOrSkipTestCase({ extensions });
+}
+
+export const enum EncoderType {
+  CommandEncoder = 'CommandEncoder',
+  ComputeEncoder = 'ComputeEncoder',
+  RenderEncoder = 'RenderEncoder',
+}
+
 export const g = makeTestGroup(ValidationTest);
+
+g.test('writeTimestamp')
+  .desc(
+    `
+Tests that write timestamp to all types of query set on all possible encoders:
+- queryIndex {in, out of} range for GPUQuerySet
+- GPUQuerySet {valid, invalid}
+- x= {occlusion, pipeline statistics, timestamp} query
+- x= {compute, render, non-pass} enconder
+  `
+  )
+  .params(
+    params()
+      .combine(
+        poptions('encoderType', [
+          EncoderType.CommandEncoder,
+          EncoderType.ComputeEncoder,
+          EncoderType.RenderEncoder,
+        ] as const)
+      )
+      .combine(poptions('type', kQueryTypes))
+      .combine(poptions('queryIndex', [0, 1, 2]))
+      .unless(({ type, queryIndex }) => type !== 'timestamp' && queryIndex !== 0)
+  )
+  .fn(async t => {
+    const { encoderType, type, queryIndex } = t.params;
+
+    await selectDeviceForQueryType(t, type);
+
+    const count = 2;
+    const pipelineStatistics =
+      type === 'pipeline-statistics' ? (['clipper-invocations'] as const) : ([] as const);
+    const querySet = t.device.createQuerySet({ type, count, pipelineStatistics });
+
+    const encoder = t.device.createCommandEncoder();
+
+    switch (encoderType) {
+      case EncoderType.CommandEncoder: {
+        encoder.writeTimestamp(querySet, queryIndex);
+        break;
+      }
+      case EncoderType.ComputeEncoder: {
+        const pass = encoder.beginComputePass();
+        pass.writeTimestamp(querySet, queryIndex);
+        pass.endPass();
+        break;
+      }
+      case EncoderType.RenderEncoder: {
+        const colorAttachment = t.device.createTexture({
+          format: 'rgba8unorm',
+          size: { width: 4, height: 4, depth: 1 },
+          usage: GPUTextureUsage.RENDER_ATTACHMENT,
+        });
+        const pass = encoder.beginRenderPass({
+          colorAttachments: [
+            {
+              attachment: colorAttachment.createView(),
+              loadValue: { r: 1.0, g: 1.0, b: 1.0, a: 1.0 },
+            },
+          ],
+        });
+        pass.writeTimestamp(querySet, queryIndex);
+        pass.endPass();
+        break;
+      }
+    }
+
+    t.expectValidationError(() => {
+      encoder.finish();
+    }, type !== 'timestamp' || queryIndex >= count);
+  });
--- src/webgpu/api/validation/queue/destroyed/query_set.spec.ts
+++ src/webgpu/api/validation/queue/destroyed/query_set.spec.ts
@@ -9,7 +9,107 @@ TODO: implement. (Search for other places some of these cases may have already b
 Consider whether these tests should be distributed throughout the suite, instead of centralized.
 `;
 
+import { params, pbool, poptions } from '../../../../../common/framework/params_builder.js';
 import { makeTestGroup } from '../../../../../common/framework/test_group.js';
 import { ValidationTest } from '../../validation_test.js';
 
+export const enum EncoderType {
+  CommandEncoder = 'CommandEncoder',
+  ComputeEncoder = 'ComputeEncoder',
+  RenderEncoder = 'RenderEncoder',
+}
+
 export const g = makeTestGroup(ValidationTest);
+
+g.test('writeTimestamp')
+  .desc(
+    `
+Tests that use a destroyed query set in writeTimestamp on {compute, render, non-pass} encoder.
+- x= {destroyed, not destroyed (control case)}
+  `
+  )
+  .params(
+    params()
+      .combine(
+        poptions('encoderType', [
+          EncoderType.CommandEncoder,
+          EncoderType.ComputeEncoder,
+          EncoderType.RenderEncoder,
+        ] as const)
+      )
+      .combine(pbool('destroyed'))
+  )
+  .fn(async t => {
+    await t.selectDeviceOrSkipTestCase({ extensions: ['timestamp-query'] });
+
+    const querySet = t.device.createQuerySet({ type: 'timestamp', count: 2 });
+
+    if (t.params.destroyed) {
+      querySet.destroy();
+    }
+
+    const encoder = t.device.createCommandEncoder();
+
+    switch (t.params.encoderType) {
+      case EncoderType.CommandEncoder: {
+        encoder.writeTimestamp(querySet, 0);
+        break;
+      }
+      case EncoderType.ComputeEncoder: {
+        const pass = encoder.beginComputePass();
+        pass.writeTimestamp(querySet, 0);
+        pass.endPass();
+        break;
+      }
+      case EncoderType.RenderEncoder: {
+        const colorAttachment = t.device.createTexture({
+          format: 'rgba8unorm',
+          size: { width: 4, height: 4, depth: 1 },
+          usage: GPUTextureUsage.RENDER_ATTACHMENT,
+        });
+        const pass = encoder.beginRenderPass({
+          colorAttachments: [
+            {
+              attachment: colorAttachment.createView(),
+              loadValue: { r: 1.0, g: 1.0, b: 1.0, a: 1.0 },
+            },
+          ],
+        });
+        pass.writeTimestamp(querySet, 0);
+        pass.endPass();
+        break;
+      }
+    }
+
+    t.expectValidationError(() => {
+      t.queue.submit([encoder.finish()]);
+    }, t.params.destroyed);
+  });
+
+g.test('resolveQuerySet')
+  .desc(
+    `
+Tests that use a destroyed query set in resolveQuerySet.
+- x= {destroyed, not destroyed (control case)}
+  `
+  )
+  .params(pbool('destroyed'))
+  .fn(async t => {
+    const querySet = t.device.createQuerySet({
+      type: 'occlusion',
+      count: 1,
+    });
+
+    if (t.params.destroyed) {
+      querySet.destroy();
+    }
+
+    const buffer = t.device.createBuffer({ size: 8, usage: GPUBufferUsage.QUERY_RESOLVE });
+
+    const encoder = t.device.createCommandEncoder();
+    encoder.resolveQuerySet(querySet, 0, 1, buffer, 0);
+
+    t.expectValidationError(() => {
+      t.queue.submit([encoder.finish()]);
+    }, t.params.destroyed);
+  });
