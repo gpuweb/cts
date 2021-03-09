c3ede591a24d82adaafbbd3c0e289ec3423914f8
Yunchao He <yunchao.he@intel.com>
2021-03-08 23:13:48 -0800

    Remove unneeded code

--- src/webgpu/api/validation/createTexture.spec.ts
+++ src/webgpu/api/validation/createTexture.spec.ts
@@ -29,52 +29,6 @@ import { maxMipLevelCount } from '../../util/texture/base.js';
 
 import { ValidationTest } from './validation_test.js';
 
-/* const kValidTextureFormatsForMSAA = [
-  'r8unorm',
-  'r8snorm',
-  'r8uint',
-  'r8sint',
-  'r16uint',
-  'r16sint',
-  'r16float',
-  'rg8unorm',
-  'rg8snorm',
-  'rg8uint',
-  'rg8sint',
-  'r32uint',
-  'r32sint',
-  'r32float',
-  'rg16uint',
-  'rg16sint',
-  'rg16float',
-  'rgba8unorm',
-  'rgba8unorm-srgb',
-  'rgba8snorm',
-  'rgba8uint',
-  'rgba8sint',
-  'bgra8unorm',
-  'bgra8unorm-srgb',
-  'rgb9e5ufloat',
-  'rgb10a2unorm',
-  'rg11b10ufloat',
-  'rg32uint',
-  'rg32sint',
-  'rg32float',
-  'rgba16uint',
-  'rgba16sint',
-  'rgba16float',
-  'rgba32uint',
-  'rgba32sint',
-  'rgba32float',
-  'stencil8',
-  'depth16unorm',
-  'depth24plus',
-  'depth24plus-stencil8',
-  'depth32float',
-  'depth24unorm-stencil8',
-  'depth32float-stencil8',
-]; */
-
 class F extends ValidationTest {
   getDescriptor(
     options: {
