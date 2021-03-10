725830c14e3d4fb5c30388069120f54521a15cf2
Kai Ninomiya <kainino@chromium.org>
2021-03-09 16:44:09 -0800

    Remove unnecessary step from wpt runtime (#487)
    * Remove unnecessary step from wpt runtime
    
    * fix warning

--- src/common/runtime/wpt.ts
+++ src/common/runtime/wpt.ts
@@ -35,7 +35,7 @@ setup({
 
   for (const testcase of testcases) {
     const name = testcase.query.toString();
-    const wpt_fn = async (t: WptTestObject) => {
+    const wpt_fn = async () => {
       const [rec, res] = log.record(name);
       if (worker) {
         await worker.run(rec, name);
@@ -43,12 +43,10 @@ setup({
         await testcase.run(rec);
       }
 
-      t.step(() => {
-        // Unfortunately, it seems not possible to surface any logs for warn/skip.
-        if (res.status === 'fail') {
-          throw (res.logs || []).map(s => s.toJSON()).join('\n\n');
-        }
-      });
+      // Unfortunately, it seems not possible to surface any logs for warn/skip.
+      if (res.status === 'fail') {
+        throw (res.logs || []).map(s => s.toJSON()).join('\n\n');
+      }
     };
 
     promise_test(wpt_fn, name);
