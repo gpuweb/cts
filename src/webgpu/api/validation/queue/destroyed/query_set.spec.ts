b01b954211ce50b9fe7ea50b3145b8d3fbd82e0c
Hao Li <hao.x.li@intel.com>
2021-03-25 17:04:35 +0800

    Simple the TODO in query_set.spec.ts

--- src/webgpu/api/validation/queue/destroyed/query_set.spec.ts
+++ src/webgpu/api/validation/queue/destroyed/query_set.spec.ts
@@ -1,11 +1,7 @@
 export const description = `
 Tests using a destroyed query set on a queue.
 
-- used in pipeline statistics {compute, render}
-- x= {destroyed, not destroyed (control case)}
-
-TODO: implement. (Search for other places some of these cases may have already been tested.)
-Consider whether these tests should be distributed throughout the suite, instead of centralized.
+TODO: Test with pipeline statistics queries on {compute, render} as well.
 `;
 
 import { poptions } from '../../../../../common/framework/params_builder.js';
