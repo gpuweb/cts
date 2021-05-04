d748d570e4acc36893f4ee1f8f7bc1999e624bd5
Brandon Jones <tojiro@gmail.com>
2021-05-04 16:09:00 -0700

    Review createBindGroupLayout spec
     - Added descriptions to all tests
     - Updated BindGroupLayout entry format to match latest spec
     - Split some tests into multiple cases

--- src/webgpu/api/validation/createBindGroupLayout.spec.ts
+++ src/webgpu/api/validation/createBindGroupLayout.spec.ts
@@ -1,7 +1,7 @@
 export const description = `
 createBindGroupLayout validation tests.
 
-TODO: review existing tests, write descriptions, and make sure tests are complete.
+TODO: make sure tests are complete.
 `;
 
 import { poptions, params } from '../../../common/framework/params_builder.js';
@@ -26,27 +26,35 @@ function clone<T extends GPUBindGroupLayoutDescriptor>(descriptor: T): T {
 
 export const g = makeTestGroup(ValidationTest);
 
-g.test('some_binding_index_was_specified_more_than_once').fn(async t => {
-  const goodDescriptor = {
-    entries: [
-      { binding: 0, visibility: GPUShaderStage.COMPUTE, type: 'storage-buffer' as const },
-      { binding: 1, visibility: GPUShaderStage.COMPUTE, type: 'storage-buffer' as const },
-    ],
-  };
+g.test('some_binding_index_was_specified_more_than_once')
+  .desc('Test that uniqueness of binding numbers across entries is enforced.')
+  .fn(async t => {
+    const goodDescriptor = {
+      entries: [
+        { binding: 0, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'storage' as const } },
+        { binding: 1, visibility: GPUShaderStage.COMPUTE, buffer: { type: 'storage' as const } },
+      ],
+    };
 
-  // Control case
-  t.device.createBindGroupLayout(goodDescriptor);
+    // Control case
+    t.device.createBindGroupLayout(goodDescriptor);
 
-  const badDescriptor = clone(goodDescriptor);
-  badDescriptor.entries[1].binding = 0;
+    const badDescriptor = clone(goodDescriptor);
+    badDescriptor.entries[1].binding = 0;
 
-  // Binding index 0 can't be specified twice.
-  t.expectValidationError(() => {
-    t.device.createBindGroupLayout(badDescriptor);
+    // Binding index 0 can't be specified twice.
+    t.expectValidationError(() => {
+      t.device.createBindGroupLayout(badDescriptor);
+    });
   });
-});
 
 g.test('visibility')
+  .desc(
+    `
+    Test that only the appropriate combinations of visibilities are allowed for each resource type.
+    - Test each possible combination of shader stage visibilities.
+    - Test each type of bind group resource.`
+  )
   .cases(poptions('visibility', kShaderStageCombinations))
   .subcases(() => poptions('entry', allBindingEntries(false)))
   .fn(async t => {
@@ -63,7 +71,8 @@ g.test('visibility')
   });
 
 g.test('multisample_requires_2d_view_dimension')
-  .params(params().combine(poptions('viewDimension', [undefined, ...kTextureViewDimensions])))
+  .desc('Test that multisampling is only allowed with "2d" view dimensions.')
+  .subcases(() => poptions('viewDimension', [undefined, ...kTextureViewDimensions]))
   .fn(async t => {
     const { viewDimension } = t.params;
 
@@ -84,9 +93,11 @@ g.test('multisample_requires_2d_view_dimension')
 
 g.test('number_of_dynamic_buffers_exceeds_the_maximum_value')
   .desc(
-    `TODO: describe
-
-TODO(#230): Update to enforce per-stage and per-pipeline-layout limits on BGLs as well.`
+    `
+    Test that limits on the maximum number of dynamic buffers are enforced.
+    - Test creation of a bind group layout using the maximum number of dynamic buffers works.
+    - Test creation of a bind group layout using the maximum number of dynamic buffers + 1 fails.
+    - TODO(#230): Update to enforce per-stage and per-pipeline-layout limits on BGLs as well.`
   )
   .cases(poptions('type', kBufferBindingTypes))
   .fn(async t => {
@@ -153,26 +164,32 @@ function* pickExtraBindingTypesForPerStage(
   }
 }
 
-const kCasesForMaxResourcesPerStageTests = params()
-  .combine(poptions('maxedEntry', allBindingEntries(false)))
-  .combine(poptions('maxedVisibility', kShaderStages))
-  .filter(p => (bindingTypeInfo(p.maxedEntry).validStages & p.maxedVisibility) !== 0)
-  .expand(function* (p) {
-    yield* poptions('extraEntry', pickExtraBindingTypesForPerStage(p.maxedEntry, true));
-    yield* poptions('extraEntry', pickExtraBindingTypesForPerStage(p.maxedEntry, false));
-  })
-  .combine(poptions('extraVisibility', kShaderStages))
-  .filter(p => (bindingTypeInfo(p.extraEntry).validStages & p.extraVisibility) !== 0);
+function subcasesForMaxResourcesPerStageTests(caseParams: { maxedEntry: BGLEntry }) {
+  return params()
+    .combine(poptions('maxedVisibility', kShaderStages))
+    .filter(p => (bindingTypeInfo(caseParams.maxedEntry).validStages & p.maxedVisibility) !== 0)
+    .expand(function* () {
+      yield* poptions('extraEntry', pickExtraBindingTypesForPerStage(caseParams.maxedEntry, true));
+      yield* poptions('extraEntry', pickExtraBindingTypesForPerStage(caseParams.maxedEntry, false));
+    })
+    .combine(poptions('extraVisibility', kShaderStages))
+    .filter(p => (bindingTypeInfo(p.extraEntry).validStages & p.extraVisibility) !== 0);
+}
 
 // Should never fail unless kMaxBindingsPerBindGroup is exceeded, because the validation for
 // resources-of-type-per-stage is in pipeline layout creation.
 g.test('max_resources_per_stage,in_bind_group_layout')
   .desc(
-    `TODO: describe
-
-TODO(#230): Update to enforce per-stage and per-pipeline-layout limits on BGLs as well.`
+    `
+    Test that the maximum number of bindings of a given type per-stage cannot be exceeded in a
+    single bind group layout.
+    - Test each binding type.
+    - Test that creation of a bind group layout using the maximum number of bindings works.
+    - Test that creation of a bind group layout using the maximum number of bindings + 1 fails.
+    - TODO(#230): Update to enforce per-stage and per-pipeline-layout limits on BGLs as well.`
   )
-  .params(kCasesForMaxResourcesPerStageTests)
+  .cases(poptions('maxedEntry', allBindingEntries(false)))
+  .subcases(cp => subcasesForMaxResourcesPerStageTests(cp))
   .fn(async t => {
     const { maxedEntry, extraEntry, maxedVisibility, extraVisibility } = t.params;
     const maxedTypeInfo = bindingTypeInfo(maxedEntry);
@@ -210,7 +227,17 @@ TODO(#230): Update to enforce per-stage and per-pipeline-layout limits on BGLs a
 // different for each type). Test that the max works, then add one more binding of same-or-different
 // type and same-or-different visibility.
 g.test('max_resources_per_stage,in_pipeline_layout')
-  .params(kCasesForMaxResourcesPerStageTests)
+  .desc(
+    `
+    Test that the maximum number of bindings of a given type per-stage cannot be exceeded across
+    multiple bind group layouts when creating a pipeline layout.
+    - Test each binding type.
+    - Test that creation of a pipeline using the maximum number of bindings works.
+    - Test that creation of a pipeline using the maximum number of bindings + 1 fails.
+  `
+  )
+  .cases(poptions('maxedEntry', allBindingEntries(false)))
+  .subcases(cp => subcasesForMaxResourcesPerStageTests(cp))
   .fn(async t => {
     const { maxedEntry, extraEntry, maxedVisibility, extraVisibility } = t.params;
     const maxedTypeInfo = bindingTypeInfo(maxedEntry);
