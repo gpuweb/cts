3e18076310381d02e2f37c757455e8484dccf979
James Price <jrprice@google.com>
2021-06-10 11:47:36 -0400

    Remove expected error codes from WGSL validation tests
    These have been removed from the WGSL specification. The code for
    checking for a specific error substring has been left in for now, in
    case we decide to revisit standardized error codes in the future.

--- src/webgpu/shader/validation/variable_and_const.spec.ts
+++ src/webgpu/shader/validation/variable_and_const.spec.ts
@@ -63,9 +63,9 @@ function getType(scalarType: ScalarType, containerType: ContainerType) {
   return type;
 }
 
-g.test('v_0033')
+g.test('initializer_type')
   .desc(
-    `Tests for validation rule v-0033:
+    `
   If present, the initializer's type must match the store type of the variable.
   Testing scalars, vectors, and matrices of every dimension and type.
   TODO: add test for: structs - arrays of vectors and matrices - arrays of different length
@@ -98,14 +98,13 @@ g.test('v_0033')
       }
     `;
 
-    const expectation =
-      (lhsScalarType === rhsScalarType && lhsContainerType === rhsContainerType) || 'v-0033';
+    const expectation = lhsScalarType === rhsScalarType && lhsContainerType === rhsContainerType;
     t.expectCompileResult(expectation, code);
   });
 
-g.test('v_0038')
+g.test('io_shareable_type')
   .desc(
-    `Tests for validation rule v-0038:
+    `
   The following types are IO-shareable:
   - numeric scalar types
   - numeric vector types
@@ -168,6 +167,6 @@ g.test('v_0038')
       `;
     }
 
-    const expectation = storageClass === 'private' || scalarType !== 'bool' || 'v-0038';
+    const expectation = storageClass === 'private' || scalarType !== 'bool';
     t.expectCompileResult(expectation, code);
   });
