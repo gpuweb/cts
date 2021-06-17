7492ae4e61db436344d95a503e13c16d1bcc1103
Ben Clayton <bclayton@google.com>
2021-06-17 10:39:17 +0100

    wgsl: Fixes for new validation failures
    Values of array and matrix can now only be indexed by a constant value.
    Dynamic indexing requires the value to be held in memory.
    
    See: https://github.com/gpuweb/gpuweb/issues/1782

--- src/webgpu/api/operation/render_pipeline/culling_tests.spec.ts
+++ src/webgpu/api/operation/render_pipeline/culling_tests.spec.ts
@@ -103,7 +103,7 @@ g.test('culling')
               [[stage(vertex)]] fn main(
                 [[builtin(vertex_index)]] VertexIndex : i32
                 ) -> [[builtin(position)]] vec4<f32> {
-                let pos : array<vec2<f32>, 6> = array<vec2<f32>, 6>(
+                var pos : array<vec2<f32>, 6> = array<vec2<f32>, 6>(
                     vec2<f32>(-1.0,  1.0),
                     vec2<f32>(-1.0,  0.0),
                     vec2<f32>( 0.0,  1.0),
