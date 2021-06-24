629340e22add52e9ff89b5358eb0e220a7843288
Ben Clayton <bclayton@google.com>
2021-06-24 14:17:55 +0100

    wgsl: Fix datatype of [[builtin(vertex_index)]] (#596)
    This has to be `u32`. See https://www.w3.org/TR/WGSL/#builtin-variables

--- src/webgpu/api/validation/encoding/programmable/pipeline_bind_group_compat.spec.ts
+++ src/webgpu/api/validation/encoding/programmable/pipeline_bind_group_compat.spec.ts
@@ -35,7 +35,7 @@ class F extends ValidationTest {
             [[group(0), binding(0)]] var<uniform> uniforms : VertexUniforms;
 
             [[stage(vertex)]] fn main(
-              [[builtin(vertex_index)]] VertexIndex : i32
+              [[builtin(vertex_index)]] VertexIndex : u32
               ) -> [[builtin(position)]] vec4<f32> {
               var pos : array<vec2<f32>, 3> = array<vec2<f32>, 3>(
                 vec2<f32>(-1.0, -1.0),
