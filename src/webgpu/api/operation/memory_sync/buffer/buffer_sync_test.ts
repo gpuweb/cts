7f85888bb1ecda7b46f3768fce841a4332c36815
Corentin Wallez <corentin@wallez.net>
2021-06-30 12:17:36 +0200

    wgsl: add workgroup_size to all stage(compute) (#608)
    The WebGPU spec now requires that this decoration be always present for
    compute stage.

--- src/webgpu/api/operation/memory_sync/buffer/buffer_sync_test.ts
+++ src/webgpu/api/operation/memory_sync/buffer/buffer_sync_test.ts
@@ -58,7 +58,7 @@ export class BufferSyncTest extends GPUTest {
       };
 
       [[group(0), binding(0)]] var<storage, read_write> data : Data;
-      [[stage(compute)]] fn main() {
+      [[stage(compute), workgroup_size(1)]] fn main() {
         data.a = ${value};
         return;
       }
