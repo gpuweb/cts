2448a49391a4ab2f97dcedee25e2edc9d2d7d7dc
Li Hao <hao.x.li@intel.com>
2021-07-02 18:30:13 +0800

    Address Kai's comments

--- src/webgpu/api/validation/encoding/cmds/render/indirect_draw.spec.ts
+++ src/webgpu/api/validation/encoding/cmds/render/indirect_draw.spec.ts
@@ -2,16 +2,15 @@ export const description = `
 Validation tests for drawIndirect/drawIndexedIndirect on render pass and render bundle.
 `;
 
-import { kUnitCaseParamsBuilder } from '../../../../../../common/framework/params_builder.js';
 import { makeTestGroup } from '../../../../../../common/framework/test_group.js';
 import { GPUConst } from '../../../../../constants.js';
-import { ValidationTest, kRenderEncodeTypes } from '../../../validation_test.js';
+import { ValidationTest } from '../../../validation_test.js';
+
+import { kRenderEncodeTypeParams, kBufferStates } from './render.js';
+
+const kIndirectDrawTestParams = kRenderEncodeTypeParams.combine('indexed', [true, false] as const);
 
 export const g = makeTestGroup(ValidationTest);
-
-const kTestParams = kUnitCaseParamsBuilder
-  .combine('encoder', kRenderEncodeTypes)
-  .combine('indexed', [true, false] as const);
 
 g.test('indirect_buffer')
   .desc(
@@ -19,7 +18,7 @@ g.test('indirect_buffer')
 Tests indirect buffer must be valid.
   `
   )
-  .paramsSubcasesOnly(kTestParams.combine('state', ['valid', 'invalid', 'destroyed'] as const))
+  .paramsSubcasesOnly(kIndirectDrawTestParams.combine('state', kBufferStates))
   .unimplemented();
 
 g.test('indirect_buffer_usage')
@@ -29,7 +28,7 @@ Tests indirect buffer must have 'Indirect' usage.
   `
   )
   .paramsSubcasesOnly(
-    kTestParams.combine('usage', [
+    kIndirectDrawTestParams.combine('usage', [
       GPUConst.BufferUsage.INDIRECT, // control case
       GPUConst.BufferUsage.COPY_DST,
       GPUConst.BufferUsage.COPY_DST | GPUConst.BufferUsage.INDIRECT,
@@ -43,7 +42,7 @@ g.test('indirect_offset_alignment')
 Tests indirect offset must be a multiple of 4.
   `
   )
-  .paramsSubcasesOnly(kTestParams.combine('offset', [0, 2, 4] as const))
+  .paramsSubcasesOnly(kIndirectDrawTestParams.combine('offset', [0, 2, 4] as const))
   .unimplemented();
 
 g.test('indirect_offset_oob')
@@ -52,30 +51,34 @@ g.test('indirect_offset_oob')
 Tests indirect draw calls with various indirect offsets and buffer sizes.
 - (offset, b.size) is
   - (0, 0)
-  - (0, min size - min alignment)
-  - (0, min size - 1)
   - (0, min size) (control case)
+  - (0, min size + 1) (control case)
+  - (0, min size - 1)
+  - (0, min size - min alignment)
   - (min alignment, min size + min alignment)
-  - (min alignment, min alignment + min size - 1)
+  - (min alignment, min size + min alignment - 1)
   - (min alignment +/- 1, min size + alignment)
   - (min size, min size)
   - (min size + min alignment, min size)
+  - min size = indirect draw parameters size
+  - x =(drawIndirect, drawIndexedIndirect)
   `
   )
   .paramsSubcasesOnly(
-    kTestParams.expandWithParams(p => {
-      const minSize = p.indexed ? 20 : 16;
+    kIndirectDrawTestParams.expandWithParams(p => {
+      const indirectParamsSize = p.indexed ? 20 : 16;
       return [
         { indirectOffset: 0, bufferSize: 0 },
-        { indirectOffset: 0, bufferSize: minSize },
-        { indirectOffset: 0, bufferSize: minSize - 1 },
-        { indirectOffset: 0, bufferSize: minSize - 4 },
-        { indirectOffset: 4, bufferSize: minSize + 4 },
-        { indirectOffset: 4, bufferSize: minSize + 3 },
-        { indirectOffset: 3, bufferSize: minSize + 4 },
-        { indirectOffset: 5, bufferSize: minSize + 4 },
-        { indirectOffset: minSize, bufferSize: minSize },
-        { indirectOffset: minSize + 4, bufferSize: minSize },
+        { indirectOffset: 0, bufferSize: indirectParamsSize },
+        { indirectOffset: 0, bufferSize: indirectParamsSize + 1 },
+        { indirectOffset: 0, bufferSize: indirectParamsSize - 1 },
+        { indirectOffset: 0, bufferSize: indirectParamsSize - 4 },
+        { indirectOffset: 4, bufferSize: indirectParamsSize + 4 },
+        { indirectOffset: 4, bufferSize: indirectParamsSize + 3 },
+        { indirectOffset: 3, bufferSize: indirectParamsSize + 4 },
+        { indirectOffset: 5, bufferSize: indirectParamsSize + 4 },
+        { indirectOffset: indirectParamsSize, bufferSize: indirectParamsSize },
+        { indirectOffset: indirectParamsSize + 4, bufferSize: indirectParamsSize },
       ] as const;
     })
   )
--- src/webgpu/api/validation/encoding/cmds/render/setIndexBuffer.spec.ts
+++ src/webgpu/api/validation/encoding/cmds/render/setIndexBuffer.spec.ts
@@ -4,7 +4,13 @@ Validation tests for setIndexBuffer on render pass and render bundle.
 
 import { makeTestGroup } from '../../../../../../common/framework/test_group.js';
 import { GPUConst } from '../../../../../constants.js';
-import { ValidationTest, kRenderEncodeTypes } from '../../../validation_test.js';
+import { ValidationTest } from '../../../validation_test.js';
+
+import {
+  kRenderEncodeTypeParams,
+  kBufferStates,
+  buildBufferOffsetAndSizeOOBTestParams,
+} from './render.js';
 
 export const g = makeTestGroup(ValidationTest);
 
@@ -14,11 +20,7 @@ g.test('index_buffer')
 Tests index buffer must be valid.
   `
   )
-  .paramsSubcasesOnly(u =>
-    u
-      .combine('encoder', kRenderEncodeTypes)
-      .combine('state', ['valid', 'invalid', 'destroyed'] as const)
-  )
+  .paramsSubcasesOnly(kRenderEncodeTypeParams.combine('state', kBufferStates))
   .unimplemented();
 
 g.test('index_buffer_usage')
@@ -27,8 +29,8 @@ g.test('index_buffer_usage')
 Tests index buffer must have 'Index' usage.
   `
   )
-  .paramsSubcasesOnly(u =>
-    u.combine('encoder', kRenderEncodeTypes).combine('usage', [
+  .paramsSubcasesOnly(
+    kRenderEncodeTypeParams.combine('usage', [
       GPUConst.BufferUsage.INDEX, // control case
       GPUConst.BufferUsage.COPY_DST,
       GPUConst.BufferUsage.COPY_DST | GPUConst.BufferUsage.INDEX,
@@ -42,9 +44,8 @@ g.test('offset_alignment')
 Tests offset must be a multiple of index format’s byte size.
   `
   )
-  .paramsSubcasesOnly(u =>
-    u
-      .combine('encoder', kRenderEncodeTypes)
+  .paramsSubcasesOnly(
+    kRenderEncodeTypeParams
       .combine('indexFormat', ['uint16', 'uint32'] as const)
       .expand('offset', p => {
         return p.indexFormat === 'uint16' ? ([0, 1, 2] as const) : ([0, 2, 4] as const);
@@ -55,42 +56,8 @@ Tests offset must be a multiple of index format’s byte size.
 g.test('offset_and_size_oob')
   .desc(
     `
-Tests offset and size cannot be larger than index buffer size
-- (offset, size) is
-  - (0, 1)
-  - (0, min size) (control case)
-  - (0, min size + 1)
-  - (0, buffer.size) (control case)
-  - (0, buffer.size + 4)
-  - (min alignment, buffer.size)
-  - (min alignment, buffer.size - min alignment)
-  - (buffer.size - min size, min size)
-  - (buffer.size, min size)
-  - (0, 0)
-  - (min alignment, 0) (control case)
-  - (buffer.size - 4, 0)
-  - (buffer.size, 0)
-  - (buffer.size + 4, 0)
+Tests offset and size cannot be larger than index buffer size.
   `
   )
-  .paramsSubcasesOnly(u =>
-    u.combine('encoder', kRenderEncodeTypes).combineWithParams([
-      // Explicit size
-      { offset: 0, size: 1 },
-      { offset: 0, size: 4 },
-      { offset: 0, size: 5 },
-      { offset: 0, size: 256 },
-      { offset: 0, size: 260 },
-      { offset: 4, size: 256 },
-      { offset: 4, size: 252 },
-      { offset: 252, size: 4 },
-      { offset: 256, size: 4 },
-      // Implicit size: buffer.size - offset
-      { offset: 0, size: 0 },
-      { offset: 4, size: 0 },
-      { offset: 252, size: 0 },
-      { offset: 256, size: 0 },
-      { offset: 260, size: 0 },
-    ])
-  )
+  .paramsSubcasesOnly(buildBufferOffsetAndSizeOOBTestParams(4, 256))
   .unimplemented();
