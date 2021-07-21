fff4294940c26f5a267b355ef568ccdd2087abfc
Kai Ninomiya <kainino@chromium.org>
2021-07-21 11:19:41 -0700

    lint fix

--- src/webgpu/api/validation/attachment_compatibility.spec.ts
+++ src/webgpu/api/validation/attachment_compatibility.spec.ts
@@ -5,7 +5,7 @@ TODO: Add sparse color attachment compatibility test when defined by specificati
 `;
 
 import { makeTestGroup } from '../../../common/framework/test_group.js';
-import { range, unreachable } from '../../../common/util/util.js';
+import { range } from '../../../common/util/util.js';
 import {
   kRegularTextureFormats,
   kSizedDepthStencilFormats,
