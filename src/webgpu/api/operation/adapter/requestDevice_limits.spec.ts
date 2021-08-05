702a484985aa0df4d1faf011ebdc4be7e838568d
Brandon Jones <tojiro@gmail.com>
2021-08-05 11:16:32 -0700

    Some minor refactoring of cases/subcases

--- src/webgpu/api/operation/adapter/requestDevice_limits.spec.ts
+++ src/webgpu/api/operation/adapter/requestDevice_limits.spec.ts
@@ -2,12 +2,12 @@ export const description = `
 Tests passing various requiredLimits to GPUAdapter.requestDevice.
 `;
 
-import { getGPU } from '../../../util/navigator_gpu.js';
 import { Fixture } from '../../../../common/framework/fixture.js';
 import { makeTestGroup } from '../../../../common/framework/test_group.js';
 import { keysOf } from '../../../../common/util/data_tables.js';
 import { assert } from '../../../../common/util/util.js';
 import { DefaultLimits } from '../../../constants.js';
+import { getGPU } from '../../../util/navigator_gpu.js';
 
 const kLimitTypes = keysOf(DefaultLimits);
 
@@ -34,57 +34,41 @@ g.test('unknown_limits')
     t.shouldReject('OperationError', adapter.requestDevice({ requiredLimits }));
   });
 
-g.test('default_limits')
+g.test('supported_limits')
   .desc(
     `
-    Test that each supported limit can be specified with the default values required by the
-    spec.
-    - Tests each limit`
+    Test that each supported limit can be specified with valid values.
+    - Tests each limit with the default values given by the spec
+    - Tests each limit with the supported values given by the adapter`
   )
-  .paramsSubcasesOnly(u =>
-    u.combine('limit', kLimitTypes).unless(p => typeof DefaultLimits[p.limit] !== 'number')
+  .params(u =>
+    u
+      .combine('limit', kLimitTypes)
+      .unless(p => typeof DefaultLimits[p.limit] !== 'number')
+      .beginSubcases()
+      .combine('limitValue', ['default', 'adapter'])
   )
   .fn(async t => {
-    const { limit } = t.params;
+    const { limit, limitValue } = t.params;
 
     const gpu = getGPU();
     const adapter = await gpu.requestAdapter();
     assert(adapter !== null);
 
-    if (adapter.limits) { return; }
+    let value: number = -1;
+    switch (limitValue) {
+      case 'default':
+        value = DefaultLimits[limit] as number;
+        break;
+      case 'adapter':
+        value = adapter.limits[limit] as number;
+        break;
+    }
 
-    const requiredLimits = { [limit]: DefaultLimits[limit] as number };
-
-    const device = await adapter.requestDevice({ requiredLimits });
+    const device = await adapter.requestDevice({ requiredLimits: { [limit]: value } });
     assert(device !== null);
     t.expect(
-      device.limits[limit] === requiredLimits[limit],
-      'Devices reported limit should match the required limit'
-    );
-  });
-
-g.test('adapter_limits')
-  .desc(
-    `
-    Test that each supported limit can be specified with the adapter's reported values.
-    - Tests each limit`
-  )
-  .paramsSubcasesOnly(u =>
-    u.combine('limit', kLimitTypes).unless(p => typeof DefaultLimits[p.limit] !== 'number')
-  )
-  .fn(async t => {
-    const { limit } = t.params;
-
-    const gpu = getGPU();
-    const adapter = await gpu.requestAdapter();
-    assert(adapter !== null);
-
-    const requiredLimits = { [limit]: adapter.limits[limit] };
-
-    const device = await adapter.requestDevice({ requiredLimits });
-    assert(device !== null);
-    t.expect(
-      device.limits[limit] === requiredLimits[limit],
+      device.limits[limit] === value,
       'Devices reported limit should match the required limit'
     );
   });
@@ -97,11 +81,12 @@ g.test('better_than_supported')
     - Tests each limit
     - Tests requesting better limits by various amounts`
   )
-  .paramsSubcasesOnly(u =>
+  .params(u =>
     u
       .combine('limit', kLimitTypes)
-      .combine('over', [1, 32, 65535])
       .unless(p => typeof DefaultLimits[p.limit] !== 'number')
+      .beginSubcases()
+      .combine('over', [1, 32, 65535])
   )
   .fn(async t => {
     const { limit, over } = t.params;
@@ -113,7 +98,7 @@ g.test('better_than_supported')
     const mult = limit.startsWith('min') ? -1 : 1;
 
     const requiredLimits = {
-      [limit]: clampToUnsignedLong(adapter.limits[limit] + over * mult)
+      [limit]: clampToUnsignedLong(adapter.limits[limit] + over * mult),
     };
 
     t.shouldReject('OperationError', adapter.requestDevice({ requiredLimits }));
@@ -127,11 +112,12 @@ g.test('worse_than_default')
     - Tests each limit
     - Tests requesting worse limits by various amounts`
   )
-  .paramsSubcasesOnly(u =>
+  .params(u =>
     u
       .combine('limit', kLimitTypes)
-      .combine('under', [1, 32, 65535])
       .unless(p => typeof DefaultLimits[p.limit] !== 'number')
+      .beginSubcases()
+      .combine('under', [1, 32, 65535])
   )
   .fn(async t => {
     const { limit, under } = t.params;
@@ -143,7 +129,7 @@ g.test('worse_than_default')
     const mult = limit.startsWith('min') ? -1 : 1;
 
     const requiredLimits = {
-      [limit]: clampToUnsignedLong(DefaultLimits[limit] as number - under * mult)
+      [limit]: clampToUnsignedLong((DefaultLimits[limit] as number) - under * mult),
     };
 
     const device = await adapter.requestDevice({ requiredLimits });
