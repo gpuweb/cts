9b09d514c4037fd7a26931ddea9554fa2a8b989d
shrekshao <shrekshao@google.com>
2021-08-05 18:12:28 -0700

    Expand Ref tests with output formats/ways writing canvas

--- src/webgpu/web_platform/reftests/canvas_complex.html.ts
+++ src/webgpu/web_platform/reftests/canvas_complex.html.ts
@@ -1,17 +1,28 @@
-import { unreachable } from '../../../common/util/util.js';
+import { assert, unreachable } from '../../../common/util/util.js';
 
 import { runRefTest } from './gpu_ref_test.js';
 
 // <canvas> element from html page
 declare const cvs: HTMLCanvasElement;
 
-export function run(format: GPUTextureFormat) {
+type WriteCanvasMethod =
+  | 'copyBufferToTexture'
+  | 'copyTextureToTexture'
+  | 'copyExternalImageToTexture'
+  | 'DrawTextureSample'
+  | 'DrawVertexColor';
+
+export function run(format: GPUTextureFormat, writeCanvasMethod: WriteCanvasMethod) {
   runRefTest(async t => {
-    const ctx = (cvs.getContext('webgpu') as unknown) as GPUCanvasContext;
+    // const ctx = (cvs.getContext('webgpu') as unknown) as GPUCanvasContext;
+    const ctx = cvs.getContext('webgpu');
+    assert(ctx !== null, 'Failed to get WebGPU context from canvas');
 
     switch (format) {
       case 'bgra8unorm':
-      case 'rgba16float':
+      case 'bgra8unorm-srgb':
+      case 'rgba8unorm':
+      case 'rgba8unorm-srgb':
         break;
       default:
         unreachable();
@@ -20,59 +31,308 @@ export function run(format: GPUTextureFormat) {
     ctx.configure({
       device: t.device,
       format,
-      usage: GPUTextureUsage.COPY_DST,
+      usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.RENDER_ATTACHMENT,
     });
 
-    const rows = 2;
-    const bytesPerRow = 256;
-    const buffer = t.device.createBuffer({
-      mappedAtCreation: true,
-      size: rows * bytesPerRow,
-      usage: GPUBufferUsage.COPY_SRC,
-    });
-    const mapping = buffer.getMappedRange();
-    switch (format) {
-      case 'bgra8unorm':
-        {
-          const data = new Uint8Array(mapping);
-          data.set(new Uint8Array([0x00, 0x00, 0x7f, 0xff]), 0); // red
-          data.set(new Uint8Array([0x00, 0x7f, 0x00, 0xff]), 4); // green
-          data.set(new Uint8Array([0x7f, 0x00, 0x00, 0xff]), 256 + 0); // blue
-          data.set(new Uint8Array([0x00, 0x7f, 0x7f, 0xff]), 256 + 4); // yellow
-        }
-        break;
-      case 'rgba16float':
-        {
-          // Untested!
-          const zero = 0x0000;
-          const half = 0x3800;
-          const one = 0x3c00;
-          const data = new DataView(mapping);
-          data.setUint16(0x000, half, false); // red
-          data.setUint16(0x002, zero, false);
-          data.setUint16(0x004, zero, false);
-          data.setUint16(0x008, one, false);
-          data.setUint16(0x010, zero, false); // green
-          data.setUint16(0x020, half, false);
-          data.setUint16(0x040, zero, false);
-          data.setUint16(0x080, one, false);
-          data.setUint16(0x100, zero, false); // blue
-          data.setUint16(0x102, zero, false);
-          data.setUint16(0x104, half, false);
-          data.setUint16(0x108, one, false);
-          data.setUint16(0x110, half, false); // yellow
-          data.setUint16(0x120, half, false);
-          data.setUint16(0x140, zero, false);
-          data.setUint16(0x180, one, false);
-        }
-        break;
+    function copyBufferToTexture() {
+      const rows = 2;
+      const bytesPerRow = 256;
+      const buffer = t.device.createBuffer({
+        mappedAtCreation: true,
+        size: rows * bytesPerRow,
+        usage: GPUBufferUsage.COPY_SRC,
+      });
+      const mapping = buffer.getMappedRange();
+      switch (format) {
+        case 'bgra8unorm':
+          {
+            const data = new Uint8Array(mapping);
+            data.set(new Uint8Array([0x00, 0x00, 0x7f, 0xff]), 0); // red
+            data.set(new Uint8Array([0x00, 0x7f, 0x00, 0xff]), 4); // green
+            data.set(new Uint8Array([0x7f, 0x00, 0x00, 0xff]), 256 + 0); // blue
+            data.set(new Uint8Array([0x00, 0x7f, 0x7f, 0xff]), 256 + 4); // yellow
+          }
+          break;
+        case 'rgba8unorm':
+          {
+            const data = new Uint8Array(mapping);
+            data.set(new Uint8Array([0x7f, 0x00, 0x00, 0xff]), 0); // red
+            data.set(new Uint8Array([0x00, 0x7f, 0x00, 0xff]), 4); // green
+            data.set(new Uint8Array([0x00, 0x00, 0x7f, 0xff]), 256 + 0); // blue
+            data.set(new Uint8Array([0x7f, 0x7f, 0x00, 0xff]), 256 + 4); // yellow
+          }
+          break;
+        case 'bgra8unorm-srgb':
+          {
+            // TODO
+          }
+          break;
+        case 'rgba8unorm-srgb':
+          {
+            // TODO
+          }
+          break;
+      }
+      buffer.unmap();
+
+      const encoder = t.device.createCommandEncoder();
+      encoder.copyBufferToTexture({ buffer, bytesPerRow }, { texture: ctx.getCurrentTexture() }, [
+        2,
+        2,
+        1,
+      ]);
+      t.device.queue.submit([encoder.finish()]);
     }
-    buffer.unmap();
 
-    const texture = ctx.getCurrentTexture();
+    async function getImageBitmapFromFile(): Promise<ImageBitmap> {
+      const img = new Image();
+      img.src = '../../../resources/canvas-complex-2x2.png';
+      await img.decode();
+      return createImageBitmap(img);
+    }
 
-    const encoder = t.device.createCommandEncoder();
-    encoder.copyBufferToTexture({ buffer, bytesPerRow }, { texture }, [2, 2, 1]);
-    t.device.queue.submit([encoder.finish()]);
+    function setupSrcTexture(imageBitmap: ImageBitmap): GPUTexture {
+      const [srcWidth, srcHeight] = [imageBitmap.width, imageBitmap.height];
+      const srcTexture = t.device.createTexture({
+        size: [srcWidth, srcHeight, 1],
+        format,
+        usage:
+          GPUTextureUsage.SAMPLED |
+          GPUTextureUsage.RENDER_ATTACHMENT |
+          GPUTextureUsage.COPY_DST |
+          GPUTextureUsage.COPY_SRC,
+      });
+      t.device.queue.copyExternalImageToTexture({ source: imageBitmap }, { texture: srcTexture }, [
+        imageBitmap.width,
+        imageBitmap.height,
+      ]);
+      return srcTexture;
+    }
+
+    async function copyExternalImageToTexture() {
+      const imageBitmap = await getImageBitmapFromFile();
+      t.device.queue.copyExternalImageToTexture(
+        { source: imageBitmap },
+        { texture: ctx.getCurrentTexture() },
+        [imageBitmap.width, imageBitmap.height]
+      );
+    }
+
+    async function copyTextureToTexture() {
+      const imageBitmap = await getImageBitmapFromFile();
+      const srcTexture = setupSrcTexture(imageBitmap);
+
+      const encoder = t.device.createCommandEncoder();
+      encoder.copyTextureToTexture(
+        { texture: srcTexture, mipLevel: 0, origin: { x: 0, y: 0, z: 0 } },
+        { texture: ctx.getCurrentTexture(), mipLevel: 0, origin: { x: 0, y: 0, z: 0 } },
+        [imageBitmap.width, imageBitmap.height, 1]
+      );
+      t.device.queue.submit([encoder.finish()]);
+    }
+
+    async function DrawTextureSample() {
+      const imageBitmap = await getImageBitmapFromFile();
+      const srcTexture = setupSrcTexture(imageBitmap);
+
+      const pipeline = t.device.createRenderPipeline({
+        vertex: {
+          module: t.device.createShaderModule({
+            code: `
+struct VertexOutput {
+  [[builtin(position)]] Position : vec4<f32>;
+  [[location(0)]] fragUV : vec2<f32>;
+};
+
+[[stage(vertex)]]
+fn main([[builtin(vertex_index)]] VertexIndex : u32) -> VertexOutput {
+  var pos = array<vec2<f32>, 6>(
+      vec2<f32>( 1.0,  1.0),
+      vec2<f32>( 1.0, -1.0),
+      vec2<f32>(-1.0, -1.0),
+      vec2<f32>( 1.0,  1.0),
+      vec2<f32>(-1.0, -1.0),
+      vec2<f32>(-1.0,  1.0));
+
+  var uv = array<vec2<f32>, 6>(
+      vec2<f32>(1.0, 0.0),
+      vec2<f32>(1.0, 1.0),
+      vec2<f32>(0.0, 1.0),
+      vec2<f32>(1.0, 0.0),
+      vec2<f32>(0.0, 1.0),
+      vec2<f32>(0.0, 0.0));
+
+  var output : VertexOutput;
+  output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+  output.fragUV = uv[VertexIndex];
+  return output;
+}
+            `,
+          }),
+          entryPoint: 'main',
+        },
+        fragment: {
+          module: t.device.createShaderModule({
+            code: `
+[[group(0), binding(0)]] var mySampler: sampler;
+[[group(0), binding(1)]] var myTexture: texture_2d<f32>;
+
+[[stage(fragment)]]
+fn main([[location(0)]] fragUV: vec2<f32>) -> [[location(0)]] vec4<f32> {
+  return textureSample(myTexture, mySampler, fragUV);
+}
+            `,
+          }),
+          entryPoint: 'main',
+          targets: [
+            {
+              format,
+            },
+          ],
+        },
+        primitive: {
+          topology: 'triangle-list',
+        },
+      });
+
+      const sampler = t.device.createSampler({
+        magFilter: 'nearest',
+        minFilter: 'nearest',
+      });
+
+      const uniformBindGroup = t.device.createBindGroup({
+        layout: pipeline.getBindGroupLayout(0),
+        entries: [
+          {
+            binding: 0,
+            resource: sampler,
+          },
+          {
+            binding: 1,
+            resource: srcTexture.createView(),
+          },
+        ],
+      });
+
+      const renderPassDescriptor: GPURenderPassDescriptor = {
+        colorAttachments: [
+          {
+            view: ctx.getCurrentTexture().createView(),
+
+            loadValue: { r: 0.5, g: 0.5, b: 0.5, a: 1.0 },
+            storeOp: 'store',
+          },
+        ],
+      };
+
+      const commandEncoder = t.device.createCommandEncoder();
+      const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+      passEncoder.setPipeline(pipeline);
+      passEncoder.setBindGroup(0, uniformBindGroup);
+      passEncoder.draw(6, 1, 0, 0);
+      passEncoder.endPass();
+      t.device.queue.submit([commandEncoder.finish()]);
+    }
+
+    function DrawVertexColor() {
+      const pipeline = t.device.createRenderPipeline({
+        vertex: {
+          module: t.device.createShaderModule({
+            code: `
+struct VertexOutput {
+  [[builtin(position)]] Position : vec4<f32>;
+  [[location(0)]] fragColor : vec4<f32>;
+};
+
+[[stage(vertex)]]
+fn main([[builtin(vertex_index)]] VertexIndex : u32) -> VertexOutput {
+  var pos = array<vec2<f32>, 6>(
+      vec2<f32>( 0.5,  0.5),
+      vec2<f32>( 0.5, -0.5),
+      vec2<f32>(-0.5, -0.5),
+      vec2<f32>( 0.5,  0.5),
+      vec2<f32>(-0.5, -0.5),
+      vec2<f32>(-0.5,  0.5));
+
+  var offset = array<vec2<f32>, 4>(
+    vec2<f32>( -0.5,  0.5),
+    vec2<f32>( 0.5, 0.5),
+    vec2<f32>(-0.5, -0.5),
+    vec2<f32>( 0.5,  -0.5));
+
+  var color = array<vec4<f32>, 4>(
+      vec4<f32>(0.49804, 0.0, 0.0, 1.0),
+      vec4<f32>(0.0, 0.49804, 0.0, 1.0),
+      vec4<f32>(0.0, 0.0, 0.49804, 1.0),
+      vec4<f32>(0.49804, 0.49804, 0.0, 1.0)); // 0.49804 -> 0x7f
+
+  var output : VertexOutput;
+  output.Position = vec4<f32>(pos[VertexIndex % 6u] + offset[VertexIndex / 6u], 0.0, 1.0);
+  output.fragColor = color[VertexIndex / 6u];
+  return output;
+}
+            `,
+          }),
+          entryPoint: 'main',
+        },
+        fragment: {
+          module: t.device.createShaderModule({
+            code: `
+[[stage(fragment)]]
+fn main([[location(0)]] fragColor: vec4<f32>) -> [[location(0)]] vec4<f32> {
+  return fragColor;
+}
+            `,
+          }),
+          entryPoint: 'main',
+          targets: [
+            {
+              format,
+            },
+          ],
+        },
+        primitive: {
+          topology: 'triangle-list',
+        },
+      });
+
+      const renderPassDescriptor: GPURenderPassDescriptor = {
+        colorAttachments: [
+          {
+            view: ctx.getCurrentTexture().createView(),
+
+            loadValue: { r: 0.5, g: 0.5, b: 0.5, a: 1.0 },
+            storeOp: 'store',
+          },
+        ],
+      };
+
+      const commandEncoder = t.device.createCommandEncoder();
+      const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+      passEncoder.setPipeline(pipeline);
+      passEncoder.draw(24, 1, 0, 0);
+      passEncoder.endPass();
+      t.device.queue.submit([commandEncoder.finish()]);
+    }
+
+    switch (writeCanvasMethod) {
+      case 'copyBufferToTexture':
+        copyBufferToTexture();
+        break;
+      case 'copyExternalImageToTexture':
+        await copyExternalImageToTexture();
+        break;
+      case 'copyTextureToTexture':
+        await copyTextureToTexture();
+        break;
+      case 'DrawTextureSample':
+        await DrawTextureSample();
+        break;
+      case 'DrawVertexColor':
+        DrawVertexColor();
+        break;
+      default:
+        unreachable();
+    }
   });
 }
