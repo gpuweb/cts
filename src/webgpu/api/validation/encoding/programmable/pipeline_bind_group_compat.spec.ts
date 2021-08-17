da81a0bba89557c04e0295bc09426186b729b675
Hao Li <hao.x.li@intel.com>
2021-08-17 14:36:23 +0800

    api,validation,encoding,programmable,pipeline_bind_group_compat plan (#697)
    * api,validation,encoding,programmable,pipeline_bind_group_compat plan
    
    Add basic test plan and leave the more detailed compatiable tests of
    binding resources in TODOs.
    
    * Address comments
    
    * Short test name
    
    * Add useU32Array param

--- src/webgpu/api/validation/encoding/programmable/pipeline_bind_group_compat.spec.ts
+++ src/webgpu/api/validation/encoding/programmable/pipeline_bind_group_compat.spec.ts
@@ -1,10 +1,7 @@
 export const description = `
 TODO:
 - test compatibility between bind groups and pipelines
-    - bind groups required by the pipeline layout are required.
-    - bind groups unused by the pipeline layout can be set or not.
-        (Even if e.g. bind groups 0 and 2 are used, but 1 is unused.)
-    - bindGroups[i].layout is "group-equivalent" (value-equal) to pipelineLayout.bgls[i].
+    - the binding resource in bindGroups[i].layout is "group-equivalent" (value-equal) to pipelineLayout.bgls[i].
     - in the test fn, test once without the dispatch/draw (should always be valid) and once with
       the dispatch/draw, to make sure the validation happens in dispatch/draw.
     - x= {dispatch, all draws} (dispatch/draw should be size 0 to make sure validation still happens if no-op)
@@ -14,7 +11,23 @@ TODO: subsume existing test, rewrite fixture as needed.
 `;
 
 import { makeTestGroup } from '../../../../../common/framework/test_group.js';
+import { kShaderStageCombinations } from '../../../../capability_info.js';
+import { GPUConst } from '../../../../constants.js';
+import {
+  ProgrammableEncoderType,
+  kProgrammableEncoderTypes,
+} from '../../util/command_buffer_maker.js';
 import { ValidationTest } from '../../validation_test.js';
+
+function getTestCmds(encoderType: ProgrammableEncoderType): readonly string[] {
+  if (encoderType === 'compute pass') {
+    return ['dispatch', 'dispatchIndirect'] as const;
+  } else {
+    return ['draw', 'drawIndexed', 'drawIndirect', 'drawIndexedIndirect'] as const;
+  }
+}
+
+const kResourceTypes = ['buffer', 'sampler', 'texture', 'storageTexture', 'externalTexture'];
 
 class F extends ValidationTest {
   getUniformBuffer(): GPUBuffer {
@@ -88,6 +101,32 @@ class F extends ValidationTest {
 
 export const g = makeTestGroup(F);
 
+g.test('bind_groups_and_pipeline_layout_mismatch')
+  .desc(
+    `
+    Tests the bind groups must match the requirements of the pipeline layout.
+    - bind groups required by the pipeline layout are required.
+    - bind groups unused by the pipeline layout can be set or not.
+
+    TODO: merge existing tests to this test
+    `
+  )
+  .params(u =>
+    u
+      .combine('encoderType', kProgrammableEncoderTypes)
+      .expand('call', p => getTestCmds(p.encoderType))
+      .beginSubcases()
+      .combineWithParams([
+        { setBindGroup0: true, setUnusedBindGroup1: true, setBindGroup2: true, _success: true },
+        { setBindGroup0: true, setUnusedBindGroup1: false, setBindGroup2: true, _success: true },
+        { setBindGroup0: true, setUnusedBindGroup1: true, setBindGroup2: false, _success: false },
+        { setBindGroup0: false, setUnusedBindGroup1: true, setBindGroup2: true, _success: false },
+        { setBindGroup0: false, setUnusedBindGroup1: false, setBindGroup2: false, _success: false },
+      ])
+      .combine('useU32Array', [false, true])
+  )
+  .unimplemented();
+
 g.test('it_is_invalid_to_draw_in_a_render_pass_with_missing_bind_groups')
   .paramsSubcasesOnly([
     { setBindGroup1: true, setBindGroup2: true, _success: true },
@@ -141,3 +180,60 @@ g.test('it_is_invalid_to_draw_in_a_render_pass_with_missing_bind_groups')
       commandEncoder.finish();
     }, !_success);
   });
+
+g.test('bgl_binding_mismatch')
+  .desc(
+    'Tests the binding number must exist or not exist in both bindGroups[i].layout and pipelineLayout.bgls[i]'
+  )
+  .params(u =>
+    u
+      .combine('encoderType', kProgrammableEncoderTypes)
+      .expand('call', p => getTestCmds(p.encoderType))
+      .beginSubcases()
+      .combineWithParams([
+        { bgBindings: [0, 1, 2], plBindings: [0, 1, 2], _success: true },
+        { bgBindings: [0, 1, 2], plBindings: [0, 1, 3], _success: false },
+        { bgBindings: [0, 2], plBindings: [0, 2], _success: true },
+        { bgBindings: [0, 2], plBindings: [2, 0], _success: true },
+        { bgBindings: [0, 1, 2], plBindings: [0, 1], _success: false },
+        { bgBindings: [0, 1], plBindings: [0, 1, 2], _success: false },
+      ])
+      .combine('useU32Array', [false, true])
+  )
+  .unimplemented();
+
+g.test('bgl_visibility_mismatch')
+  .desc('Tests the visibility in bindGroups[i].layout and pipelineLayout.bgls[i] must be matched')
+  .params(u =>
+    u
+      .combine('encoderType', kProgrammableEncoderTypes)
+      .expand('call', p => getTestCmds(p.encoderType))
+      .beginSubcases()
+      .combine('bgVisibility', kShaderStageCombinations)
+      .expand('plVisibility', p =>
+        p.encoderType === 'compute pass'
+          ? ([GPUConst.ShaderStage.COMPUTE] as const)
+          : ([
+              GPUConst.ShaderStage.VERTEX,
+              GPUConst.ShaderStage.FRAGMENT,
+              GPUConst.ShaderStage.VERTEX | GPUConst.ShaderStage.FRAGMENT,
+            ] as const)
+      )
+      .combine('useU32Array', [false, true])
+  )
+  .unimplemented();
+
+g.test('bgl_resource_type_mismatch')
+  .desc(
+    'Tests the binding resource type in bindGroups[i].layout and pipelineLayout.bgls[i] must be matched'
+  )
+  .params(u =>
+    u
+      .combine('encoderType', kProgrammableEncoderTypes)
+      .expand('call', p => getTestCmds(p.encoderType))
+      .beginSubcases()
+      .combine('bgResourceType', kResourceTypes)
+      .combine('plResourceType', kResourceTypes)
+      .combine('useU32Array', [false, true])
+  )
+  .unimplemented();
