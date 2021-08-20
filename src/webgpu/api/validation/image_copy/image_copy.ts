7a5f33de46c18434be7b96affe4cabe486af3422
Yunchao He <yunchao.he@intel.com>
2021-08-20 11:41:47 -0700

    Revise a comment

--- src/webgpu/api/validation/image_copy/image_copy.ts
+++ src/webgpu/api/validation/image_copy/image_copy.ts
@@ -129,7 +129,7 @@ interface WithFormatAndMethod extends WithFormat {
   method: ImageCopyType;
 }
 
-// This is a helper function used for expanding test parameters for texel block alignment tests on offset
+// This is a helper function used for expanding test parameters for offset alignment, by spec
 export function texelBlockAlignmentTestExpanderForOffset({ format }: WithFormat) {
   const info = kTextureFormatInfo[format];
   if (info.depth || info.stencil) {
