ec53efe2afcceea5f562d6991c7795cd3d18b37c
ben-clayton <bclayton@google.com>
2021-10-19 16:36:16 +0000

    Deploying to gh-pages from @ gpuweb/cts@54bf5c20bf51ab612b01a5bf0d79ab3a6e6ebe2e 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '0fe558118e33f11bca67927490146f2e250bd44f';
+export const version = '54bf5c20bf51ab612b01a5bf0d79ab3a6e6ebe2e';
