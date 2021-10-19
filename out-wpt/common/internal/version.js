31c34ced7381f7939fcdaed8e1ae4ed6a1e23025
ben-clayton <bclayton@google.com>
2021-10-19 17:13:30 +0000

    Deploying to gh-pages from @ gpuweb/cts@85d3fe5c28d0f69e7f0ab0372e0740e12dd1dceb 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '2c347ed772d487d3f88971ffb7ce0c8972924965';
+export const version = '85d3fe5c28d0f69e7f0ab0372e0740e12dd1dceb';
---
+++ out/common/runtime/server.js
@@ -0,0 +1,128 @@
+/**
+* AUTO-GENERATED - DO NOT EDIT. Source: https://github.com/gpuweb/cts
+**/import * as http from 'http';
+
+
+import { DefaultTestFileLoader } from '../internal/file_loader.js';
+import { prettyPrintLog } from '../internal/logging/log_message.js';
+import { Logger } from '../internal/logging/logger.js';
+
+import { parseQuery } from '../internal/query/parseQuery.js';
+
+
+import { setGPUProvider } from '../util/navigator_gpu.js';
+
+import sys from './helper/sys.js';
+
+function usage(rc) {
+  console.log('Usage:');
+  console.log(`  tools/run_${sys.type} [OPTIONS...]`);
+  console.log('Options:');
+  console.log('  --verbose       Print result/log of every test as it runs.');
+  console.log('  --gpu-provider  Path to node module that provides the GPU implementation.');
+  console.log(``);
+  console.log(`Provides an HTTP server used for running tests via an HTTP RPC interface`);
+  console.log(`To run a test, perform an HTTP GET or POST at the URL:`);
+  console.log(`  http://localhost:port/run?<test-name>`);
+  console.log(`To shutdown the server perform an HTTP GET or POST at the URL:`);
+  console.log(`  http://localhost:port/terminate`);
+  return sys.exit(rc);
+}
+
+
+
+
+
+
+if (!sys.existsSync('src/common/runtime/cmdline.ts')) {
+  console.log('Must be run from repository root');
+  usage(1);
+}
+
+let debug = false;
+
+for (let i = 0; i < sys.args.length; ++i) {
+  const a = sys.args[i];
+  if (a.startsWith('-')) {
+    if (a === '--gpu-provider') {
+      const modulePath = sys.args[++i];
+      setGPUProvider(() => require(modulePath).gpu);
+    } else if (a === '--help') {
+      usage(1);
+    } else if (a === '--verbose') {
+      debug = true;
+    }
+  }
+}
+
+(async () => {
+  Logger.globalDebugMode = debug;
+  const log = new Logger();
+  const testcases = allWebGPUTestcases();
+
+  async function allWebGPUTestcases() {
+    const webgpuQuery = parseQuery('webgpu:*');
+    const loader = new DefaultTestFileLoader();
+    const map = new Map();
+    for (const testcase of await loader.loadCases(webgpuQuery)) {
+      const name = testcase.query.toString();
+      map.set(name, testcase);
+    }
+    return map;
+  }
+
+  async function runTestcase(
+  testcase,
+  expectations = [])
+  {
+    const name = testcase.query.toString();
+    const [rec, res] = log.record(name);
+    await testcase.run(rec, expectations);
+    return res;
+  }
+
+  const server = http.createServer(
+  async (request, response) => {
+    if (request.url === undefined) {
+      response.end('invalid url');
+      return;
+    }
+
+    const runPrefix = '/run?';
+    const terminatePrefix = '/terminate';
+
+    if (request.url.startsWith(runPrefix)) {
+      const name = request.url.substr(runPrefix.length);
+      const testcase = (await testcases).get(name);
+      let status = 'fail';
+      let message = '';
+      if (testcase) {
+        const result = await runTestcase(testcase);
+        status = result.status;
+        if (result.logs !== undefined) {
+          message = result.logs.map(log => prettyPrintLog(log)).join('\n');
+        }
+      } else {
+        message = `test case '${name}' not found`;
+      }
+      const res = { status, message };
+      response.end(JSON.stringify(res));
+    } else if (request.url.startsWith(terminatePrefix)) {
+      server.close();
+      sys.exit(1);
+    } else {
+      response.statusCode = 404;
+      response.end('unhandled url request');
+    }
+  });
+
+
+  server.listen(0, () => {
+    const address = server.address();
+    console.log(`Server listening at [[${address.port}]]`);
+  });
+})().catch(ex => {
+  console.error(ex.stack ?? ex.toString());
+  sys.exit(1);
+});
+//# sourceMappingURL=server.js.map
