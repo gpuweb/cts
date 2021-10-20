gpuweb/cts

commit 6866a15e637745d3813e80102b02b6a1e7e8b45e

src/common/util: Fix 5 second stall at end of test run (#785)
The `DeviceHolder.ensureRelease()` method calls `await raceWithRejectOnTimeout()` with a 5 second timeout. For the NodeJS command line test runner, this sets up a `setTimeout()` callback that the Node VM always has to wait on before quitting the process.

To fix this, cancel the `setTimeout()` if the other promise completes first.

src/common/util/util.ts
@@ -104,7 +104,16 @@ export function rejectOnTimeout(ms: number, msg: string): Promise<never> {
  * and otherwise passes the result through.
  */
 export function raceWithRejectOnTimeout<T>(p: Promise<T>, ms: number, msg: string): Promise<T> {
-  return Promise.race([p, rejectOnTimeout(ms, msg)]);
+  // Setup a promise that will reject after `ms` milliseconds. We cancel this timeout when
+  // `p` is finalized, so the JavaScript VM doesn't hang around waiting for the timer to
+  // complete, once the test runner has finished executing the tests.
+  const timeoutPromise = new Promise((_resolve, reject) => {
+    const handle = timeout(() => {
+      reject(new PromiseTimeoutError(msg));
+    }, ms);
+    p.finally(() => clearTimeout(handle));
+  });
+  return Promise.race([p, timeoutPromise]) as Promise<T>;
 }
 
 /**

commit 60424f8f8719c57b3a499e02a00f563196d2477f

common/util: Fix subtle bug introduced in #785
Apparently you cannot use a Promise that has had `.finally()` called on it for other `.then()` /  `.finally()` type uses, you *must* use the returned Promise.

Fixes weird failures that have been observed in the Chromium CTS roll.

src/common/util/util.ts
@@ -111,7 +111,7 @@ export function raceWithRejectOnTimeout<T>(p: Promise<T>, ms: number, msg: strin
     const handle = timeout(() => {
       reject(new PromiseTimeoutError(msg));
     }, ms);
-    p.finally(() => clearTimeout(handle));
+    p = p.finally(() => clearTimeout(handle));
   });
   return Promise.race([p, timeoutPromise]) as Promise<T>;
 }