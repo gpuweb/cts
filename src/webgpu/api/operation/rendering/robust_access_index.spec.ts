a138de79769470999b2cd50df6f7e1a40c41296f
Kai Ninomiya <kainino@chromium.org>
2021-12-30 16:19:36 -0800

    Add stubs for new tests from spreadsheet

--- src/webgpu/api/operation/rendering/robust_access_index.spec.ts
+++ src/webgpu/api/operation/rendering/robust_access_index.spec.ts
@@ -2,7 +2,7 @@ export const description = `
 TODO: Test that drawIndexedIndirect accesses the index buffer robustly.
 `;
 
-import { makeTestGroup } from '../../../../common/internal/test_group.js';
+import { makeTestGroup } from '../../../../common/framework/test_group.js';
 import { GPUTest } from '../../../gpu_test.js';
 
 export const g = makeTestGroup(GPUTest);
