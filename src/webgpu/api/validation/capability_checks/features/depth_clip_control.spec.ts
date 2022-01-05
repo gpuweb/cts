452b85460e5e8d8f1a9e283ca5a4c02aa224cb8f
Kai Ninomiya <kainino@chromium.org>
2022-01-05 15:11:01 -0800

    organize a few TODOs

--- src/webgpu/api/validation/capability_checks/features/depth_clip_control.spec.ts
+++ src/webgpu/api/validation/capability_checks/features/depth_clip_control.spec.ts
@@ -1,8 +1,12 @@
 export const description = `
-Tests 'depth-clamping' must be enabled for GPUDepthStencilState.clampDepth to be enabled.
+Tests checks that depend on 'depth-clip-control' being enabled.
 `;
 
 import { makeTestGroup } from '../../../../../common/framework/test_group.js';
 import { ValidationTest } from '../../validation_test.js';
 
 export const g = makeTestGroup(ValidationTest);
+
+g.test('createRenderPipeline')
+  .desc(`unclippedDepth=true requires 'depth-clip-control'; false or undefined does not.`)
+  .unimplemented();
