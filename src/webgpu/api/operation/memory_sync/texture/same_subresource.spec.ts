fd35ccfe45f352799c5a6b694bfbae8c1360fa2b
Austin Eng <enga@chromium.org>
2022-03-01 13:12:09 -0800

    memory_sync,texture,* factor out common shader code

--- src/webgpu/api/operation/memory_sync/texture/same_subresource.spec.ts
+++ src/webgpu/api/operation/memory_sync/texture/same_subresource.spec.ts
@@ -32,6 +32,36 @@ import {
 
 export const g = makeTestGroup(GPUTest);
 
+const fullscreenQuadWithUVsWGSL = `
+  struct VertexOutput {
+    @builtin(position) Position : vec4<f32>;
+    @location(0) fragUV : vec2<f32>;
+  };
+
+  @stage(vertex) fn vert_main(@builtin(vertex_index) VertexIndex : u32) -> VertexOutput {
+    var pos = array<vec2<f32>, 6>(
+        vec2<f32>( 1.0,  1.0),
+        vec2<f32>( 1.0, -1.0),
+        vec2<f32>(-1.0, -1.0),
+        vec2<f32>( 1.0,  1.0),
+        vec2<f32>(-1.0, -1.0),
+        vec2<f32>(-1.0,  1.0));
+
+    var uv = array<vec2<f32>, 6>(
+        vec2<f32>(1.0, 0.0),
+        vec2<f32>(1.0, 1.0),
+        vec2<f32>(0.0, 1.0),
+        vec2<f32>(1.0, 0.0),
+        vec2<f32>(0.0, 1.0),
+        vec2<f32>(0.0, 0.0));
+
+    var output : VertexOutput;
+    output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+    output.fragUV = uv[VertexIndex];
+    return output;
+  }
+`;
+
 class TextureSyncTestHelper {
   // We start at the queue context which is top-level.
   private currentContext: OperationContext = 'queue';
@@ -179,34 +209,7 @@ class TextureSyncTestHelper {
           case 'render-pass-encoder':
           case 'render-bundle-encoder': {
             const module = this.device.createShaderModule({
-              code: `
-                struct VertexOutput {
-                  @builtin(position) Position : vec4<f32>;
-                  @location(0) fragUV : vec2<f32>;
-                };
-
-                @stage(vertex) fn vert_main(@builtin(vertex_index) VertexIndex : u32) -> VertexOutput {
-                  var pos = array<vec2<f32>, 6>(
-                      vec2<f32>( 1.0,  1.0),
-                      vec2<f32>( 1.0, -1.0),
-                      vec2<f32>(-1.0, -1.0),
-                      vec2<f32>( 1.0,  1.0),
-                      vec2<f32>(-1.0, -1.0),
-                      vec2<f32>(-1.0,  1.0));
-
-                  var uv = array<vec2<f32>, 6>(
-                      vec2<f32>(1.0, 0.0),
-                      vec2<f32>(1.0, 1.0),
-                      vec2<f32>(0.0, 1.0),
-                      vec2<f32>(1.0, 0.0),
-                      vec2<f32>(0.0, 1.0),
-                      vec2<f32>(0.0, 0.0));
-
-                  var output : VertexOutput;
-                  output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);
-                  output.fragUV = uv[VertexIndex];
-                  return output;
-                }
+              code: `${fullscreenQuadWithUVsWGSL}
 
                 @group(0) @binding(0) var inputTex: texture_2d<f32>;
                 @group(0) @binding(1) var outputTex: texture_storage_2d<rgba8unorm, write>;
@@ -472,34 +475,7 @@ class TextureSyncTestHelper {
           case 'render-pass-encoder':
           case 'render-bundle-encoder': {
             const module = this.device.createShaderModule({
-              code: `
-                struct VertexOutput {
-                  @builtin(position) Position : vec4<f32>;
-                  @location(0) fragUV : vec2<f32>;
-                };
-
-                @stage(vertex) fn vert_main(@builtin(vertex_index) VertexIndex : u32) -> VertexOutput {
-                  var pos = array<vec2<f32>, 6>(
-                      vec2<f32>( 1.0,  1.0),
-                      vec2<f32>( 1.0, -1.0),
-                      vec2<f32>(-1.0, -1.0),
-                      vec2<f32>( 1.0,  1.0),
-                      vec2<f32>(-1.0, -1.0),
-                      vec2<f32>(-1.0,  1.0));
-
-                  var uv = array<vec2<f32>, 6>(
-                      vec2<f32>(1.0, 0.0),
-                      vec2<f32>(1.0, 1.0),
-                      vec2<f32>(0.0, 1.0),
-                      vec2<f32>(1.0, 0.0),
-                      vec2<f32>(0.0, 1.0),
-                      vec2<f32>(0.0, 0.0));
-
-                  var output : VertexOutput;
-                  output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);
-                  output.fragUV = uv[VertexIndex];
-                  return output;
-                }
+              code: `${fullscreenQuadWithUVsWGSL}
 
                 @group(0) @binding(0) var outputTex: texture_storage_2d<rgba8unorm, write>;
 
