cb8374c96306908114e36986fabed5288a8bcb7d
Jiawei Shao <jiawei.shao@intel.com>
2022-03-04 16:21:53 +0800

    Add validation,resource_usages,texture,in_render_common:* - Part I
    This PR adds the first part of validation,resource_usages,texture,*:
    - in_render_common:subresources_from_same_texture_as_color_attachments

--- src/webgpu/api/validation/resource_usages/texture/in_render_common.spec.ts
+++ src/webgpu/api/validation/resource_usages/texture/in_render_common.spec.ts
@@ -18,4 +18,75 @@ subresources, one for same draw/same pass/different pass, one for visibilities).
 import { makeTestGroup } from '../../../../../common/framework/test_group.js';
 import { ValidationTest } from '../../validation_test.js';
 
-export const g = makeTestGroup(ValidationTest);
+class F extends ValidationTest {
+  getColorAttachment(
+    texture: GPUTexture,
+    textureViewDescriptor?: GPUTextureViewDescriptor
+  ): GPURenderPassColorAttachment {
+    const view = texture.createView(textureViewDescriptor);
+
+    return {
+      view,
+      loadValue: { r: 1.0, g: 0.0, b: 0.0, a: 1.0 },
+      storeOp: 'store',
+    };
+  }
+}
+
+export const g = makeTestGroup(F);
+
+// Test that the different subresource of the same texture are allowed to be used as color
+// attachments in same / different render passes, while the same subresource is only allowed to be
+// used as different color attachments in different render passes.
+g.test('subresources_from_same_texture_as_color_attachments')
+  .params(u =>
+    u
+      .combine('baseLayer0', [0, 1])
+      .combine('baseLevel0', [0, 1])
+      .combine('baseLayer1', [0, 1])
+      .combine('baseLevel1', [0, 1])
+      .combine('inSamePass', [true, false])
+      .unless(t => t.inSamePass && t.baseLevel0 !== t.baseLevel1)
+  )
+  .fn(async t => {
+    const { baseLayer0, baseLevel0, baseLayer1, baseLevel1, inSamePass } = t.params;
+
+    const texture = t.device.createTexture({
+      format: 'rgba8unorm',
+      usage: GPUTextureUsage.RENDER_ATTACHMENT,
+      size: [16, 16, 2],
+      mipLevelCount: 2,
+    });
+
+    const colorAttachment1 = t.getColorAttachment(texture, {
+      baseArrayLayer: baseLayer0,
+      baseMipLevel: baseLevel0,
+      mipLevelCount: 1,
+    });
+    const colorAttachment2 = t.getColorAttachment(texture, {
+      baseArrayLayer: baseLayer1,
+      baseMipLevel: baseLevel1,
+      mipLevelCount: 1,
+    });
+    const encoder = t.device.createCommandEncoder();
+    if (inSamePass) {
+      const renderPass = encoder.beginRenderPass({
+        colorAttachments: [colorAttachment1, colorAttachment2],
+      });
+      renderPass.endPass();
+    } else {
+      const renderPass1 = encoder.beginRenderPass({
+        colorAttachments: [colorAttachment1],
+      });
+      renderPass1.endPass();
+      const renderPass2 = encoder.beginRenderPass({
+        colorAttachments: [colorAttachment2],
+      });
+      renderPass2.endPass();
+    }
+
+    const success = inSamePass ? baseLayer0 !== baseLayer1 : true;
+    t.expectValidationError(() => {
+      encoder.finish();
+    }, !success);
+  });
