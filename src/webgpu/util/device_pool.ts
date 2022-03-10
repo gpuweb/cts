5028420f7f8de20ebf244830a07faf5d0d5f7dbd
Austin Eng <enga@chromium.org>
2022-03-10 13:32:19 -0800

    Attempt to wait for queue idle on release of the device to the pool

--- src/webgpu/util/device_pool.ts
+++ src/webgpu/util/device_pool.ts
@@ -297,6 +297,9 @@ class DeviceHolder implements DeviceProvider {
     let gpuValidationError: GPUValidationError | GPUOutOfMemoryError | null;
     let gpuOutOfMemoryError: GPUValidationError | GPUOutOfMemoryError | null;
 
+    // Submit to the queue to attempt to force a GPU flush.
+    this.device.queue.submit([]);
+
     try {
       // May reject if the device was lost.
       gpuValidationError = await this.device.popErrorScope();
@@ -308,6 +311,9 @@ class DeviceHolder implements DeviceProvider {
       );
       throw ex;
     }
+
+    // Attempt to wait for the queue to be idle.
+    await this.device.queue.onSubmittedWorkDone();
 
     await assertReject(
       this.device.popErrorScope(),
