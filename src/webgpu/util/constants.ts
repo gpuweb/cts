950e4e99c55e8262ed97f4d6a765e9cb088e6378
Ryan Harrison <rharrison@google.com>
2022-03-17 16:39:22 -0400

    Implement tests for floating point arithmetic expressions (#1073)
    Also adds quantizing functions and converts `truthFunc` to `makeCase`

--- src/webgpu/util/constants.ts
+++ src/webgpu/util/constants.ts
@@ -240,6 +240,16 @@ export const kValue = {
       max: hexToF32(kBit.f32.negative.max),
       min: hexToF32(kBit.f32.negative.min),
     },
+    subnormal: {
+      positive: {
+        min: hexToF32(kBit.f32.subnormal.positive.min),
+        max: hexToF32(kBit.f32.subnormal.positive.max),
+      },
+      negative: {
+        max: hexToF32(kBit.f32.subnormal.negative.max),
+        min: hexToF32(kBit.f32.subnormal.negative.min),
+      },
+    },
   },
 
   powTwo: {
