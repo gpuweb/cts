6d34aa5b656f4ee3cf112fabac5c9632ba177760
jchen10 <jie.a.chen@intel.com>
2022-03-19 19:44:24 +0800

    Fix toHalf and update README

--- src/webgpu/web_platform/reftests/canvas_complex.html.ts
+++ src/webgpu/web_platform/reftests/canvas_complex.html.ts
@@ -1,5 +1,5 @@
 import { assert, unreachable } from '../../../common/util/util.js';
-import { gammaDecompress } from '../../util/conversion.js';
+import { gammaDecompress, float32ToFloat16Bits } from '../../util/conversion.js';
 
 import { runRefTest } from './gpu_ref_test.js';
 
@@ -71,59 +71,43 @@ export function run(
           break;
         case 'rgba16float':
           {
-            // ref: http://stackoverflow.com/questions/32633585/how-do-you-convert-to-half-floats-in-javascript
-            const toHalf = (val: number) => {
-              /* This method is faster than the OpenEXR implementation (very often
-               * used, eg. in Ogre), with the additional benefit of rounding, inspired
-               * by James Tursa?s half-precision code. */
-              const floatView = new Float32Array(1);
-              const int32View = new Int32Array(floatView.buffer);
-
-              floatView[0] = val;
-              const x = int32View[0];
-
-              let bits = (x >> 16) & 0x8000; /* Get the sign */
-              let m = (x >> 12) & 0x07ff; /* Keep one extra bit for rounding */
-              const e = (x >> 23) & 0xff; /* Using int is faster here */
-
-              /* If zero, or denormal, or exponent underflows too much for a denormal
-               * half, return signed zero. */
-              if (e < 103) {
-                return bits;
-              }
-
-              /* If NaN, return NaN. If Inf or exponent overflow, return Inf. */
-              if (e > 142) {
-                bits |= 0x7c00;
-                /* If exponent was 0xff and one mantissa bit was set, it means NaN,
-                 * not Inf, so make sure we set one mantissa bit too. */
-                bits |= (e === 255 ? 0 : 1) && x & 0x007fffff;
-                return bits;
-              }
-
-              /* If exponent underflows but not too much, return a denormal */
-              if (e < 113) {
-                m |= 0x0800;
-                /* Extra rounding may overflow and set mantissa to 0 and exponent
-                 * to 1, which is OK. */
-                bits |= (m >> (114 - e)) + ((m >> (113 - e)) & 1);
-                return bits;
-              }
-
-              bits |= ((e - 112) << 10) | (m >> 1);
-              /* Extra rounding. An overflow will set mantissa to 0 and increment
-               * the exponent, which is OK. */
-              bits += m & 1;
-              return bits;
-            };
-
             const data = new Uint16Array(mapping);
-            data.set(new Uint16Array([toHalf(0.5), toHalf(0.0), toHalf(0.0), toHalf(1.0)]), 0); // red
-            data.set(new Uint16Array([toHalf(0.0), toHalf(0.5), toHalf(0.0), toHalf(1.0)]), 4); // green
-            // prettier-ignore
-            data.set(new Uint16Array([toHalf(0.0), toHalf(0.0), toHalf(0.5), toHalf(1.0)]), 128 + 0); // blue
-            // prettier-ignore
-            data.set(new Uint16Array([toHalf(0.5), toHalf(0.5), toHalf(0.0), toHalf(1.0)]), 128 + 4); // yellow
+            data.set(
+              new Uint16Array([
+                float32ToFloat16Bits(0.5),
+                float32ToFloat16Bits(0.0),
+                float32ToFloat16Bits(0.0),
+                float32ToFloat16Bits(1.0),
+              ]),
+              0
+            ); // red
+            data.set(
+              new Uint16Array([
+                float32ToFloat16Bits(0.0),
+                float32ToFloat16Bits(0.5),
+                float32ToFloat16Bits(0.0),
+                float32ToFloat16Bits(1.0),
+              ]),
+              4
+            ); // green
+            data.set(
+              new Uint16Array([
+                float32ToFloat16Bits(0.0),
+                float32ToFloat16Bits(0.0),
+                float32ToFloat16Bits(0.5),
+                float32ToFloat16Bits(1.0),
+              ]),
+              128 + 0
+            ); // blue
+            data.set(
+              new Uint16Array([
+                float32ToFloat16Bits(0.5),
+                float32ToFloat16Bits(0.5),
+                float32ToFloat16Bits(0.0),
+                float32ToFloat16Bits(1.0),
+              ]),
+              128 + 4
+            ); // yellow
           }
           break;
       }
