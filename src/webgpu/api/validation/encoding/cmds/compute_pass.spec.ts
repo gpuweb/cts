007ab09d90eeeda6bdf17e3b6b711a2fcc477582
Corentin Wallez <cwallez@chromium.org>
2022-03-30 17:51:40 +0200

    Make an info table for limits.
    This was a set of hard-to-separate changes:
     - Making an info table for limits.
     - Reworking the requestDevice_limits test to use the concept of limit
       class.
     - Adding isPowerOfTwo.

--- src/webgpu/api/validation/encoding/cmds/compute_pass.spec.ts
+++ src/webgpu/api/validation/encoding/cmds/compute_pass.spec.ts
@@ -5,7 +5,7 @@ Does **not** test usage scopes (resource_usages/) or programmable pass stuff (pr
 `;
 
 import { makeTestGroup } from '../../../../../common/framework/test_group.js';
-import { DefaultLimits } from '../../../../constants.js';
+import { kLimitInfo } from '../../../../capability_info.js';
 import { kResourceStates, ResourceState } from '../../../../gpu_test.js';
 import { ValidationTest } from '../../validation_test.js';
 
@@ -88,7 +88,7 @@ g.test('pipeline,device_mismatch')
     validateFinish(!mismatched);
   });
 
-const kMaxDispatch = DefaultLimits.maxComputeWorkgroupsPerDimension;
+const kMaxDispatch = kLimitInfo.maxComputeWorkgroupsPerDimension.default;
 g.test('dispatch_sizes')
   .desc(
     `Test 'direct' and 'indirect' dispatch with various sizes.
