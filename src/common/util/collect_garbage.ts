897c4bbea6f4f846f01bc43e0d0b079274f59a20
Corentin Wallez <cwallez@chromium.org>
2022-04-05 18:44:11 +0100

    Use globalThis instead of self. (#1141)
    The `self` global variable seems to only be available in browsers. Use
    `globalThis` instead that is available everywhere.

--- src/common/util/collect_garbage.ts
+++ src/common/util/collect_garbage.ts
@@ -10,7 +10,7 @@ declare const Components: any;
  */
 export async function attemptGarbageCollection(): Promise<void> {
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const w: any = self;
+  const w: any = globalThis;
   if (w.GCController) {
     w.GCController.collect();
     return;
