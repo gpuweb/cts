9e419c0303693fa4186c60e26a32c6e409c669e4
Corentin Wallez <cwallez@chromium.org>
2022-04-08 10:42:41 +0200

    image_copy,mip_levels: reduce the mip level count.
    Using a mip level of 6 made the test request a 1.2 GB 3d texture, which
    shouldn't be something regular tests do, only stress tests.

--- src/webgpu/api/operation/command_buffer/image_copy.spec.ts
+++ src/webgpu/api/operation/command_buffer/image_copy.spec.ts
@@ -1620,14 +1620,14 @@ Tests both 2D and 3D textures. 1D textures are skipped because they can only hav
           copySizeInBlocks: { width: 5, height: 4, depthOrArrayLayers: 2 },
           originInBlocks: { x: 3, y: 2, z: 1 },
           _mipSizeInBlocks: { width: 9, height: 7, depthOrArrayLayers: 3 },
-          mipLevel: 5,
+          mipLevel: 4,
         },
         // origin + copySize < texturePhysicalSizeAtMipLevel for all coordinates
         {
           copySizeInBlocks: { width: 5, height: 4, depthOrArrayLayers: 2 },
           originInBlocks: { x: 3, y: 2, z: 1 },
           _mipSizeInBlocks: { width: 9, height: 7, depthOrArrayLayers: 4 },
-          mipLevel: 6,
+          mipLevel: 4,
         },
       ])
       .expand('textureSize', generateTestTextureSizes)
