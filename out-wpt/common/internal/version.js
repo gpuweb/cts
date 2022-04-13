1240b37e4ac5386bbc97a7207dc5eb494034c57b
Kangz <corentin@wallez.net>
2022-04-13 16:20:08 +0000

    Deploying to gh-pages from @ gpuweb/cts@5e83d8349be14cf3af9e789aa3cf365f58b19836 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'a676a5c0f05fca5fc75b831d74106738e29fb337';
+export const version = '5e83d8349be14cf3af9e789aa3cf365f58b19836';
