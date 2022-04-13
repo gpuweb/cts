5723a6ea5e682302ac60c4d2044af70837552cb7
austinEng <213reeses@gmail.com>
2022-04-13 23:25:42 +0000

    Deploying to gh-pages from @ gpuweb/cts@cd7e9e9953475a47d266d2d5456f7c06a7b3403a 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '5e83d8349be14cf3af9e789aa3cf365f58b19836';
+export const version = 'cd7e9e9953475a47d266d2d5456f7c06a7b3403a';
