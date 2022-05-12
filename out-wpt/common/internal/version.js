5bf4b6db72ccf9496a22d161c7e2f82f608f3ff3
amaiorano <amaiorano@gmail.com>
2022-05-12 15:50:21 +0000

    Deploying to gh-pages from @ gpuweb/cts@b32ac31a0eab7386807f01c7cb8d77a6e1104c23 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '5a1defd2980454b434c707ef002bdb21ff1e145e';
+export const version = 'b32ac31a0eab7386807f01c7cb8d77a6e1104c23';
