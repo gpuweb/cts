43de9343ae25cc07ba1ad11c5499833d970c51a8
shaoboyan <shaobo.yan@intel.com>
2022-06-02 05:55:08 +0000

    Deploying to gh-pages from @ gpuweb/cts@6302a621f833d7b1a5aa7cc1fd29900dd653fa26 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'f188c0d576470eb96ecb75c19f77ef670886dd64';
+export const version = '6302a621f833d7b1a5aa7cc1fd29900dd653fa26';
