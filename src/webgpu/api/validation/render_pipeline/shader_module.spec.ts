aa6069dea0d78e51fa1bdd8f05a798354c2e6aba
ShrekShao <shrekshao@google.com>
2022-06-14 16:04:30 -0700

    Review createRenderPipeline.spec.ts (#1520)
    * major review and impl
    
    * reorganize
    
    * shader module, entry point
    
    * render pass color attachments empty test
    
    * description
    
    * fix run:generate-listing
    
    * fix
    
    * fix webgpu types
    
    * address comments
    
    * regroup
    
    * entry point regroup
    
    * fix max_color_attachments_limit write mask
    
    * fix shader_module prefix
    
    * remove redundent prefixes

---
+++ src/webgpu/api/validation/render_pipeline/shader_module.spec.ts
@@ -0,0 +1,198 @@
+export const description = `
+This test dedicatedly tests createRenderPipeline validation issues related to the shader modules.
+
+Note: entry point matching tests are in ../shader_module/entry_point.spec.ts
+`;
+
+import { makeTestGroup } from '../../../../common/framework/test_group.js';
+import {
+  getFragmentShaderCodeWithOutput,
+  getShaderWithEntryPoint,
+  kDefaultVertexShaderCode,
+  kDefaultFragmentShaderCode,
+} from '../../../util/shader.js';
+
+import { CreateRenderPipelineValidationTest } from './common.js';
+
+export const g = makeTestGroup(CreateRenderPipelineValidationTest);
+
+const values = [0, 1, 0, 1];
+
+g.test('device_mismatch')
+  .desc(
+    'Tests createRenderPipeline(Async) cannot be called with a shader module created from another device'
+  )
+  .paramsSubcasesOnly(u =>
+    u.combine('isAsync', [true, false]).combineWithParams([
+      { vertex_mismatched: false, fragment_mismatched: false, _success: true },
+      { vertex_mismatched: true, fragment_mismatched: false, _success: false },
+      { vertex_mismatched: false, fragment_mismatched: true, _success: false },
+    ])
+  )
+  .beforeAllSubcases(t => {
+    t.selectMismatchedDeviceOrSkipTestCase(undefined);
+  })
+  .fn(async t => {
+    const { isAsync, vertex_mismatched, fragment_mismatched, _success } = t.params;
+
+    const code = `
+      @vertex fn main() -> @builtin(position) vec4<f32> {
+        return vec4<f32>(0.0, 0.0, 0.0, 1.0);
+      }
+    `;
+
+    const descriptor = {
+      vertex: {
+        module: vertex_mismatched
+          ? t.mismatchedDevice.createShaderModule({ code })
+          : t.device.createShaderModule({ code }),
+        entryPoint: 'main',
+      },
+      fragment: {
+        module: fragment_mismatched
+          ? t.mismatchedDevice.createShaderModule({
+              code: getFragmentShaderCodeWithOutput([
+                { values, plainType: 'f32', componentCount: 4 },
+              ]),
+            })
+          : t.device.createShaderModule({
+              code: getFragmentShaderCodeWithOutput([
+                { values, plainType: 'f32', componentCount: 4 },
+              ]),
+            }),
+        entryPoint: 'main',
+        targets: [{ format: 'rgba8unorm' }] as const,
+      },
+      layout: t.getPipelineLayout(),
+    };
+
+    t.doCreateRenderPipelineTest(isAsync, _success, descriptor);
+  });
+
+g.test('invalid,vertex')
+  .desc(`Tests shader module must be valid.`)
+  .params(u => u.combine('isAsync', [true, false]).combine('isVertexShaderValid', [true, false]))
+  .fn(async t => {
+    const { isAsync, isVertexShaderValid } = t.params;
+    t.doCreateRenderPipelineTest(isAsync, isVertexShaderValid, {
+      layout: 'auto',
+      vertex: {
+        module: isVertexShaderValid
+          ? t.device.createShaderModule({
+              code: kDefaultVertexShaderCode,
+            })
+          : t.createInvalidShaderModule(),
+        entryPoint: 'main',
+      },
+    });
+  });
+
+g.test('invalid,fragment')
+  .desc(`Tests shader module must be valid.`)
+  .params(u => u.combine('isAsync', [true, false]).combine('isFragmentShaderValid', [true, false]))
+  .fn(async t => {
+    const { isAsync, isFragmentShaderValid } = t.params;
+    t.doCreateRenderPipelineTest(isAsync, isFragmentShaderValid, {
+      layout: 'auto',
+      vertex: {
+        module: t.device.createShaderModule({
+          code: kDefaultVertexShaderCode,
+        }),
+        entryPoint: 'main',
+      },
+      fragment: {
+        module: isFragmentShaderValid
+          ? t.device.createShaderModule({
+              code: kDefaultFragmentShaderCode,
+            })
+          : t.createInvalidShaderModule(),
+        entryPoint: 'main',
+        targets: [{ format: 'rgba8unorm' }],
+      },
+    });
+  });
+
+const kEntryPointTestCases = [
+  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main' },
+  { shaderModuleEntryPoint: 'main', stageEntryPoint: '' },
+  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main\0' },
+  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main\0a' },
+  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'mian' },
+  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main ' },
+  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'ma in' },
+  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main\n' },
+  { shaderModuleEntryPoint: 'mian', stageEntryPoint: 'mian' },
+  { shaderModuleEntryPoint: 'mian', stageEntryPoint: 'main' },
+  { shaderModuleEntryPoint: 'mainmain', stageEntryPoint: 'mainmain' },
+  { shaderModuleEntryPoint: 'mainmain', stageEntryPoint: 'foo' },
+  { shaderModuleEntryPoint: 'main_t12V3', stageEntryPoint: 'main_t12V3' },
+  { shaderModuleEntryPoint: 'main_t12V3', stageEntryPoint: 'main_t12V5' },
+  { shaderModuleEntryPoint: 'main_t12V3', stageEntryPoint: '_main_t12V3' },
+  { shaderModuleEntryPoint: 'séquençage', stageEntryPoint: 'séquençage' },
+  { shaderModuleEntryPoint: 'séquençage', stageEntryPoint: 'sequencage' },
+];
+
+g.test('entry_point,vertex')
+  .desc(
+    `
+Tests calling createRenderPipeline(Async) with valid vertex stage shader and different entryPoints,
+and check that the APIs only accept matching entryPoint.
+
+The entryPoint in shader module include standard "main" and others.
+The entryPoint assigned in descriptor include:
+- Matching case (control case)
+- Empty string
+- Mistyping
+- Containing invalid char, including space and control codes (Null character)
+- Unicode entrypoints and their ASCIIfied version
+
+TODO:
+- Test unicode normalization (gpuweb/gpuweb#1160)
+- Fine-tune test cases to reduce number by removing trivially similiar cases
+`
+  )
+  .params(u => u.combine('isAsync', [true, false]).combineWithParams(kEntryPointTestCases))
+  .fn(async t => {
+    const { isAsync, shaderModuleEntryPoint, stageEntryPoint } = t.params;
+    const descriptor: GPURenderPipelineDescriptor = {
+      layout: 'auto',
+      vertex: {
+        module: t.device.createShaderModule({
+          code: getShaderWithEntryPoint('vertex', shaderModuleEntryPoint),
+        }),
+        entryPoint: stageEntryPoint,
+      },
+    };
+    const _success = shaderModuleEntryPoint === stageEntryPoint;
+    t.doCreateRenderPipelineTest(isAsync, _success, descriptor);
+  });
+
+g.test('entry_point,fragment')
+  .desc(
+    `
+Tests calling createRenderPipeline(Async) with valid fragment stage shader and different entryPoints,
+and check that the APIs only accept matching entryPoint.
+`
+  )
+  .params(u => u.combine('isAsync', [true, false]).combineWithParams(kEntryPointTestCases))
+  .fn(async t => {
+    const { isAsync, shaderModuleEntryPoint, stageEntryPoint } = t.params;
+    const descriptor: GPURenderPipelineDescriptor = {
+      layout: 'auto',
+      vertex: {
+        module: t.device.createShaderModule({
+          code: kDefaultVertexShaderCode,
+        }),
+        entryPoint: 'main',
+      },
+      fragment: {
+        module: t.device.createShaderModule({
+          code: getShaderWithEntryPoint('fragment', shaderModuleEntryPoint),
+        }),
+        entryPoint: stageEntryPoint,
+        targets: [{ format: 'rgba8unorm' }],
+      },
+    };
+    const _success = shaderModuleEntryPoint === stageEntryPoint;
+    t.doCreateRenderPipelineTest(isAsync, _success, descriptor);
+  });
--- src/webgpu/util/shader.ts
+++ src/webgpu/util/shader.ts
@@ -1,5 +1,16 @@
 import { unreachable } from '../../common/util/util.js';
 
+export const kDefaultVertexShaderCode = `
+@vertex fn main() -> @builtin(position) vec4<f32> {
+  return vec4<f32>(0.0, 0.0, 0.0, 1.0);
+}
+`;
+
+export const kDefaultFragmentShaderCode = `
+@fragment fn main() -> @location(0) vec4<f32>  {
+  return vec4<f32>(1.0, 1.0, 1.0, 1.0);
+}`;
+
 const kPlainTypeInfo = {
   i32: {
     suffix: '',
@@ -124,4 +135,42 @@ export function getFragmentShaderCodeWithOutput(
     @fragment fn main() -> Outputs {
         return Outputs(${resultStrings.join(',')});
     }`;
+}
+
+export type TShaderStage = 'compute' | 'vertex' | 'fragment' | 'empty';
+
+/**
+ * Return a foo shader of the given stage with the given entry point
+ * @param shaderStage
+ * @param entryPoint
+ * @returns the shader string
+ */
+export function getShaderWithEntryPoint(shaderStage: TShaderStage, entryPoint: string): string {
+  let code;
+  switch (shaderStage) {
+    case 'compute': {
+      code = `@compute @workgroup_size(1) fn ${entryPoint}() {}`;
+      break;
+    }
+    case 'vertex': {
+      code = `
+      @vertex fn ${entryPoint}() -> @builtin(position) vec4<f32> {
+        return vec4<f32>(0.0, 0.0, 0.0, 1.0);
+      }`;
+      break;
+    }
+    case 'fragment': {
+      code = `
+      @fragment fn ${entryPoint}() -> @location(0) vec4<f32> {
+        return vec4<f32>(0.0, 1.0, 0.0, 1.0);
+      }`;
+      break;
+    }
+    case 'empty':
+    default: {
+      code = '';
+      break;
+    }
+  }
+  return code;
 }
