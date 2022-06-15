dd3da59d85e1de28dc9627e296e906483c85d37e
shrekshao <shrekshao@gmail.com>
2022-06-15 21:57:01 +0000

    Deploying to gh-pages from @ gpuweb/cts@1e9b6f7a6dbb5a3bb4c5e6472e599fb6577c9ef2 🚀

--- out-wpt/webgpu/api/validation/render_pipeline/shader_module.spec.js
+++ out-wpt/webgpu/api/validation/render_pipeline/shader_module.spec.js
@@ -8,7 +8,6 @@ Note: entry point matching tests are in ../shader_module/entry_point.spec.ts
 import { makeTestGroup } from '../../../../common/framework/test_group.js';
 import {
   getFragmentShaderCodeWithOutput,
-  getShaderWithEntryPoint,
   kDefaultVertexShaderCode,
   kDefaultFragmentShaderCode,
 } from '../../../util/shader.js';
@@ -116,95 +115,4 @@ g.test('invalid,fragment')
         targets: [{ format: 'rgba8unorm' }],
       },
     });
-  });
-
-const kEntryPointTestCases = [
-  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main' },
-  { shaderModuleEntryPoint: 'main', stageEntryPoint: '' },
-  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main\0' },
-  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main\0a' },
-  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'mian' },
-  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main ' },
-  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'ma in' },
-  { shaderModuleEntryPoint: 'main', stageEntryPoint: 'main\n' },
-  { shaderModuleEntryPoint: 'mian', stageEntryPoint: 'mian' },
-  { shaderModuleEntryPoint: 'mian', stageEntryPoint: 'main' },
-  { shaderModuleEntryPoint: 'mainmain', stageEntryPoint: 'mainmain' },
-  { shaderModuleEntryPoint: 'mainmain', stageEntryPoint: 'foo' },
-  { shaderModuleEntryPoint: 'main_t12V3', stageEntryPoint: 'main_t12V3' },
-  { shaderModuleEntryPoint: 'main_t12V3', stageEntryPoint: 'main_t12V5' },
-  { shaderModuleEntryPoint: 'main_t12V3', stageEntryPoint: '_main_t12V3' },
-  { shaderModuleEntryPoint: 'séquençage', stageEntryPoint: 'séquençage' },
-  { shaderModuleEntryPoint: 'séquençage', stageEntryPoint: 'sequencage' },
-];
-
-g.test('entry_point,vertex')
-  .desc(
-    `
-Tests calling createRenderPipeline(Async) with valid vertex stage shader and different entryPoints,
-and check that the APIs only accept matching entryPoint.
-
-The entryPoint in shader module include standard "main" and others.
-The entryPoint assigned in descriptor include:
-- Matching case (control case)
-- Empty string
-- Mistyping
-- Containing invalid char, including space and control codes (Null character)
-- Unicode entrypoints and their ASCIIfied version
-
-TODO:
-- Test unicode normalization (gpuweb/gpuweb#1160)
-- Fine-tune test cases to reduce number by removing trivially similiar cases
-`
-  )
-  .params(u => u.combine('isAsync', [true, false]).combineWithParams(kEntryPointTestCases))
-  .fn(async t => {
-    const { isAsync, shaderModuleEntryPoint, stageEntryPoint } = t.params;
-    const descriptor = {
-      layout: 'auto',
-      vertex: {
-        module: t.device.createShaderModule({
-          code: getShaderWithEntryPoint('vertex', shaderModuleEntryPoint),
-        }),
-
-        entryPoint: stageEntryPoint,
-      },
-    };
-
-    const _success = shaderModuleEntryPoint === stageEntryPoint;
-    t.doCreateRenderPipelineTest(isAsync, _success, descriptor);
-  });
-
-g.test('entry_point,fragment')
-  .desc(
-    `
-Tests calling createRenderPipeline(Async) with valid fragment stage shader and different entryPoints,
-and check that the APIs only accept matching entryPoint.
-`
-  )
-  .params(u => u.combine('isAsync', [true, false]).combineWithParams(kEntryPointTestCases))
-  .fn(async t => {
-    const { isAsync, shaderModuleEntryPoint, stageEntryPoint } = t.params;
-    const descriptor = {
-      layout: 'auto',
-      vertex: {
-        module: t.device.createShaderModule({
-          code: kDefaultVertexShaderCode,
-        }),
-
-        entryPoint: 'main',
-      },
-
-      fragment: {
-        module: t.device.createShaderModule({
-          code: getShaderWithEntryPoint('fragment', shaderModuleEntryPoint),
-        }),
-
-        entryPoint: stageEntryPoint,
-        targets: [{ format: 'rgba8unorm' }],
-      },
-    };
-
-    const _success = shaderModuleEntryPoint === stageEntryPoint;
-    t.doCreateRenderPipelineTest(isAsync, _success, descriptor);
   });
--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '3c5e8a8e5ad539ae28a8a6980c31538f0d7fc9ec';
+export const version = '1e9b6f7a6dbb5a3bb4c5e6472e599fb6577c9ef2';
