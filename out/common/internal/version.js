03c519d7354f0a46575d3962cf9ebf04062e4b74
Gyuyoung <gyuyoung@igalia.com>
2022-07-13 01:57:24 +0000

    Deploying to gh-pages from @ gpuweb/cts@06954d98b3f95e55b4810f422b64b4bcd440d62f 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'deec4fea50711c51954e7b19ba4d92aeaa5a1ff1';
+export const version = '06954d98b3f95e55b4810f422b64b4bcd440d62f';
