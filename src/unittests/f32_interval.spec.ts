5b964114f5812582d2fc43bc7457fb165adeb70b
Ryan Harrison <rharrison@google.com>
2022-07-19 16:54:04 -0400

    Correct the order of params in atan2 message (#1677)
    Fixes #1680

--- src/unittests/f32_interval.spec.ts
+++ src/unittests/f32_interval.spec.ts
@@ -1232,7 +1232,7 @@ g.test('atan2Interval')
     const got = atan2Interval(y, x);
     t.expect(
       objectEquals(expected, got),
-      `atan2Interval(${x}, ${y}) returned ${got}. Expected ${expected}`
+      `atan2Interval(${y}, ${x}) returned ${got}. Expected ${expected}`
     );
   });
 
