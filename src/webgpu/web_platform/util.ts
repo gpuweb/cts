c979a712aeb8529c475f07d83b16c84f34908d9b
Kai Ninomiya <kainino@chromium.org>
2022-07-22 14:32:30 -0700

    nit: remove unnecessary `as any`

--- src/webgpu/web_platform/util.ts
+++ src/webgpu/web_platform/util.ts
@@ -38,8 +38,7 @@ export function startPlayingAndWaitForVideo(
       );
 
       if ('requestVideoFrameCallback' in video) {
-        /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-        (video as any).requestVideoFrameCallback(() => {
+        video.requestVideoFrameCallback(() => {
           callbackAndResolve();
         });
       } else {
