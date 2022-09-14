05caef0eaca021d0bed8614875aa95a577e0444a
jzm-intel <zhaoming.jiang@intel.com>
2022-09-14 02:14:15 +0000

    Deploying to gh-pages from @ gpuweb/cts@d33144732adb4b7150569bf2e7d1ee82d14c5812 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '5e70787c4de387b358243184fd47b5889d4504f6';
+export const version = 'd33144732adb4b7150569bf2e7d1ee82d14c5812';
