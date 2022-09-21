d05fdaba161e329265c25fbc256c4ad393d9094a
dj2 <dj2@everburning.com>
2022-09-21 14:11:01 +0000

    Deploying to gh-pages from @ gpuweb/cts@0860ad6114aa63cbfdd2a1f79c91cc1e96091621 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'e77a9f259c7e08893b11a1296c3a0b2d7821db42';
+export const version = '0860ad6114aa63cbfdd2a1f79c91cc1e96091621';
