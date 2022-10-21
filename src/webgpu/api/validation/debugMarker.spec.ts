8acd0240023f61d551d495b9ab8f44c18d4b7a19
Gyuyoung Kim <gyuyoung@igalia.com>
2022-10-21 10:12:14 +0900

    val: Add 'push_pop_call_count_unbalance,render_compute_pass' test
    This PR adds a new test to ensure that a validation error is generated
    if {push,pop} debug group call count is not paired in ComputePassEncoder
    and RenderPassEncoder.
    
    Issue: #1928

--- src/webgpu/api/validation/debugMarker.spec.ts
+++ src/webgpu/api/validation/debugMarker.spec.ts
@@ -6,9 +6,30 @@ import { makeTestGroup } from '../../../common/framework/test_group.js';
 
 import { ValidationTest } from './validation_test.js';
 
-export const g = makeTestGroup(ValidationTest);
+class F extends ValidationTest {
+  beginRenderPass(commandEncoder: GPUCommandEncoder): GPURenderPassEncoder {
+    const attachmentTexture = this.device.createTexture({
+      format: 'rgba8unorm',
+      size: { width: 16, height: 16, depthOrArrayLayers: 1 },
+      usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    });
+    this.trackForCleanup(attachmentTexture);
+    return commandEncoder.beginRenderPass({
+      colorAttachments: [
+        {
+          view: attachmentTexture.createView(),
+          clearValue: { r: 1.0, g: 0.0, b: 0.0, a: 1.0 },
+          loadOp: 'clear',
+          storeOp: 'store',
+        },
+      ],
+    });
+  }
+}
 
-g.test('push_pop_call_count_unbalance')
+export const g = makeTestGroup(F);
+
+g.test('push_pop_call_count_unbalance,command_encoder')
   .desc(
     `
   Test that a validation error is generated if {push,pop} debug group call count is not paired.
@@ -35,6 +56,43 @@ g.test('push_pop_call_count_unbalance')
     }
 
     t.expectValidationError(() => {
+      encoder.finish();
+    }, pushCount !== popCount);
+  });
+
+g.test('push_pop_call_count_unbalance,render_compute_pass')
+  .desc(
+    `
+  Test that a validation error is generated if {push,pop} debug group call count is not paired in
+  ComputePassEncoder and RenderPassEncoder.
+  `
+  )
+  .params(u =>
+    u //
+      .combine('passType', ['compute', 'render'])
+      .beginSubcases()
+      .combine('pushCount', [1, 2, 3])
+      .combine('popCount', [1, 2, 3])
+  )
+  .fn(async t => {
+    const { passType, pushCount, popCount } = t.params;
+
+    const encoder = t.device.createCommandEncoder();
+
+    const pass = passType === 'compute' ? encoder.beginComputePass() : t.beginRenderPass(encoder);
+
+    for (let i = 0; i < pushCount; ++i) {
+      pass.pushDebugGroup('EventStart');
+    }
+
+    pass.insertDebugMarker('Marker');
+
+    for (let i = 0; i < popCount; ++i) {
+      pass.popDebugGroup();
+    }
+
+    t.expectValidationError(() => {
+      pass.end();
       encoder.finish();
     }, pushCount !== popCount);
   });
