d786dbf6b3da704f04e2eb1875b0e5c8fbc08087
Takahiro <hogehoge@gachapin.jp>
2022-11-04 00:19:38 -0700

    delint

--- src/webgpu/api/validation/texture/rg11b10ufloat_renderable.spec.ts
+++ src/webgpu/api/validation/texture/rg11b10ufloat_renderable.spec.ts
@@ -16,9 +16,7 @@ sampleCount > 1, iff rg11b10ufloat-renderable feature is enabled.
 `
   )
   .params(u =>
-    u
-      .combine('usage', [0, GPUConst.TextureUsage.RENDER_ATTACHMENT])
-      .combine('sampleCount', [1, 4])
+    u.combine('usage', [0, GPUConst.TextureUsage.RENDER_ATTACHMENT]).combine('sampleCount', [1, 4])
   )
   .unimplemented();
 
