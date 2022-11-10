16db9daa6b407abef2fcc9c58ff4142272ebb2af
Ben Clayton <bclayton@google.com>
2022-11-10 16:49:58 +0000

    Add a DataCache & expression CaseCache
    Profiling has highlighted that the logic to build the floating-point shader expression test cases can often be more expensive than running the test.
    
    This problem is compounded by the fact that the shader expression tests are often run with slight variations to the input source ('const', 'uniform', etc).
    
    This change introduces a framework for caching the test cases. The `DataCache` is two level:
    * A "L1" in-memory cache - backed by a `Map`.
    * A "L2" file cache, which can be pre-generated with an "offline" tool.
    
    This change introduces the new tool: `npm run gen_cache <dir> [suite...]`
    
    The idea here is that CTS testing infrastructure can pre-generate the test cases, and then reuse this data between the concurrent shards.
    
    With the shader expression tests updated to use the `CaseCache`, local macOS + chromium + `standalone` CTS testing has shown 2x+ speed improvement when running all the `webgpu:shader,execution,expression,*` tests.
    These performance gains will likely be reduced with multi-process execution, as the number of cache-misses will scale with the number of instances.

--- src/webgpu/util/f32_interval.ts
+++ src/webgpu/util/f32_interval.ts
@@ -2,6 +2,7 @@ import { assert, unreachable } from '../../common/util/util.js';
 import { Float16Array } from '../../external/petamoriken/float16/float16.js';
 
 import { kValue } from './constants.js';
+import { reinterpretF32AsU32, reinterpretU32AsF32 } from './conversion.js';
 import {
   cartesianProduct,
   correctlyRoundedF16,
@@ -97,6 +98,26 @@ export class F32Interval {
     }
     return this._any;
   }
+}
+
+/**
+ * SerializedF32Interval holds the serialized form of a F32Interval.
+ * This form can be safely encoded to JSON.
+ */
+export type SerializedF32Interval = { begin: number; end: number } | 'any';
+
+/** serializeF32Interval() converts a F32Interval to a SerializedF32Interval */
+export function serializeF32Interval(i: F32Interval): SerializedF32Interval {
+  return i === F32Interval.any()
+    ? 'any'
+    : { begin: reinterpretF32AsU32(i.begin), end: reinterpretF32AsU32(i.end) };
+}
+
+/** serializeF32Interval() converts a SerializedF32Interval to a F32Interval */
+export function deserializeF32Interval(data: SerializedF32Interval): F32Interval {
+  return data === 'any'
+    ? F32Interval.any()
+    : new F32Interval(reinterpretU32AsF32(data.begin), reinterpretU32AsF32(data.end));
 }
 
 /** @returns an interval containing the point or the original interval */
