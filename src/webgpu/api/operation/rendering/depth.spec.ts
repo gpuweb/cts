f20834c668eeb0693dae338d37603c7b6eea5e4a
Gyuyoung Kim <gyuyoung@igalia.com>
2022-12-07 16:43:20 +0900

    op: Implement 'depth_disabled' test in depth.spec.ts
    This PR adds a new test to check if rendering works correctly with depth testing disabled.
    
    Issue: #2023

--- src/webgpu/api/operation/rendering/depth.spec.ts
+++ src/webgpu/api/operation/rendering/depth.spec.ts
@@ -154,7 +154,43 @@ g.test('depth_bias')
   )
   .unimplemented();
 
-g.test('depth_disabled').desc(`Tests render results with depth test disabled`).unimplemented();
+g.test('depth_disabled')
+  .desc('Tests render results with depth test disabled.')
+  .fn(async t => {
+    const depthSpencilFormat: GPUTextureFormat = 'depth24plus-stencil8';
+
+    const stencilState = {
+      compare: 'always',
+      failOp: 'keep',
+      passOp: 'keep',
+    } as const;
+
+    const state = {
+      format: depthSpencilFormat,
+      depthWriteEnabled: false,
+      depthCompare: 'always',
+      stencilFront: stencilState,
+      stencilBack: stencilState,
+      stencilReadMask: 0xff,
+      stencilWriteMask: 0xff,
+    } as const;
+
+    const testStates = [
+      { state, color: kBaseColor, depth: 0.0 },
+      { state, color: kRedStencilColor, depth: 0.5 },
+      { state, color: kGreenStencilColor, depth: 1.0 },
+    ];
+
+    // Test that for all combinations and ensure the last triangle drawn is the one visible
+    // regardless of depth testing.
+    for (let last = 0; last < 3; ++last) {
+      const i = (last + 1) % 3;
+      const j = (last + 2) % 3;
+
+      t.runDepthStateTest([testStates[i], testStates[j], testStates[last]], testStates[last].color);
+      t.runDepthStateTest([testStates[j], testStates[i], testStates[last]], testStates[last].color);
+    }
+  });
 
 g.test('depth_write_disabled')
   .desc(
