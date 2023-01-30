3e8e0e3fe9497528c9f3203d7eece3ef28cc3a25
Antonio Maiorano <amaiorano@google.com>
2023-01-30 14:15:27 -0500

    Factor our u32 arithmetic functions to reduce copy-pasta

--- src/webgpu/shader/execution/expression/binary/u32_arithmetic.spec.ts
+++ src/webgpu/shader/execution/expression/binary/u32_arithmetic.spec.ts
@@ -17,243 +17,207 @@ import {
 
 import { binary } from './binary.js';
 
+function u32_add(x: number, y: number): number | undefined {
+  return x + y;
+}
+
+function u32_subtract(x: number, y: number): number | undefined {
+  return x - y;
+}
+
+function u32_multiply(x: number, y: number): number | undefined {
+  return Math.imul(x, y);
+}
+
+function u32_divide_non_const(x: number, y: number): number | undefined {
+  if (y === 0) {
+    return x;
+  }
+  return x / y;
+}
+
+function u32_divide_const(x: number, y: number): number | undefined {
+  if (y === 0) {
+    return undefined;
+  }
+  return x / y;
+}
+
+function u32_remainder_non_const(x: number, y: number): number | undefined {
+  if (y === 0) {
+    return 0;
+  }
+  return x % y;
+}
+
+function u32_remainder_const(x: number, y: number): number | undefined {
+  if (y === 0) {
+    return undefined;
+  }
+  return x % y;
+}
+
 export const g = makeTestGroup(GPUTest);
 
 export const d = makeCaseCache('binary/u32_arithmetic', {
   addition: () => {
-    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), (x, y) => {
-      return x + y;
-    });
+    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), u32_add);
   },
   subtraction: () => {
-    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), (x, y) => {
-      return x - y;
-    });
+    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), u32_subtract);
   },
   multiplication: () => {
-    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), (x, y) => {
-      return Math.imul(x, y);
-    });
+    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), u32_multiply);
   },
   division_non_const: () => {
-    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), (x, y) => {
-      if (y === 0) {
-        return x;
-      }
-      return x / y;
-    });
+    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), u32_divide_non_const);
   },
   division_const: () => {
-    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), (x, y) => {
-      if (y === 0) {
-        return undefined;
-      }
-      return x / y;
-    });
+    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), u32_divide_const);
   },
   remainder_non_const: () => {
-    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), (x, y) => {
-      if (y === 0) {
-        return 0;
-      }
-      return x % y;
-    });
+    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), u32_remainder_non_const);
   },
   remainder_const: () => {
-    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), (x, y) => {
-      if (y === 0) {
-        return undefined;
-      }
-      return x % y;
-    });
+    return generateBinaryToU32Cases(fullU32Range(), fullU32Range(), u32_remainder_const);
   },
   addition_scalar_vector2: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(2), (x, y) => {
-      return x + y;
-    });
+    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(2), u32_add);
   },
   addition_scalar_vector3: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(3), (x, y) => {
-      return x + y;
-    });
+    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(3), u32_add);
   },
   addition_scalar_vector4: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(4), (x, y) => {
-      return x + y;
-    });
+    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(4), u32_add);
   },
   addition_vector2_scalar: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(2), sparseU32Range(), (x, y) => {
-      return x + y;
-    });
+    return generateVectorU32BinaryToVectorCases(vectorU32Range(2), sparseU32Range(), u32_add);
   },
   addition_vector3_scalar: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(3), sparseU32Range(), (x, y) => {
-      return x + y;
-    });
+    return generateVectorU32BinaryToVectorCases(vectorU32Range(3), sparseU32Range(), u32_add);
   },
   addition_vector4_scalar: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(4), sparseU32Range(), (x, y) => {
-      return x + y;
-    });
+    return generateVectorU32BinaryToVectorCases(vectorU32Range(4), sparseU32Range(), u32_add);
   },
   subtraction_scalar_vector2: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(2), (x, y) => {
-      return x - y;
-    });
+    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(2), u32_subtract);
   },
   subtraction_scalar_vector3: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(3), (x, y) => {
-      return x - y;
-    });
+    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(3), u32_subtract);
   },
   subtraction_scalar_vector4: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(4), (x, y) => {
-      return x - y;
-    });
+    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(4), u32_subtract);
   },
   subtraction_vector2_scalar: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(2), sparseU32Range(), (x, y) => {
-      return x - y;
-    });
+    return generateVectorU32BinaryToVectorCases(vectorU32Range(2), sparseU32Range(), u32_subtract);
   },
   subtraction_vector3_scalar: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(3), sparseU32Range(), (x, y) => {
-      return x - y;
-    });
+    return generateVectorU32BinaryToVectorCases(vectorU32Range(3), sparseU32Range(), u32_subtract);
   },
   subtraction_vector4_scalar: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(4), sparseU32Range(), (x, y) => {
-      return x - y;
-    });
+    return generateVectorU32BinaryToVectorCases(vectorU32Range(4), sparseU32Range(), u32_subtract);
   },
   multiplication_scalar_vector2: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(2), (x, y) => {
-      return Math.imul(x, y);
-    });
+    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(2), u32_multiply);
   },
   multiplication_scalar_vector3: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(3), (x, y) => {
-      return Math.imul(x, y);
-    });
+    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(3), u32_multiply);
   },
   multiplication_scalar_vector4: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(4), (x, y) => {
-      return Math.imul(x, y);
-    });
+    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(4), u32_multiply);
   },
   multiplication_vector2_scalar: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(2), sparseU32Range(), (x, y) => {
-      return Math.imul(x, y);
-    });
+    return generateVectorU32BinaryToVectorCases(vectorU32Range(2), sparseU32Range(), u32_multiply);
   },
   multiplication_vector3_scalar: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(3), sparseU32Range(), (x, y) => {
-      return Math.imul(x, y);
-    });
+    return generateVectorU32BinaryToVectorCases(vectorU32Range(3), sparseU32Range(), u32_multiply);
   },
   multiplication_vector4_scalar: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(4), sparseU32Range(), (x, y) => {
-      return Math.imul(x, y);
-    });
+    return generateVectorU32BinaryToVectorCases(vectorU32Range(4), sparseU32Range(), u32_multiply);
   },
-
   division_scalar_vector2_non_const: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(2), (x, y) => {
-      if (y === 0) {
-        return x;
-      }
-      return x / y;
-    });
+    return generateU32VectorBinaryToVectorCases(
+      sparseU32Range(),
+      vectorU32Range(2),
+      u32_divide_non_const
+    );
   },
   division_scalar_vector3_non_const: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(3), (x, y) => {
-      if (y === 0) {
-        return x;
-      }
-      return x / y;
-    });
+    return generateU32VectorBinaryToVectorCases(
+      sparseU32Range(),
+      vectorU32Range(3),
+      u32_divide_non_const
+    );
   },
   division_scalar_vector4_non_const: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(4), (x, y) => {
-      if (y === 0) {
-        return x;
-      }
-      return x / y;
-    });
+    return generateU32VectorBinaryToVectorCases(
+      sparseU32Range(),
+      vectorU32Range(4),
+      u32_divide_non_const
+    );
   },
   division_vector2_scalar_non_const: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(2), sparseU32Range(), (x, y) => {
-      if (y === 0) {
-        return x;
-      }
-      return x / y;
-    });
+    return generateVectorU32BinaryToVectorCases(
+      vectorU32Range(2),
+      sparseU32Range(),
+      u32_divide_non_const
+    );
   },
   division_vector3_scalar_non_const: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(3), sparseU32Range(), (x, y) => {
-      if (y === 0) {
-        return x;
-      }
-      return x / y;
-    });
+    return generateVectorU32BinaryToVectorCases(
+      vectorU32Range(3),
+      sparseU32Range(),
+      u32_divide_non_const
+    );
   },
   division_vector4_scalar_non_const: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(4), sparseU32Range(), (x, y) => {
-      if (y === 0) {
-        return x;
-      }
-      return x / y;
-    });
+    return generateVectorU32BinaryToVectorCases(
+      vectorU32Range(4),
+      sparseU32Range(),
+      u32_divide_non_const
+    );
   },
-
   division_scalar_vector2_const: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(2), (x, y) => {
-      if (y === 0) {
-        return undefined;
-      }
-      return x / y;
-    });
+    return generateU32VectorBinaryToVectorCases(
+      sparseU32Range(),
+      vectorU32Range(2),
+      u32_divide_const
+    );
   },
   division_scalar_vector3_const: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(3), (x, y) => {
-      if (y === 0) {
-        return undefined;
-      }
-      return x / y;
-    });
+    return generateU32VectorBinaryToVectorCases(
+      sparseU32Range(),
+      vectorU32Range(3),
+      u32_divide_const
+    );
   },
   division_scalar_vector4_const: () => {
-    return generateU32VectorBinaryToVectorCases(sparseU32Range(), vectorU32Range(4), (x, y) => {
-      if (y === 0) {
-        return undefined;
-      }
-      return x / y;
-    });
+    return generateU32VectorBinaryToVectorCases(
+      sparseU32Range(),
+      vectorU32Range(4),
+      u32_divide_const
+    );
   },
   division_vector2_scalar_const: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(2), sparseU32Range(), (x, y) => {
-      if (y === 0) {
-        return undefined;
-      }
-      return x / y;
-    });
+    return generateVectorU32BinaryToVectorCases(
+      vectorU32Range(2),
+      sparseU32Range(),
+      u32_divide_const
+    );
   },
   division_vector3_scalar_const: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(3), sparseU32Range(), (x, y) => {
-      if (y === 0) {
-        return undefined;
-      }
-      return x / y;
-    });
+    return generateVectorU32BinaryToVectorCases(
+      vectorU32Range(3),
+      sparseU32Range(),
+      u32_divide_const
+    );
   },
   division_vector4_scalar_const: () => {
-    return generateVectorU32BinaryToVectorCases(vectorU32Range(4), sparseU32Range(), (x, y) => {
-      if (y === 0) {
-        return undefined;
-      }
-      return x / y;
-    });
+    return generateVectorU32BinaryToVectorCases(
+      vectorU32Range(4),
+      sparseU32Range(),
+      u32_divide_const
+    );
   },
 });
 
