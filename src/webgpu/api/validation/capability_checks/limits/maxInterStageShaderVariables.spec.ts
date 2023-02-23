6b907177990df8adfb58a3679e5a738a4c925c70
Gregg Tavares <github@greggman.com>
2023-02-23 10:34:52 -0800

    Test maxInterStageShaderComponents and maxInterStageShaderVariables

---
+++ src/webgpu/api/validation/capability_checks/limits/maxInterStageShaderVariables.spec.ts
@@ -0,0 +1,66 @@
+import { kLimitBaseParams, makeLimitTestGroup } from './limit_utils.js';
+
+function getPipelineDescriptor(device: GPUDevice, testValue: number): GPURenderPipelineDescriptor {
+  const code = `
+    struct VSOut {
+      @builtin(position) p: vec4f,
+      @location(${testValue}) v: f32,
+    }
+    @vertex fn vs() -> VSOut {
+      var o: VSOut;
+      o.p = vec4f(0);
+      o.v = 1.0;
+      return o;
+    }
+  `;
+  const module = device.createShaderModule({ code });
+  return {
+    layout: 'auto',
+    vertex: {
+      module,
+      entryPoint: 'vs',
+    },
+  };
+}
+
+const limit = 'maxInterStageShaderVariables';
+export const { g, description } = makeLimitTestGroup(limit);
+
+g.test('createRenderPipeline,at_over')
+  .desc(`Test using at and over ${limit} limit in createRenderPipeline`)
+  .params(kLimitBaseParams)
+  .fn(async t => {
+    const { limitTest, testValueName } = t.params;
+    await t.testDeviceWithRequestedLimits(
+      limitTest,
+      testValueName,
+      async ({ device, testValue, shouldError }) => {
+        const lastIndex = testValue - 1;
+        const pipelineDescriptor = getPipelineDescriptor(device, lastIndex);
+
+        await t.expectValidationError(() => {
+          device.createRenderPipeline(pipelineDescriptor);
+        }, shouldError);
+      }
+    );
+  });
+
+g.test('createRenderPipelineAsync,at_over')
+  .desc(`Test using at and over ${limit} limit in createRenderPipelineAsync`)
+  .params(kLimitBaseParams)
+  .fn(async t => {
+    const { limitTest, testValueName } = t.params;
+    await t.testDeviceWithRequestedLimits(
+      limitTest,
+      testValueName,
+      async ({ device, testValue, shouldError }) => {
+        const lastIndex = testValue - 1;
+        const pipelineDescriptor = getPipelineDescriptor(device, lastIndex);
+        await t.shouldRejectConditionally(
+          'GPUPipelineError',
+          device.createRenderPipelineAsync(pipelineDescriptor),
+          shouldError
+        );
+      }
+    );
+  });
