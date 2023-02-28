820b200241416a0dbc37fce4643b7f3614b57f66
Gregg Tavares <github@greggman.com>
2023-02-28 09:58:35 -0800

    Fix WGSL warning for maxStorageTexturesPerShaderStage.spec.ts

--- src/webgpu/api/validation/capability_checks/limits/limit_utils.ts
+++ src/webgpu/api/validation/capability_checks/limits/limit_utils.ts
@@ -223,7 +223,7 @@ export function getPerStageWGSLForBindingCombinationStorageTextures(
     (numBindings: number, set: number) => {
       return bindingCombination === 'compute'
         ? `${range(numBindings, i => usageWGSLSnippetFn(i, set)).join('\n')};`
-        : `${range(numBindings, i => usageWGSLSnippetFn(i, set)).join('\n')}; return vec4f(0);`;
+        : `${range(numBindings, i => usageWGSLSnippetFn(i, set)).join('\n')};`;
     },
     numBindings,
     extraWGSL
