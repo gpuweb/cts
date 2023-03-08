22896445c99fc04f0f1c8404a1c9d5c74025ec4c
shaoboyan <shaobo.yan@intel.com>
2023-03-08 02:38:48 +0000

    Deploying to gh-pages from @ gpuweb/cts@9179cd43998ef3470e7c1ba1c0361e457306c8df 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'fb969c949619123eafe842fd279c054992b8d638';
+export const version = '9179cd43998ef3470e7c1ba1c0361e457306c8df';
