99d11d8864eadf5a13f447fb1637c09377882ee7
Yan, Shaobo <shaobo.yan@intel.com>
2023-03-14 14:46:57 +0800

    Add test case about copying HTMLVideoElement to texture (#2018)
    * Add test case about copying HTMLVideoElement to texture
    
    This PR add a base test about copying HTMLVideoElement to texture.
    It also moves some helper functions from web_platfrom/external_texture/video.spec.ts
    to web_platform/util.ts for reusing.
    
    * Address comments
    
    * Cleanup TODOs

--- src/webgpu/web_platform/util.ts
+++ src/webgpu/web_platform/util.ts
@@ -29,6 +29,96 @@ export const kVideoInfo = /* prettier-ignore */ makeTable(
 } as const);
 export type VideoName = keyof typeof kVideoInfo;
 
+// Expectation values about converting video contents to sRGB color space.
+// Source video color space affects expected values.
+// The process to calculate these expected pixel values can be found:
+// https://github.com/gpuweb/cts/pull/2242#issuecomment-1430382811
+// and https://github.com/gpuweb/cts/pull/2242#issuecomment-1463273434
+const kBt601PixelValue = {
+  red: new Float32Array([0.972945567233341, 0.141794376683341, -0.0209589916711088, 1.0]),
+  green: new Float32Array([0.248234279433399, 0.984810378661784, -0.0564701319494314, 1.0]),
+  blue: new Float32Array([0.10159735826538, 0.135451122863674, 1.00262982899724, 1.0]),
+  yellow: new Float32Array([0.995470750775951, 0.992742114518355, -0.0774291236205402, 1.0]),
+};
+
+function convertToUnorm8(expectation: Float32Array): Uint8Array {
+  const unorm8 = new Uint8ClampedArray(expectation.length);
+
+  for (let i = 0; i < expectation.length; ++i) {
+    unorm8[i] = Math.round(expectation[i] * 255.0);
+  }
+
+  return new Uint8Array(unorm8.buffer);
+}
+
+// kVideoExpectations uses unorm8 results
+const kBt601Red = convertToUnorm8(kBt601PixelValue.red);
+const kBt601Green = convertToUnorm8(kBt601PixelValue.green);
+const kBt601Blue = convertToUnorm8(kBt601PixelValue.blue);
+const kBt601Yellow = convertToUnorm8(kBt601PixelValue.yellow);
+
+export const kVideoExpectations = [
+  {
+    videoName: 'four-colors-vp8-bt601.webm',
+    _redExpectation: kBt601Red,
+    _greenExpectation: kBt601Green,
+    _blueExpectation: kBt601Blue,
+    _yellowExpectation: kBt601Yellow,
+  },
+  {
+    videoName: 'four-colors-theora-bt601.ogv',
+    _redExpectation: kBt601Red,
+    _greenExpectation: kBt601Green,
+    _blueExpectation: kBt601Blue,
+    _yellowExpectation: kBt601Yellow,
+  },
+  {
+    videoName: 'four-colors-h264-bt601.mp4',
+    _redExpectation: kBt601Red,
+    _greenExpectation: kBt601Green,
+    _blueExpectation: kBt601Blue,
+    _yellowExpectation: kBt601Yellow,
+  },
+  {
+    videoName: 'four-colors-vp9-bt601.webm',
+    _redExpectation: kBt601Red,
+    _greenExpectation: kBt601Green,
+    _blueExpectation: kBt601Blue,
+    _yellowExpectation: kBt601Yellow,
+  },
+  {
+    videoName: 'four-colors-vp9-bt709.webm',
+    _redExpectation: new Uint8Array([255, 0, 0, 255]),
+    _greenExpectation: new Uint8Array([0, 255, 0, 255]),
+    _blueExpectation: new Uint8Array([0, 0, 255, 255]),
+    _yellowExpectation: new Uint8Array([255, 255, 0, 255]),
+  },
+] as const;
+
+export const kVideoRotationExpectations = [
+  {
+    videoName: 'four-colors-h264-bt601-rotate-90.mp4',
+    _topLeftExpectation: kBt601Red,
+    _topRightExpectation: kBt601Green,
+    _bottomLeftExpectation: kBt601Yellow,
+    _bottomRightExpectation: kBt601Blue,
+  },
+  {
+    videoName: 'four-colors-h264-bt601-rotate-180.mp4',
+    _topLeftExpectation: kBt601Green,
+    _topRightExpectation: kBt601Blue,
+    _bottomLeftExpectation: kBt601Red,
+    _bottomRightExpectation: kBt601Yellow,
+  },
+  {
+    videoName: 'four-colors-h264-bt601-rotate-270.mp4',
+    _topLeftExpectation: kBt601Blue,
+    _topRightExpectation: kBt601Yellow,
+    _bottomLeftExpectation: kBt601Green,
+    _bottomRightExpectation: kBt601Red,
+  },
+] as const;
+
 /**
  * Starts playing a video and waits for it to be consumable.
  * Returns a promise which resolves after `callback` (which may be async) completes.
