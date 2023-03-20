db3b8daf48e7dc782ef71f72f17f638c74514ac3
Antonio Maiorano <amaiorano@google.com>
2023-03-20 16:49:58 -0400

    Validate that atomic builtins are only valid in compute and fragment shaders (#2442)
    Moved the stubbed 'stage' tests under validation, and combined them to be able to parameterize them.

--- src/webgpu/shader/execution/expression/call/builtin/atomicCompareExchangeWeak.spec.ts
+++ src/webgpu/shader/execution/expression/call/builtin/atomicCompareExchangeWeak.spec.ts
@@ -18,24 +18,14 @@ import { GPUTest } from '../../../../../gpu_test.js';
 
 export const g = makeTestGroup(GPUTest);
 
-g.test('stage')
-  .specURL('https://www.w3.org/TR/WGSL/#atomic-rmw')
-  .desc(
-    `
-Atomic built-in functions must not be used in a vertex shader stage.
-`
-  )
-  .params(u => u.combine('stage', ['fragment', 'vertex', 'compute'] as const))
-  .unimplemented();
-
 g.test('exchange')
   .specURL('https://www.w3.org/TR/WGSL/#atomic-rmw')
   .desc(
     `
-SC is storage or workgroup
+AS is storage or workgroup
 T is i32 or u32
 
-fn atomicCompareExchangeWeak(atomic_ptr: ptr<SC, atomic<T>, read_write>, cmp: T, v: T) -> __atomic_compare_exchange_result<T>
+fn atomicCompareExchangeWeak(atomic_ptr: ptr<AS, atomic<T>, read_write>, cmp: T, v: T) -> __atomic_compare_exchange_result<T>
 
 struct __atomic_compare_exchange_result<T> {
   old_value : T,    // old value stored in the atomic
--- src/webgpu/shader/execution/expression/call/builtin/atomicLoad.spec.ts
+++ src/webgpu/shader/execution/expression/call/builtin/atomicLoad.spec.ts
@@ -7,24 +7,14 @@ import { GPUTest } from '../../../../../gpu_test.js';
 
 export const g = makeTestGroup(GPUTest);
 
-g.test('stage')
-  .specURL('https://www.w3.org/TR/WGSL/#atomic-load')
-  .desc(
-    `
-Atomic built-in functions must not be used in a vertex shader stage.
-`
-  )
-  .params(u => u.combine('stage', ['fragment', 'vertex', 'compute'] as const))
-  .unimplemented();
-
 g.test('load')
   .specURL('https://www.w3.org/TR/WGSL/#atomic-load')
   .desc(
     `
-SC is storage or workgroup
+AS is storage or workgroup
 T is i32 or u32
 
-fn atomicLoad(atomic_ptr: ptr<SC, atomic<T>, read_write>) -> T
+fn atomicLoad(atomic_ptr: ptr<AS, atomic<T>, read_write>) -> T
 
 `
   )
