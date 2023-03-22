dd474b33f85d63f950fedef2ba889d914c58652d
Ryan Harrison <rharrison@google.com>
2023-03-22 15:17:48 -0400

    Update tests for min/max spec changes (#2455)
    If both inputs to min and max are subnormal either value may be
    returned. This has knock on effects for the min-max formulation of
    clamp and saturation.
    
    Fixes #2403

--- src/webgpu/util/math.ts
+++ src/webgpu/util/math.ts
@@ -39,7 +39,7 @@ export function flushSubnormalNumberF32(val: number): number {
   return isSubnormalNumberF32(val) ? 0 : val;
 }
 
-/** U/** @returns if number is within subnormal range of f32 */
+/** @returns if number is within subnormal range of f32 */
 export function isSubnormalNumberF32(n: number): boolean {
   return n > kValue.f32.negative.max && n < kValue.f32.positive.min;
 }
