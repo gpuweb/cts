d33a5df322b58eb46156e549233b2e44526ab6de
greggman <github@greggman.com>
2023-03-30 19:49:06 +0000

    Deploying to gh-pages from @ gpuweb/cts@552fe1d464863f2346d16d4fae4c85682e22f3a3 🚀

--- out-wpt/webgpu/api/operation/command_buffer/queries/occlusionQuery.spec.js
+++ out-wpt/webgpu/api/operation/command_buffer/queries/occlusionQuery.spec.js
@@ -421,7 +421,8 @@ class OcclusionQueryTest extends GPUTest {
 
     const result = await this.readBufferAsBigUint64(readBuffer);
     for (const queryIndex of queryIndices) {
-      const passed = !!result[queryIndex];
+      const resultNdx = queryIndex - querySetOffset;
+      const passed = !!result[resultNdx];
       checkQueryIndexResultFn(passed, queryIndex);
     }
 
@@ -859,18 +860,28 @@ g.test('occlusion_query,alpha_to_coverage')
     ];
 
     const numPassedPerGroup = new Array(kNumQueries / 4).fill(0);
+
+    // These tests can't use queryIndex to decide what to draw because which mask
+    // a particular alpha converts to is implementation defined. When querySetOffset is
+    // non-zero the queryIndex will go 7, 8, 9, 10, ... but we need to guarantee
+    // 4 queries per pixel and group those results so `queryIndex / 4 | 0` won't work.
+    // Instead we count the queries to get 4 draws per group, one to each quadrant of a pixel
+    // Then we total up the passes for those 4 queries by queryCount.
+    let queryCount = 0;
+    let resultCount = 0;
     await t.runQueryTest(
       resources,
       renderPassDescriptor,
       (helper, queryIndex) => {
         const queryHelper = helper.beginOcclusionQuery(queryIndex);
         queryHelper.setPipeline(pipeline);
-        queryHelper.setVertexBuffer(vertexBuffers[queryIndex % 4]);
+        queryHelper.setVertexBuffer(vertexBuffers[queryCount++ % 4]);
         queryHelper.draw(6);
         queryHelper.end();
       },
-      (passed, queryIndex) => {
-        numPassedPerGroup[(queryIndex / 4) | 0] += passed ? 1 : 0;
+      passed => {
+        const groupIndex = (resultCount++ / 4) | 0;
+        numPassedPerGroup[groupIndex] += passed ? 1 : 0;
       }
     );
 
--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '3182e81ad0c69ea004613a67d0033a0d1b585e96';
+export const version = '552fe1d464863f2346d16d4fae4c85682e22f3a3';
