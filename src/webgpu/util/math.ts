a75df214dddb2940076d37600a8e276c1611daa3
Ryan Harrison <rharrison@google.com>
2023-04-26 09:48:20 -0400

    Replace hexToFoo function names with more accuracte names (#2502)
    Fixes #2500

--- src/webgpu/util/math.ts
+++ src/webgpu/util/math.ts
@@ -680,7 +680,7 @@ export function fullF32Range(
     ),
     ...linearRange(kBit.f32.positive.min, kBit.f32.positive.max, counts.pos_norm),
   ].map(Math.trunc);
-  return bit_fields.map(hexToF32);
+  return bit_fields.map(reinterpretU32AsF32);
 }
 
 /**
@@ -744,7 +744,7 @@ export function fullF16Range(
     ),
     ...linearRange(kBit.f16.positive.min, kBit.f16.positive.max, counts.pos_norm),
   ].map(Math.trunc);
-  return bit_fields.map(hexToF16);
+  return bit_fields.map(reinterpretU16AsF16);
 }
 
 /** Short list of i32 values of interest to test against */
@@ -1208,19 +1208,28 @@ export function lcm(a: number, b: number): number {
   return (a * b) / gcd(a, b);
 }
 
-/** Converts a 32-bit hex value to a 32-bit float value */
-export function hexToF32(hex: number): number {
-  return floatBitsToNumber(hex, kFloat32Format);
+/**
+ * @returns a 64-bit float value via interpreting the input as the bit
+ * representation as a 64-bit integer
+ */
+export function reinterpretU64AsF64(input: bigint): number {
+  return new Float64Array(new BigInt64Array([input]).buffer)[0];
 }
 
-/** Converts a 16-bit hex value to a 16-bit float value */
-export function hexToF16(hex: number): number {
+/**
+ * @returns a 32-bit float value via interpreting the input as the bit
+ * representation as a 32-bit integer
+ */
+export function reinterpretU32AsF32(input: number): number {
+  return floatBitsToNumber(input, kFloat32Format);
+}
+
+/**
+ * @returns a 16-bit float value via interpreting the input as the bit
+ * representation as a 16-bit integer
+ */
+export function reinterpretU16AsF16(hex: number): number {
   return floatBitsToNumber(hex, kFloat16Format);
-}
-
-/** Converts 64-bit hex value to a 64-bit float value */
-export function hexToF64(hex: bigint): number {
-  return new Float64Array(new BigInt64Array([hex]).buffer)[0];
 }
 
 /** @returns the cross of an array with the intermediate result of cartesianProduct
