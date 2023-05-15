6ac65a373b810a95f5a9afce513bca6f8eaeb104
Ryan Harrison <rharrison@google.com>
2023-05-15 13:36:53 -0400

    Switch to using sparseF32Range in f32 arithmetic tests (#2614)
    Fixes #2613

--- src/webgpu/shader/execution/expression/binary/f32_addition.spec.ts
+++ src/webgpu/shader/execution/expression/binary/f32_addition.spec.ts
@@ -6,7 +6,7 @@ import { makeTestGroup } from '../../../../../common/framework/test_group.js';
 import { GPUTest } from '../../../../gpu_test.js';
 import { TypeF32, TypeVec } from '../../../../util/conversion.js';
 import { FP, FPVector } from '../../../../util/floating_point.js';
-import { fullF32Range, sparseVectorF32Range } from '../../../../util/math.js';
+import { sparseF32Range, sparseVectorF32Range } from '../../../../util/math.js';
 import { makeCaseCache } from '../case_cache.js';
 import { allInputSources, run } from '../expression.js';
 
@@ -25,16 +25,16 @@ export const g = makeTestGroup(GPUTest);
 export const d = makeCaseCache('binary/f32_addition', {
   scalar_const: () => {
     return FP.f32.generateScalarPairToIntervalCases(
-      fullF32Range(),
-      fullF32Range(),
+      sparseF32Range(),
+      sparseF32Range(),
       'finite',
       FP.f32.additionInterval
     );
   },
   scalar_non_const: () => {
     return FP.f32.generateScalarPairToIntervalCases(
-      fullF32Range(),
-      fullF32Range(),
+      sparseF32Range(),
+      sparseF32Range(),
       'unfiltered',
       FP.f32.additionInterval
     );
@@ -42,7 +42,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   vec2_scalar_const: () => {
     return FP.f32.generateVectorScalarToVectorCases(
       sparseVectorF32Range(2),
-      fullF32Range(),
+      sparseF32Range(),
       'finite',
       additionVectorScalarInterval
     );
@@ -50,7 +50,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   vec2_scalar_non_const: () => {
     return FP.f32.generateVectorScalarToVectorCases(
       sparseVectorF32Range(2),
-      fullF32Range(),
+      sparseF32Range(),
       'unfiltered',
       additionVectorScalarInterval
     );
@@ -58,7 +58,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   vec3_scalar_const: () => {
     return FP.f32.generateVectorScalarToVectorCases(
       sparseVectorF32Range(3),
-      fullF32Range(),
+      sparseF32Range(),
       'finite',
       additionVectorScalarInterval
     );
@@ -66,7 +66,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   vec3_scalar_non_const: () => {
     return FP.f32.generateVectorScalarToVectorCases(
       sparseVectorF32Range(3),
-      fullF32Range(),
+      sparseF32Range(),
       'unfiltered',
       additionVectorScalarInterval
     );
@@ -74,7 +74,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   vec4_scalar_const: () => {
     return FP.f32.generateVectorScalarToVectorCases(
       sparseVectorF32Range(4),
-      fullF32Range(),
+      sparseF32Range(),
       'finite',
       additionVectorScalarInterval
     );
@@ -82,14 +82,14 @@ export const d = makeCaseCache('binary/f32_addition', {
   vec4_scalar_non_const: () => {
     return FP.f32.generateVectorScalarToVectorCases(
       sparseVectorF32Range(4),
-      fullF32Range(),
+      sparseF32Range(),
       'unfiltered',
       additionVectorScalarInterval
     );
   },
   scalar_vec2_const: () => {
     return FP.f32.generateScalarVectorToVectorCases(
-      fullF32Range(),
+      sparseF32Range(),
       sparseVectorF32Range(2),
       'finite',
       additionScalarVectorInterval
@@ -97,7 +97,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   },
   scalar_vec2_non_const: () => {
     return FP.f32.generateScalarVectorToVectorCases(
-      fullF32Range(),
+      sparseF32Range(),
       sparseVectorF32Range(2),
       'unfiltered',
       additionScalarVectorInterval
@@ -105,7 +105,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   },
   scalar_vec3_const: () => {
     return FP.f32.generateScalarVectorToVectorCases(
-      fullF32Range(),
+      sparseF32Range(),
       sparseVectorF32Range(3),
       'finite',
       additionScalarVectorInterval
@@ -113,7 +113,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   },
   scalar_vec3_non_const: () => {
     return FP.f32.generateScalarVectorToVectorCases(
-      fullF32Range(),
+      sparseF32Range(),
       sparseVectorF32Range(3),
       'unfiltered',
       additionScalarVectorInterval
@@ -121,7 +121,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   },
   scalar_vec4_const: () => {
     return FP.f32.generateScalarVectorToVectorCases(
-      fullF32Range(),
+      sparseF32Range(),
       sparseVectorF32Range(4),
       'finite',
       additionScalarVectorInterval
@@ -129,7 +129,7 @@ export const d = makeCaseCache('binary/f32_addition', {
   },
   scalar_vec4_non_const: () => {
     return FP.f32.generateScalarVectorToVectorCases(
-      fullF32Range(),
+      sparseF32Range(),
       sparseVectorF32Range(4),
       'unfiltered',
       additionScalarVectorInterval
@@ -137,8 +137,8 @@ export const d = makeCaseCache('binary/f32_addition', {
   },
   subtraction_const: () => {
     return FP.f32.generateScalarPairToIntervalCases(
-      fullF32Range(),
-      fullF32Range(),
+      sparseF32Range(),
+      sparseF32Range(),
       'finite',
       FP.f32.subtractionInterval
     );
