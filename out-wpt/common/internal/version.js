ef7541b2751a112287d0a267a80cce3c6ab7ebed
ErichDonGubler <erichdongubler@gmail.com>
2023-05-24 01:42:55 +0000

    Deploying to gh-pages from @ gpuweb/cts@71d6a776334768915304338e2269b25fc176cea1 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '6b636a9cf3ce9f2c1c144b2dd8b8d5c6801c4dc3';
+export const version = '71d6a776334768915304338e2269b25fc176cea1';
