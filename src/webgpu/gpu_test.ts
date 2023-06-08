066aaeb07777421db296fd5d1839a08d60a40557
Gregg Tavares <github@greggman.com>
2023-06-08 16:08:34 -0700

    Skip test if compat mode
    Here's one example of skipping a test that is expected
    to fail in compat.

--- src/webgpu/gpu_test.ts
+++ src/webgpu/gpu_test.ts
@@ -7,6 +7,7 @@ import {
   TestCaseRecorder,
   TestParams,
 } from '../common/framework/fixture.js';
+import { globalTestConfig } from '../common/framework/test_config.js';
 import {
   assert,
   range,
@@ -207,6 +208,10 @@ export class GPUTestBase extends Fixture<GPUTestSubcaseBatchState> {
   /** GPUQueue for the test to use. (Same as `t.device.queue`.) */
   get queue(): GPUQueue {
     return this.device.queue;
+  }
+
+  get isCompatibility() {
+    return globalTestConfig.compatibility;
   }
 
   /** Snapshot a GPUBuffer's contents, returning a new GPUBuffer with the `MAP_READ` usage. */
