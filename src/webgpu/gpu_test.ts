ab3498debbcc16e81291c431d60be02dd800b1d6
Greggman <github@greggman.com>
2023-06-22 18:11:15 -0700

    Compat: Skip bgra8uorm-srgb in test (#2781)
    bgra8unorm-srgb is not supported in compat

--- src/webgpu/gpu_test.ts
+++ src/webgpu/gpu_test.ts
@@ -3,6 +3,7 @@ import {
   FixtureClass,
   FixtureClassInterface,
   FixtureClassWithMixin,
+  SkipTestCase,
   SubcaseBatchState,
   TestCaseRecorder,
   TestParams,
@@ -107,6 +108,10 @@ export class GPUTestSubcaseBatchState extends SubcaseBatchState {
     return this.provider;
   }
 
+  get isCompatibility() {
+    return globalTestConfig.compatibility;
+  }
+
   /**
    * Some tests or cases need particular feature flags or limits to be enabled.
    * Call this function with a descriptor or feature name (or `undefined`) to select a
@@ -182,6 +187,24 @@ export class GPUTestSubcaseBatchState extends SubcaseBatchState {
     );
     // Suppress uncaught promise rejection (we'll catch it later).
     this.mismatchedProvider.catch(() => {});
+  }
+
+  /** Throws an exception marking the subcase as skipped. */
+  skip(msg: string): never {
+    throw new SkipTestCase(msg);
+  }
+
+  /**
+   * Skips test if any format is not supported.
+   */
+  skipIfTextureFormatNotSupported(...formats: GPUTextureFormat[]) {
+    if (this.isCompatibility) {
+      for (const format of formats) {
+        if (format === 'bgra8unorm-srgb') {
+          this.skip(`texture format '${format} is not supported`);
+        }
+      }
+    }
   }
 }
 
@@ -307,6 +330,19 @@ export class GPUTestBase extends Fixture<GPUTestSubcaseBatchState> {
         mappable.destroy();
       },
     };
+  }
+
+  /**
+   * Skips test if any format is not supported.
+   */
+  skipIfTextureFormatNotSupported(...formats: GPUTextureFormat[]) {
+    if (this.isCompatibility) {
+      for (const format of formats) {
+        if (format === 'bgra8unorm-srgb') {
+          this.skip(`texture format '${format} is not supported`);
+        }
+      }
+    }
   }
 
   /**
