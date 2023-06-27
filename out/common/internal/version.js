6bd07934765d3d47c57a3b178734a4263cf53d4f
alan-baker <alanbaker@google.com>
2023-06-27 14:55:51 +0000

    Deploying to gh-pages from @ gpuweb/cts@768732af326ad32ef8d0bc6781f06da73c3c12c4 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '086261bcd213f8ae6f57ebdc293283ed6262097d';
+export const version = '768732af326ad32ef8d0bc6781f06da73c3c12c4';
