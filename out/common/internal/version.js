f70f3a33bdcdc09bf9688b64767979c81f200ba6
dj2 <dj2@everburning.com>
2023-07-05 21:02:21 +0000

    Deploying to gh-pages from @ gpuweb/cts@502d88bb99a153cbba5a5d6eb6e0f406adb91a86 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '4727ca4b6361396a9014573e922c03c9f27bfd54';
+export const version = '502d88bb99a153cbba5a5d6eb6e0f406adb91a86';
