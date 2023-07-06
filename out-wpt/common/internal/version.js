e0d62dd9399a0df50d67a4ded89f1d67b69e595a
dj2 <dj2@everburning.com>
2023-07-06 15:34:13 +0000

    Deploying to gh-pages from @ gpuweb/cts@83b59c809799491a309f9b8be28cd8a3cf66c317 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'c794da9f5d01232579bb129acac0596d6fbf2151';
+export const version = '83b59c809799491a309f9b8be28cd8a3cf66c317';
--- out-wpt/webgpu/shader/validation/shader_io/workgroup_size.spec.js
+++ out-wpt/webgpu/shader/validation/shader_io/workgroup_size.spec.js
@@ -20,6 +20,18 @@ const kWorkgroupSizeTests = {
     src: `@workgroup_size(8, 8, 8f)`,
     pass: false,
   },
+  x_only_float_literal: {
+    src: `@workgroup_size(8.0)`,
+    pass: false,
+  },
+  xy_only_float_literal: {
+    src: `@workgroup_size(8, 8.0)`,
+    pass: false,
+  },
+  xyz_float_literal: {
+    src: `@workgroup_size(8, 8, 8.0)`,
+    pass: false,
+  },
   empty: {
     src: `@workgroup_size()`,
     pass: false,
@@ -71,6 +83,18 @@ const kWorkgroupSizeTests = {
   },
   xyz_signed: {
     src: `@workgroup_size(8i, 8i, 8i)`,
+    pass: true,
+  },
+  x_only_hex: {
+    src: `@workgroup_size(0x1)`,
+    pass: true,
+  },
+  xy_only_hex: {
+    src: `@workgroup_size(0x1, 0x1)`,
+    pass: true,
+  },
+  xyz_hex: {
+    src: `@workgroup_size(0x1, 0x1, 0x1)`,
     pass: true,
   },
 
@@ -156,6 +180,50 @@ const kWorkgroupSizeTests = {
     src: `@workgroup_size(256, 256, 64)`,
     pass: true,
   },
+
+  missing_left_paren: {
+    src: `@workgroup_size 1, 2, 3)`,
+    pass: false,
+  },
+  missing_right_paren: {
+    src: `@workgroup_size(1, 2, 3`,
+    pass: false,
+  },
+  misspelling: {
+    src: `@aworkgroup_size(1)`,
+    pass: false,
+  },
+  no_params: {
+    src: `@workgroup_size`,
+    pass: false,
+  },
+  multi_line: {
+    src: '@\nworkgroup_size(1)',
+    pass: true,
+  },
+  comment: {
+    src: `@/* comment */workgroup_size(1)`,
+    pass: true,
+  },
+
+  mix_ux: {
+    src: `@workgroup_size(1u, 1i, 1i)`,
+    pass: false,
+  },
+  mix_uy: {
+    src: `@workgroup_size(1i, 1u, 1i)`,
+    pass: false,
+  },
+  mix_uz: {
+    src: `@workgroup_size(1i, 1i, 1u)`,
+    pass: false,
+  },
+
+  duplicate: {
+    src: `@workgroup_size(1)
+@workgroup_size(2, 2, 2)`,
+    pass: false,
+  },
 };
 g.test('workgroup_size')
   .desc(`Test validation of workgroup_size`)
@@ -165,4 +233,69 @@ g.test('workgroup_size')
 ${kWorkgroupSizeTests[t.params.attr].src}
 @compute fn main() {}`;
     t.expectCompileResult(kWorkgroupSizeTests[t.params.attr].pass, code);
+  });
+
+g.test('workgroup_size_fragment_shader')
+  .desc(`Test validation of workgroup_size on a fragment shader`)
+  .fn(t => {
+    const code = `
+@workgroup_size(1)
+@fragment fn main(@builtin(position) pos: vec4<f32>) {}`;
+    t.expectCompileResult(false, code);
+  });
+
+g.test('workgroup_size_vertex_shader')
+  .desc(`Test validation of workgroup_size on a vertex shader`)
+  .fn(t => {
+    const code = `
+@workgroup_size(1)
+@vertex fn main() -> @builtin(position) vec4<f32> {}`;
+    t.expectCompileResult(false, code);
+  });
+
+g.test('workgroup_size_function')
+  .desc(`Test validation of workgroup_size on user function`)
+  .fn(t => {
+    const code = `
+@workgroup_size(1)
+fn my_func() {}`;
+    t.expectCompileResult(false, code);
+  });
+
+g.test('workgroup_size_const')
+  .desc(`Test validation of workgroup_size on a const`)
+  .fn(t => {
+    const code = `
+@workgroup_size(1)
+const a : i32 = 4;
+
+fn my_func() {}`;
+    t.expectCompileResult(false, code);
+  });
+
+g.test('workgroup_size_var')
+  .desc(`Test validation of workgroup_size on a var`)
+  .fn(t => {
+    const code = `
+@workgroup_size(1)
+@group(1) @binding(1)
+var<storage> a: i32;
+
+fn my_func() {
+  _ = a;
+}`;
+    t.expectCompileResult(false, code);
+  });
+
+g.test('workgroup_size_fp16')
+  .desc(`Test validation of workgroup_size with fp16`)
+  .params(u => u.combine('ext', ['', 'h']))
+  .beforeAllSubcases(t => {
+    t.selectDeviceOrSkipTestCase('shader-f16');
+  })
+  .fn(t => {
+    const code = `
+@workgroup_size(1${t.params.ext})
+@compute fn main() {}`;
+    t.expectCompileResult(t.params.ext === '', code);
   });
