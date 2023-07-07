3863b26341379c15c0721fa064904f46a949843c
David Neto <dneto@google.com>
2023-07-07 13:22:17 -0400

    validate access mode on var decl (#2824)
    * validate access mode on var decl
    
    Contributes to #1538
    
    * Apply review comments
    
    Moved template parsing tests to the parse tests.

---
+++ src/webgpu/shader/validation/decl/util.ts
@@ -0,0 +1,42 @@
+/** An enumerator of shader stages */
+export type ShaderStage = 'vertex' | 'fragment' | 'compute';
+
+/** The list of all shader stages */
+export const kShaderStages = ['vertex', 'fragment', 'compute'] as const;
+
+/**
+ * declareEntrypoint emits the WGSL to declare an entry point with the name, stage and body.
+ * The generated function will have an appropriate return type and return statement, so that @p body
+ * does not have to change between stage.
+ * @param arg - arg specifies the
+ * optional entry point function name, the shader stage, and the body of the
+ * function, excluding any automatically generated return statements.
+ * @returns the WGSL string for the entry point
+ */
+export function declareEntryPoint(arg: {
+  name?: string;
+  stage: ShaderStage;
+  body: string;
+}): string {
+  if (arg.name === undefined) {
+    arg.name = 'main';
+  }
+  switch (arg.stage) {
+    case 'vertex':
+      return `@vertex
+fn ${arg.name}() -> @builtin(position) vec4f {
+  ${arg.body}
+  return vec4f();
+}`;
+    case 'fragment':
+      return `@fragment
+fn ${arg.name}() {
+  ${arg.body}
+}`;
+    case 'compute':
+      return `@compute @workgroup_size(1)
+fn ${arg.name}() {
+  ${arg.body}
+}`;
+  }
+}
