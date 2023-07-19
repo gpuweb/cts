af87c2c713716602a118fdd077b43ac3a5a6f5a1
Ben Clayton <bclayton@google.com>
2023-07-19 11:52:08 +0100

    shader/validation: Fix case parameter types
    We can't encode types directly into the test cases for the reasons outlined in #2860.
    Instead use a `Record` to hold the types, and parameterize over the record's keys.
    
    Fixed: #2860

--- src/webgpu/shader/validation/expression/call/builtin/ceil.spec.ts
+++ src/webgpu/shader/validation/expression/call/builtin/ceil.spec.ts
@@ -4,6 +4,7 @@ Validation tests for the ${builtin}() builtin.
 `;
 
 import { makeTestGroup } from '../../../../../../common/framework/test_group.js';
+import { keysOf, objectsToRecord } from '../../../../../../common/util/data_tables.js';
 import {
   TypeF16,
   TypeF32,
@@ -22,6 +23,8 @@ import {
 
 export const g = makeTestGroup(ShaderValidationTest);
 
+const kValuesTypes = objectsToRecord(kAllFloatScalarsAndVectors);
+
 g.test('values')
   .desc(
     `
@@ -31,12 +34,12 @@ Validates that constant evaluation and override evaluation of ${builtin}() never
   .params(u =>
     u
       .combine('stage', kConstantAndOverrideStages)
-      .combine('type', kAllFloatScalarsAndVectors)
-      .filter(u => stageSupportsType(u.stage, u.type))
-      .expand('value', u => fullRangeForType(u.type))
+      .combine('type', keysOf(kValuesTypes))
+      .filter(u => stageSupportsType(u.stage, kValuesTypes[u.type]))
+      .expand('value', u => fullRangeForType(kValuesTypes[u.type]))
   )
   .beforeAllSubcases(t => {
-    if (elementType(t.params.type) === TypeF16) {
+    if (elementType(kValuesTypes[t.params.type]) === TypeF16) {
       t.selectDeviceOrSkipTestCase('shader-f16');
     }
   })
@@ -46,10 +49,12 @@ Validates that constant evaluation and override evaluation of ${builtin}() never
       t,
       builtin,
       expectedResult,
-      [t.params.type.create(t.params.value)],
+      [kValuesTypes[t.params.type].create(t.params.value)],
       t.params.stage
     );
   });
+
+const kIntegerArgumentTypes = objectsToRecord([TypeF32, ...kAllIntegerScalarsAndVectors]);
 
 g.test('integer_argument')
   .desc(
@@ -57,13 +62,14 @@ g.test('integer_argument')
 Validates that scalar and vector integer arguments are rejected by ${builtin}()
 `
   )
-  .params(u => u.combine('type', [TypeF32, ...kAllIntegerScalarsAndVectors]))
+  .params(u => u.combine('type', keysOf(kIntegerArgumentTypes)))
   .fn(t => {
+    const type = kIntegerArgumentTypes[t.params.type];
     validateConstOrOverrideBuiltinEval(
       t,
       builtin,
-      /* expectedResult */ t.params.type === TypeF32,
-      [t.params.type.create(0)],
+      /* expectedResult */ type === TypeF32,
+      [type.create(0)],
       'constant'
     );
   });
