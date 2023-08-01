a8d2a82cd7230441af5184f5efe80851a2048a68
jzm-intel <zhaoming.jiang@intel.com>
2023-08-01 13:54:47 +0800

    Shader execution tests for f16 built-in min, max (#2879)
    This PR add execution tests for f16 builtin min and max.
    
    Issue: #1248

--- src/webgpu/util/floating_point.ts
+++ src/webgpu/util/floating_point.ts
@@ -4972,8 +4972,8 @@ class F16Traits extends FPTraits {
   public readonly lengthInterval = this.unimplementedLength.bind(this);
   public readonly logInterval = this.unimplementedScalarToInterval.bind(this);
   public readonly log2Interval = this.unimplementedScalarToInterval.bind(this);
-  public readonly maxInterval = this.unimplementedScalarPairToInterval.bind(this);
-  public readonly minInterval = this.unimplementedScalarPairToInterval.bind(this);
+  public readonly maxInterval = this.maxIntervalImpl.bind(this);
+  public readonly minInterval = this.minIntervalImpl.bind(this);
   public readonly mixImpreciseInterval = this.unimplementedScalarTripleToInterval.bind(this);
   public readonly mixPreciseInterval = this.unimplementedScalarTripleToInterval.bind(this);
   public readonly mixIntervals = [this.mixImpreciseInterval, this.mixPreciseInterval];
