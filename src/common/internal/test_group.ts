3d304cfd8806660d44e9a81257e9216eb777693e
Kai Ninomiya <kainino@chromium.org>
2023-08-16 14:25:59 -0700

    Revert "Revert "Optimize loadTreeForQuery by filtering eagerly in ParamsBuilder (#2890)" (#2900)"
    This reverts commit d2d4ab2cfaabd01982c2445277350efa3b7f437b.

--- src/common/internal/test_group.ts
+++ src/common/internal/test_group.ts
@@ -65,7 +65,7 @@ export interface IterableTest {
   testPath: string[];
   description: string | undefined;
   readonly testCreationStack: Error;
-  iterate(): Iterable<RunCase>;
+  iterate(caseFilter: TestParams | null): Iterable<RunCase>;
 }
 
 export function makeTestGroupForUnitTesting<F extends Fixture>(
@@ -280,7 +280,7 @@ class TestBuilder<S extends SubcaseBatchState, F extends Fixture> {
     }
 
     const seen = new Set<string>();
-    for (const [caseParams, subcases] of builderIterateCasesWithSubcases(this.testCases)) {
+    for (const [caseParams, subcases] of builderIterateCasesWithSubcases(this.testCases, null)) {
       for (const subcaseParams of subcases ?? [{}]) {
         const params = mergeParams(caseParams, subcaseParams);
         assert(this.batchSize === 0 || !('batch__' in params));
@@ -332,45 +332,37 @@ class TestBuilder<S extends SubcaseBatchState, F extends Fixture> {
     }
   }
 
-  *iterate(): IterableIterator<RunCase> {
+  private makeCaseSpecific(params: {}, subcases: Iterable<{}> | undefined) {
     assert(this.testFn !== undefined, 'No test function (.fn()) for test');
+    return new RunCaseSpecific(
+      this.testPath,
+      params,
+      this.isUnimplemented,
+      subcases,
+      this.fixture,
+      this.testFn,
+      this.beforeFn,
+      this.testCreationStack
+    );
+  }
+
+  *iterate(caseFilter: TestParams | null): IterableIterator<RunCase> {
     this.testCases ??= kUnitCaseParamsBuilder;
-    for (const [caseParams, subcases] of builderIterateCasesWithSubcases(this.testCases)) {
+    for (const [caseParams, subcases] of builderIterateCasesWithSubcases(
+      this.testCases,
+      caseFilter
+    )) {
       if (this.batchSize === 0 || subcases === undefined) {
-        yield new RunCaseSpecific(
-          this.testPath,
-          caseParams,
-          this.isUnimplemented,
-          subcases,
-          this.fixture,
-          this.testFn,
-          this.beforeFn,
-          this.testCreationStack
-        );
+        yield this.makeCaseSpecific(caseParams, subcases);
       } else {
         const subcaseArray = Array.from(subcases);
         if (subcaseArray.length <= this.batchSize) {
-          yield new RunCaseSpecific(
-            this.testPath,
-            caseParams,
-            this.isUnimplemented,
-            subcaseArray,
-            this.fixture,
-            this.testFn,
-            this.beforeFn,
-            this.testCreationStack
-          );
+          yield this.makeCaseSpecific(caseParams, subcaseArray);
         } else {
           for (let i = 0; i < subcaseArray.length; i = i + this.batchSize) {
-            yield new RunCaseSpecific(
-              this.testPath,
+            yield this.makeCaseSpecific(
               { ...caseParams, batch__: i / this.batchSize },
-              this.isUnimplemented,
-              subcaseArray.slice(i, Math.min(subcaseArray.length, i + this.batchSize)),
-              this.fixture,
-              this.testFn,
-              this.beforeFn,
-              this.testCreationStack
+              subcaseArray.slice(i, Math.min(subcaseArray.length, i + this.batchSize))
             );
           }
         }
