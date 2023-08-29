03819a515332bd0ac6daf11ab13839bdd75eae7f
Brandon Jones <tojiro@gmail.com>
2023-08-29 16:49:49 -0700

    Remove orientation from several texture copy tests (#2914)
    * Remove orientation from several texture copy tests
    
    The orientation argument for these tests was copied from tests which
    use an ImageBitmap source. Because CreateImageBitmap has an
    imageOrientation option it made sense to test against all variants
    of it. However, other tests, such as those that consume ImageData
    directly, don't have any native mechanism for Y-flipping the source.
    The data could be generated Y-flipped, but that doesn't increase
    coverage of any implementation features. Despite not flipping the
    source orientation, however, the test was still expected the data
    to be flipped.
    
    By removing this argument and no longer testing for flipped source
    data, these tests all begin passing with no loss of platform coverage.
    
    Bug: dawn:2017
    
    * Lint fix

--- src/webgpu/web_platform/copyToTexture/image.spec.ts
+++ src/webgpu/web_platform/copyToTexture/image.spec.ts
@@ -53,7 +53,6 @@ g.test('from_image')
   )
   .params(u =>
     u
-      .combine('orientation', ['none', 'flipY'] as const)
       .combine('srcDoFlipYDuringCopy', [true, false])
       .combine('dstColorFormat', kValidTextureFormatsForCopyE2T)
       .combine('dstPremultiplied', [true, false])
@@ -66,14 +65,7 @@ g.test('from_image')
     if (typeof HTMLImageElement === 'undefined') t.skip('HTMLImageElement not available');
   })
   .fn(async t => {
-    const {
-      width,
-      height,
-      orientation,
-      dstColorFormat,
-      dstPremultiplied,
-      srcDoFlipYDuringCopy,
-    } = t.params;
+    const { width, height, dstColorFormat, dstPremultiplied, srcDoFlipYDuringCopy } = t.params;
 
     const imageCanvas = document.createElement('canvas');
     imageCanvas.width = width;
@@ -117,7 +109,7 @@ g.test('from_image')
     });
 
     const expFormat = kTextureFormatInfo[dstColorFormat].baseFormat ?? dstColorFormat;
-    const flipSrcBeforeCopy = orientation === 'flipY';
+    const flipSrcBeforeCopy = false;
     const texelViewExpected = t.getExpectedDstPixelsFromSrcPixels({
       srcPixels: imageData.data,
       srcOrigin: [0, 0],
@@ -186,7 +178,6 @@ g.test('copy_subrect_from_2D_Canvas')
   )
   .params(u =>
     u
-      .combine('orientation', ['none', 'flipY'] as const)
       .combine('srcDoFlipYDuringCopy', [true, false])
       .combine('dstPremultiplied', [true, false])
       .beginSubcases()
@@ -196,7 +187,7 @@ g.test('copy_subrect_from_2D_Canvas')
     if (typeof HTMLImageElement === 'undefined') t.skip('HTMLImageElement not available');
   })
   .fn(async t => {
-    const { copySubRectInfo, orientation, dstPremultiplied, srcDoFlipYDuringCopy } = t.params;
+    const { copySubRectInfo, dstPremultiplied, srcDoFlipYDuringCopy } = t.params;
 
     const { srcOrigin, dstOrigin, srcSize, dstSize, copyExtent } = copySubRectInfo;
     const kColorFormat = 'rgba8unorm';
@@ -242,7 +233,7 @@ g.test('copy_subrect_from_2D_Canvas')
         GPUTextureUsage.COPY_DST | GPUTextureUsage.COPY_SRC | GPUTextureUsage.RENDER_ATTACHMENT,
     });
 
-    const flipSrcBeforeCopy = orientation === 'flipY';
+    const flipSrcBeforeCopy = false;
     const texelViewExpected = t.getExpectedDstPixelsFromSrcPixels({
       srcPixels: imageData.data,
       srcOrigin,
