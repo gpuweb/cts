b9a41dda90ae659af2dc1bcba58aaa5750546b29
Ryan Harrison <rharrison@google.com>
2023-09-05 15:41:43 -0400

    wgsl: Implement simple AbstractFloat addition execution tests
    This PR just covers infix scalar/vector `a + b`.
    
    Compound additon `a += b` and matrix addition will be covered in
    future PRs.
    
    Issue #1626

--- src/webgpu/shader/execution/expression/unary/unary.ts
+++ src/webgpu/shader/execution/expression/unary/unary.ts
@@ -10,6 +10,6 @@ export function unary(op: string): ShaderBuilder {
 }
 
 /* @returns a ShaderBuilder that evaluates a prefix unary operation that returns AbstractFloats */
-export function abstract_unary(op: string): ShaderBuilder {
+export function abstractUnary(op: string): ShaderBuilder {
   return abstractFloatShaderBuilder(value => `${op}(${value})`);
 }
