4673f2f6bcb5939c07683acbfef7e8c5e2d882dd
Ryan Harrison <rharrison@google.com>
2023-09-14 10:51:14 -0400

    wgsl: Add AbstractFloat `trunc` execution tests (#2948)
    Fixes #2525

--- src/webgpu/util/floating_point.ts
+++ src/webgpu/util/floating_point.ts
@@ -4909,7 +4909,7 @@ class FPAbstractTraits extends FPTraits {
   public readonly tanInterval = this.unimplementedScalarToInterval.bind(this);
   public readonly tanhInterval = this.unimplementedScalarToInterval.bind(this);
   public readonly transposeInterval = this.unimplementedMatrixToMatrix.bind(this);
-  public readonly truncInterval = this.unimplementedScalarToInterval.bind(this);
+  public readonly truncInterval = this.truncIntervalImpl.bind(this);
 }
 
 // Pre-defined values that get used multiple times in _constants' initializers. Cannot use FPTraits members, since this
