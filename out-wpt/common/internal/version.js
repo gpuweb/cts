97b4c207324c95200352bfa5d62183aef3cd8226
jzm-intel <zhaoming.jiang@intel.com>
2023-10-20 07:28:29 +0000

    Deploying to gh-pages from @ gpuweb/cts@e5e7ee1d2103cd2fd592c805f42a7036df731fed 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'c78f0a43e79c2b0fa86dbd7775dfaed04d79b9f1';
+export const version = 'e5e7ee1d2103cd2fd592c805f42a7036df731fed';
