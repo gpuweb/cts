d561f8dfaa7655b564eeb54b4de5037febf5b0f0
zoddicus <rharrison@google.com>
2023-10-24 22:05:35 +0000

    Deploying to gh-pages from @ gpuweb/cts@b929ebb9bc13ee2cb1c0f7261c5c86ba512c87a4 🚀

--- out-wpt/webgpu/shader/execution/expression/call/builtin/round.spec.js
+++ out-wpt/webgpu/shader/execution/expression/call/builtin/round.spec.js
@@ -13,13 +13,13 @@ Component-wise when T is a vector.
 `;
 import { makeTestGroup } from '../../../../../../common/framework/test_group.js';
 import { GPUTest } from '../../../../../gpu_test.js';
-import { TypeF32, TypeF16, TypeAbstractFloat } from '../../../../../util/conversion.js';
+import { TypeF32, TypeF16 } from '../../../../../util/conversion.js';
 import { FP } from '../../../../../util/floating_point.js';
-import { fullF32Range, fullF16Range, fullF64Range } from '../../../../../util/math.js';
+import { fullF32Range, fullF16Range } from '../../../../../util/math.js';
 import { makeCaseCache } from '../../case_cache.js';
-import { allInputSources, onlyConstInputSource, run } from '../../expression.js';
+import { allInputSources, run } from '../../expression.js';
 
-import { abstractBuiltin, builtin } from './builtin.js';
+import { builtin } from './builtin.js';
 
 export const g = makeTestGroup(GPUTest);
 
@@ -46,29 +46,13 @@ export const d = makeCaseCache('round', {
       FP.f16.roundInterval
     );
   },
-  abstract: () => {
-    return FP.abstract.generateScalarToIntervalCases(
-      [
-        0x8000_0000_0000_0000, // https://github.com/gpuweb/cts/issues/2766
-        ...fullF64Range(),
-      ],
-
-      'unfiltered',
-      FP.abstract.roundInterval
-    );
-  },
 });
 
 g.test('abstract_float')
   .specURL('https://www.w3.org/TR/WGSL/#float-builtin-functions')
   .desc(`abstract float tests`)
-  .params(u =>
-    u.combine('inputSource', onlyConstInputSource).combine('vectorize', [undefined, 2, 3, 4])
-  )
-  .fn(async t => {
-    const cases = await d.get('abstract');
-    await run(t, abstractBuiltin('round'), [TypeAbstractFloat], TypeAbstractFloat, t.params, cases);
-  });
+  .params(u => u.combine('inputSource', allInputSources).combine('vectorize', [undefined, 2, 3, 4]))
+  .unimplemented();
 
 g.test('f32')
   .specURL('https://www.w3.org/TR/WGSL/#float-builtin-functions')
--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '8e7a9958e740c3278eb143e49317633dcaeea669';
+export const version = 'b929ebb9bc13ee2cb1c0f7261c5c86ba512c87a4';
