25158315bb8003c60c53b2f64993b081a4c4ff8a
greggman <github@greggman.com>
2023-12-14 03:27:45 +0000

    Deploying to gh-pages from @ gpuweb/cts@cd9505da1d631c46cc38b7218c54a4598a4fd802 🚀

--- out-wpt/webgpu/compat/api/validation/render_pipeline/shader_module.spec.js
+++ out-wpt/webgpu/compat/api/validation/render_pipeline/shader_module.spec.js
@@ -72,3 +72,83 @@ fn((t) => {
     !isValid
   );
 });
+
+g.test('interpolate').
+desc(
+  `
+Tests that you can not create a render pipeline with a shader module that uses interpolate(linear) nor interpolate(...,sample) in compat mode.
+
+- Test that a pipeline with a shader that uses interpolate(linear) or interpolate(sample) fails.
+- Test that a pipeline that references a module that has a shader that uses interpolate(linear/sample)
+  but the pipeline does not reference that shader succeeds.
+    `
+).
+params((u) =>
+u.
+combine('interpolate', [
+'',
+'@interpolate(linear)',
+'@interpolate(linear, sample)',
+'@interpolate(perspective, sample)']
+).
+combine('entryPoint', [
+'fsWithoutInterpolationUsage',
+'fsWithInterpolationUsage1',
+'fsWithInterpolationUsage2',
+'fsWithInterpolationUsage3']
+)
+).
+fn((t) => {
+  const { entryPoint, interpolate } = t.params;
+
+  const module = t.device.createShaderModule({
+    code: `
+        struct Vertex {
+            @builtin(position) pos: vec4f,
+            @location(0) ${interpolate} color : vec4f,
+        };
+        @vertex fn vs() -> Vertex {
+            var v: Vertex;
+            v.pos = vec4f(1);
+            v.color = vec4f(1);
+            return v;
+        }
+        @fragment fn fsWithoutInterpolationUsage() -> @location(0) vec4f {
+            return vec4f(1);
+        }
+        @fragment fn fsWithInterpolationUsage1(v: Vertex) -> @location(0) vec4f {
+            return vec4f(1);
+        }
+        @fragment fn fsWithInterpolationUsage2(v: Vertex) -> @location(0) vec4f {
+            return v.pos;
+        }
+        @fragment fn fsWithInterpolationUsage3(v: Vertex) -> @location(0) vec4f {
+            return v.color;
+        }
+      `
+  });
+
+  const pipelineDescriptor = {
+    layout: 'auto',
+    vertex: {
+      module,
+      entryPoint: 'vs'
+    },
+    fragment: {
+      module,
+      entryPoint,
+      targets: [
+      {
+        format: 'rgba8unorm'
+      }]
+
+    }
+  };
+
+  const isValid = entryPoint === 'fsWithoutInterpolationUsage' || interpolate === '';
+  t.expectGPUError(
+    'validation',
+    () => t.device.createRenderPipeline(pipelineDescriptor),
+    !isValid
+  );
+});
--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '84fae1feb137891f37563965e0461ee98605b3e3';
+export const version = 'cd9505da1d631c46cc38b7218c54a4598a4fd802';
