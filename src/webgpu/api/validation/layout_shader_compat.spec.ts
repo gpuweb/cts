130be8073d12b5fceada6e56eac8f0635018c119
Jiawei Shao <jiawei.shao@intel.com>
2023-12-15 15:49:45 +0800

    Add validation tests about pipeline layout and shader declaration

--- src/webgpu/api/validation/layout_shader_compat.spec.ts
+++ src/webgpu/api/validation/layout_shader_compat.spec.ts
@@ -8,7 +8,17 @@ TODO:
 `;
 
 import { makeTestGroup } from '../../../common/framework/test_group.js';
+import { kBindableResources } from '../../capability_info.js';
 
 import { ValidationTest } from './validation_test.js';
 
 export const g = makeTestGroup(ValidationTest);
+
+g.test('pipeline_layout_shader_match')
+  .desc('')
+  .params(u =>
+    u
+      .combine('bindingInPipeline', kBindableResources)
+      .combine('bindingInShader', kBindableResources)
+  )
+  .fn(async t => {});
