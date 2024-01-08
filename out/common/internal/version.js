b19ceec3ecf2348303c8d919f62ccf9b06ee9ba4
kainino0x <kainino1@gmail.com>
2024-01-08 20:04:33 +0000

    Deploying to gh-pages from @ gpuweb/cts@1cc4ab0669fa254baf715cd274649249d37f2e5a 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'f64bdd2bd4b59aed102fafe377019b35658f7ed0';
+export const version = '1cc4ab0669fa254baf715cd274649249d37f2e5a';
