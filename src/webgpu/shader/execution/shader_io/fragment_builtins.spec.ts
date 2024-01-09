b2c215d924c4f98d6fa25a91b464a2a8160246c1
Gregg Tavares <github@greggman.com>
2024-01-09 15:41:38 -0800

    Compat: Refactor tests that use sample_index for compat
    `sample_index` generates a validation error in compat.

--- src/webgpu/shader/execution/shader_io/fragment_builtins.spec.ts
+++ src/webgpu/shader/execution/shader_io/fragment_builtins.spec.ts
@@ -1037,10 +1037,7 @@ g.test('inputs,sample_index')
       ] as const)
   )
   .beforeAllSubcases(t => {
-    const {
-      interpolation: { type, sampling },
-    } = t.params;
-    t.skipIfInterpolationTypeOrSamplingNotSupported({ type, sampling });
+    t.skipIf(t.isCompatibility, 'sample_index is not supported in compatibility mode');
   })
   .fn(async t => {
     const {
