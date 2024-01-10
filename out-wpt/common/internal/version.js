c34108223edf50acaf6ed81cda6887612955ed76
haoxli <hao.x.li@intel.com>
2024-01-10 01:25:42 +0000

    Deploying to gh-pages from @ gpuweb/cts@873305ab6a2eae37dc899856ccd55bebe8e94f8a 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '5da6d193c2f842e45da9748453d0ae4b751a7dbc';
+export const version = '873305ab6a2eae37dc899856ccd55bebe8e94f8a';
