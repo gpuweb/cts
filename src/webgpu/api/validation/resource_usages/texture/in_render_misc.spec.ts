705abe536630df7d722bd7b4c866fc35a0ff662f
Ben Clayton <bclayton@google.com>
2024-01-12 16:51:06 +0000

    Add shader pointer parameter tests, including for `unrestricted_pointer_parameter` support (#3265)
    * Rename `requireLanguageFeatureOrSkipTestCase`
    
    To `skipIfLanguageFeatureSupported` to match all the other skip methods.
    
    * [shader] Add pointer parameter execution tests
    
    Checks reading and writing to pointer parameters, with and without the 'unrestricted_pointer_parameters' WGSL feature
    
    * [shader] Update function validation tests for UPP
    
    Update the `webgpu:shader,validation,functions,restrictions` tests to handle the `unrestricted_pointer_parameters` WGSL feature

--- src/webgpu/api/validation/resource_usages/texture/in_render_misc.spec.ts
+++ src/webgpu/api/validation/resource_usages/texture/in_render_misc.spec.ts
@@ -238,7 +238,7 @@ g.test('subresources,set_unused_bind_group')
       textureUsage0 === 'readwrite-storage-texture' ||
       textureUsage1 === 'readwrite-storage-texture'
     ) {
-      t.requireLanguageFeatureOrSkipTestCase('readonly_and_readwrite_storage_textures');
+      t.skipIfLanguageFeatureNotSupported('readonly_and_readwrite_storage_textures');
     }
 
     const texture0 = t.device.createTexture({
