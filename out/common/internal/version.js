e860a34fc07505ac43a35e2fa9db54dc32632571
shrekshao <5031596+shrekshao@users.noreply.github.com>
2024-01-29 22:37:21 +0000

    Deploying to gh-pages from @ gpuweb/cts@78959c7723909a5798a57769198bc5485710789f 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'df460fa1ad87214fc74886cf223bebdd8993290c';
+export const version = '78959c7723909a5798a57769198bc5485710789f';
