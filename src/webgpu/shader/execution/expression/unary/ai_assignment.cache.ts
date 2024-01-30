6a03fa0648393214daf802b721616d1ea80f6fd5
Ryan Harrison <rharrison@google.com>
2024-01-30 13:54:52 -0500

    wgsl: Implement handling AbstractInts as output from tests (#3331)
    Fixes #3302

--- src/webgpu/shader/execution/expression/unary/ai_assignment.cache.ts
+++ src/webgpu/shader/execution/expression/unary/ai_assignment.cache.ts
@@ -1,8 +1,13 @@
 import { abstractInt, i32, u32 } from '../../../../util/conversion.js';
-import { fullI32Range, fullU32Range } from '../../../../util/math.js';
+import { fullI32Range, fullI64Range, fullU32Range } from '../../../../util/math.js';
 import { makeCaseCache } from '../case_cache.js';
 
 export const d = makeCaseCache('unary/ai_assignment', {
+  abstract: () => {
+    return fullI64Range().map(n => {
+      return { input: abstractInt(n), expected: abstractInt(n) };
+    });
+  },
   i32: () => {
     return fullI32Range().map(n => {
       return { input: abstractInt(BigInt(n)), expected: i32(n) };
