f6337a44059621aea4de2961f17a57fdb3650e76
Ryan Harrison <rharrison@google.com>
2024-02-13 16:53:33 -0500

    wgsl: Implement AbstractInt remainder execution tests (#3400)
    Issue #1626

--- src/webgpu/shader/execution/expression/binary/ai_arithmetic.spec.ts
+++ src/webgpu/shader/execution/expression/binary/ai_arithmetic.spec.ts
@@ -186,6 +186,64 @@ Expression: x * y
     await run(t, abstractIntBinary('*'), [vec_type, TypeAbstractInt], vec_type, t.params, cases);
   });
 
+g.test('remainder')
+  .specURL('https://www.w3.org/TR/WGSL/#arithmetic-expr')
+  .desc(
+    `
+Expression: x % y
+`
+  )
+  .params(u =>
+    u
+      .combine('inputSource', onlyConstInputSource)
+      .combine('vectorize', [undefined, 2, 3, 4] as const)
+  )
+  .fn(async t => {
+    const cases = await d.get('remainder');
+    await run(
+      t,
+      abstractIntBinary('%'),
+      [TypeAbstractInt, TypeAbstractInt],
+      TypeAbstractInt,
+      t.params,
+      cases
+    );
+  });
+
+g.test('remainder_scalar_vector')
+  .specURL('https://www.w3.org/TR/WGSL/#arithmetic-expr')
+  .desc(
+    `
+Expression: x % y
+`
+  )
+  .params(u =>
+    u.combine('inputSource', onlyConstInputSource).combine('vectorize_rhs', [2, 3, 4] as const)
+  )
+  .fn(async t => {
+    const vec_size = t.params.vectorize_rhs;
+    const vec_type = TypeVec(vec_size, TypeAbstractInt);
+    const cases = await d.get(`remainder_scalar_vector${vec_size}`);
+    await run(t, abstractIntBinary('%'), [TypeAbstractInt, vec_type], vec_type, t.params, cases);
+  });
+
+g.test('remainder_vector_scalar')
+  .specURL('https://www.w3.org/TR/WGSL/#arithmetic-expr')
+  .desc(
+    `
+Expression: x % y
+`
+  )
+  .params(u =>
+    u.combine('inputSource', onlyConstInputSource).combine('vectorize_lhs', [2, 3, 4] as const)
+  )
+  .fn(async t => {
+    const vec_size = t.params.vectorize_lhs;
+    const vec_type = TypeVec(vec_size, TypeAbstractInt);
+    const cases = await d.get(`remainder_vector${vec_size}_scalar`);
+    await run(t, abstractIntBinary('%'), [vec_type, TypeAbstractInt], vec_type, t.params, cases);
+  });
+
 g.test('subtraction')
   .specURL('https://www.w3.org/TR/WGSL/#arithmetic-expr')
   .desc(
