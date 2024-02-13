e771828181783f3d8abac4edcc76ba17b30e42a0
Ryan Harrison <rharrison@google.com>
2024-02-13 14:20:03 -0500

    wgsl: Implement AbstractInt remainder execution tests
    Issue #1626

--- src/webgpu/shader/execution/expression/binary/ai_arithmetic.cache.ts
+++ src/webgpu/shader/execution/expression/binary/ai_arithmetic.cache.ts
@@ -27,6 +27,13 @@ function ai_mul(x: bigint, y: bigint): bigint | undefined {
   return !isOOB(result) ? result : undefined;
 }
 
+function ai_rem(x: bigint, y: bigint): bigint | undefined {
+  if (y === 0n) return undefined;
+  if (x === kValue.i64.negative.min && y === -1n) return undefined;
+  const result = x % y;
+  return !isOOB(result) ? result : undefined;
+}
+
 function ai_sub(x: bigint, y: bigint): bigint | undefined {
   const result = x - y;
   return !isOOB(result) ? result : undefined;
@@ -95,6 +102,27 @@ export const d = makeCaseCache('binary/ai_arithmetic', {
   },
   multiplication_vector4_scalar: () => {
     return generateVectorI64BinaryToVectorCases(vectorI64Range(4), sparseI64Range(), ai_mul);
+  },
+  remainder: () => {
+    return generateBinaryToI64Cases(sparseI64Range(), sparseI64Range(), ai_rem);
+  },
+  remainder_scalar_vector2: () => {
+    return generateI64VectorBinaryToVectorCases(sparseI64Range(), vectorI64Range(2), ai_rem);
+  },
+  remainder_scalar_vector3: () => {
+    return generateI64VectorBinaryToVectorCases(sparseI64Range(), vectorI64Range(3), ai_rem);
+  },
+  remainder_scalar_vector4: () => {
+    return generateI64VectorBinaryToVectorCases(sparseI64Range(), vectorI64Range(4), ai_rem);
+  },
+  remainder_vector2_scalar: () => {
+    return generateVectorI64BinaryToVectorCases(vectorI64Range(2), sparseI64Range(), ai_rem);
+  },
+  remainder_vector3_scalar: () => {
+    return generateVectorI64BinaryToVectorCases(vectorI64Range(3), sparseI64Range(), ai_rem);
+  },
+  remainder_vector4_scalar: () => {
+    return generateVectorI64BinaryToVectorCases(vectorI64Range(4), sparseI64Range(), ai_rem);
   },
   subtraction: () => {
     return generateBinaryToI64Cases(sparseI64Range(), sparseI64Range(), ai_sub);
