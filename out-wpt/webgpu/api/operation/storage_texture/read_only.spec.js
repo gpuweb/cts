19a80d2d94db1f8af04be2bcc9c0d493edcec624
Jiawei-Shao <jiawei.shao@intel.com>
2024-02-20 01:49:06 +0000

    Deploying to gh-pages from @ gpuweb/cts@7cdae79ca3e826f55a29b526876c3908f229e688 🚀

--- out-wpt/webgpu/api/operation/storage_texture/read_only.spec.js
+++ out-wpt/webgpu/api/operation/storage_texture/read_only.spec.js
@@ -7,7 +7,6 @@ TODO:
 - Test the use of read-only storage textures in vertex and fragment shaders
 - Test 1D and 3D textures
 - Test mipmap level > 0
-- Test bgra8unorm with 'bgra8unorm-storage'
 - Test resource usage transitions with read-only storage textures
 `;import { makeTestGroup } from '../../../../common/framework/test_group.js';
 import { unreachable, assert } from '../../../../common/util/util.js';
@@ -39,6 +38,7 @@ function ComponentCount(format) {
     case 'rgba16float':
     case 'rgba16sint':
     case 'rgba16uint':
+    case 'bgra8unorm':
       return 4;
     default:
       unreachable();
@@ -71,11 +71,12 @@ class F extends GPUTest {
     texelValue,
     outputValue,
     texelDataIndex,
-    component) =>
+    component,
+    outputComponent = component) =>
     {
       const texelComponentIndex = texelDataIndex * componentCount + component;
       texelTypedDataView[texelComponentIndex] = texelValue;
-      const outputTexelComponentIndex = texelDataIndex * 4 + component;
+      const outputTexelComponentIndex = texelDataIndex * 4 + outputComponent;
       outputBufferTypedData[outputTexelComponentIndex] = outputValue;
     };
     for (let z = 0; z < depthOrArrayLayers; ++z) {
@@ -105,6 +106,15 @@ class F extends GPUTest {
                   const texelValue = (4 * texelDataIndex + component + 1) % 256;
                   const outputValue = texelValue / 255.0;
                   SetData(texelValue, outputValue, texelDataIndex, component);
+                  break;
+                }
+              case 'bgra8unorm':{
+                  const texelValue = (4 * texelDataIndex + component + 1) % 256;
+                  const outputValue = texelValue / 255.0;
+                  // BGRA -> RGBA
+                  assert(component < 4);
+                  const outputComponent = [2, 1, 0, 3][component];
+                  SetData(texelValue, outputValue, texelDataIndex, component, outputComponent);
                   break;
                 }
               case 'r32sint':
@@ -184,6 +194,7 @@ class F extends GPUTest {
         return new Uint32Array(arrayBuffer);
       case 'rgba8uint':
       case 'rgba8unorm':
+      case 'bgra8unorm':
         return new Uint8Array(arrayBuffer);
       case 'rgba16uint':
         return new Uint16Array(arrayBuffer);
@@ -293,9 +304,16 @@ desc(
 params((u) =>
 u.
 combine('format', kColorTextureFormats).
-filter((p) => kTextureFormatInfo[p.format].color?.storage === true).
+filter(
+  (p) => p.format === 'bgra8unorm' || kTextureFormatInfo[p.format].color?.storage === true
+).
 combine('depthOrArrayLayers', [1, 2])
 ).
+beforeAllSubcases((t) => {
+  if (t.params.format === 'bgra8unorm') {
+    t.selectDeviceOrSkipTestCase('bgra8unorm-storage');
+  }
+}).
 fn((t) => {
   const { format, depthOrArrayLayers } = t.params;
 
--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '8a9294d00391a8d3dbcdb3091308f9b3034b384e';
+export const version = '7cdae79ca3e826f55a29b526876c3908f229e688';
