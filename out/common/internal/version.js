8ca9e131c71dd8b9e1da3ec686943b972ced6de5
ben-clayton <bclayton@google.com>
2024-02-21 21:48:24 +0000

    Deploying to gh-pages from @ gpuweb/cts@7b71d6b4f1495dcf7b7188d85997c9159279205e 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '500dfa8c9be987d861cc3e388aa658021c650e22';
+export const version = '7b71d6b4f1495dcf7b7188d85997c9159279205e';
