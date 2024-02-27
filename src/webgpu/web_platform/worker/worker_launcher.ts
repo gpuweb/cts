cbc11443e9839fa9994ce472c264dafad44b7a79
dan sinclair <dsinclair@chromium.org>
2024-02-27 14:40:06 -0500

    Reapply "Add shared worker support (#3345)" (#3428)
    This reverts commit 55e6eea387ebc2724fc5961bb29ec22e8b8a8de2.
    
    Attempting to land this again to see if it causes any infra issues when
    rolled into Chromium.

--- src/webgpu/web_platform/worker/worker_launcher.ts
+++ src/webgpu/web_platform/worker/worker_launcher.ts
@@ -4,7 +4,7 @@ export type TestResult = {
   error: String | undefined;
 };
 
-export async function launchWorker() {
+export async function launchDedicatedWorker() {
   const selfPath = import.meta.url;
   const selfPathDir = selfPath.substring(0, selfPath.lastIndexOf('/'));
   const workerPath = selfPathDir + '/worker.js';
@@ -14,5 +14,22 @@ export async function launchWorker() {
     worker.addEventListener('message', ev => resolve(ev.data as TestResult), { once: true });
   });
   worker.postMessage({ defaultRequestAdapterOptions: getDefaultRequestAdapterOptions() });
+  return await promise;
+}
+
+export async function launchSharedWorker() {
+  const selfPath = import.meta.url;
+  const selfPathDir = selfPath.substring(0, selfPath.lastIndexOf('/'));
+  const workerPath = selfPathDir + '/worker.js';
+  const worker = new SharedWorker(workerPath, { type: 'module' });
+
+  const port = worker.port;
+  const promise = new Promise<TestResult>(resolve => {
+    port.addEventListener('message', ev => resolve(ev.data as TestResult), { once: true });
+  });
+  port.start();
+  port.postMessage({
+    defaultRequestAdapterOptions: getDefaultRequestAdapterOptions(),
+  });
   return await promise;
 }
