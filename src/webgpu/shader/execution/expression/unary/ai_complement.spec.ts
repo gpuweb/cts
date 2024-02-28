37cf5d293607b979e0515cd52ffd912d2c6a5679
Ryan Harrison <rharrison@google.com>
2024-02-28 13:12:33 -0500

    Remove cache for trivial complement cases

--- src/webgpu/shader/execution/expression/unary/ai_complement.spec.ts
+++ src/webgpu/shader/execution/expression/unary/ai_complement.spec.ts
@@ -4,10 +4,10 @@ Execution Tests for the AbstractInt bitwise complement operation
 
 import { makeTestGroup } from '../../../../../common/framework/test_group.js';
 import { GPUTest } from '../../../../gpu_test.js';
-import { TypeAbstractInt } from '../../../../util/conversion.js';
+import { abstractInt, TypeAbstractInt } from '../../../../util/conversion.js';
+import { fullI64Range } from '../../../../util/math.js';
 import { onlyConstInputSource, run } from '../expression.js';
 
-import { d } from './ai_complement.cache.js';
 import { abstractIntUnary } from './unary.js';
 
 export const g = makeTestGroup(GPUTest);
@@ -25,6 +25,8 @@ Expression: ~x
       .combine('vectorize', [undefined, 2, 3, 4] as const)
   )
   .fn(async t => {
-    const cases = await d.get('complement');
+    const cases = fullI64Range().map(e => {
+      return { input: abstractInt(e), expected: abstractInt(~e) };
+    });
     await run(t, abstractIntUnary('~'), [TypeAbstractInt], TypeAbstractInt, t.params, cases);
   });
