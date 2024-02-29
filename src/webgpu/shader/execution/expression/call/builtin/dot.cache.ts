57bd1b0ae3ab74130ae5e1f2acab2b32cedd173b
Ryan Harrison <rharrison@google.com>
2024-02-29 12:43:12 -0500

    wgsl: Implement i32 & u32 `dot` execution tests (#3411)
    Fixes #2516
    Fixes #2515

--- src/webgpu/shader/execution/expression/call/builtin/dot.cache.ts
+++ src/webgpu/shader/execution/expression/call/builtin/dot.cache.ts
@@ -4,10 +4,18 @@ import { kValue } from '../../../../../util/constants.js';
 import { FP } from '../../../../../util/floating_point.js';
 import {
   calculatePermutations,
+  sparseVectorI32Range,
   sparseVectorI64Range,
+  sparseVectorU32Range,
+  vectorI32Range,
   vectorI64Range,
+  vectorU32Range,
 } from '../../../../../util/math.js';
-import { generateVectorVectorToI64Cases } from '../../case.js';
+import {
+  generateVectorVectorToI32Cases,
+  generateVectorVectorToI64Cases,
+  generateVectorVectorToU32Cases,
+} from '../../case.js';
 import { makeCaseCache } from '../../case_cache.js';
 
 function ai_dot(x: bigint[], y: bigint[]): bigint | undefined {
@@ -44,6 +52,11 @@ function ai_dot(x: bigint[], y: bigint[]): bigint | undefined {
   return !kValue.i64.isOOB(result) ? result : undefined;
 }
 
+function ci_dot(x: number[], y: number[]): number | undefined {
+  assert(x.length === y.length, 'Cannot calculate dot for vectors of different lengths');
+  return x.reduce((prev, _, idx) => prev + Math.imul(x[idx], y[idx]), 0);
+}
+
 // Cases: [f32|f16]_vecN_[non_]const
 const float_cases = (['f32', 'f16'] as const)
   .flatMap(trait =>
@@ -74,6 +87,24 @@ const cases = {
   },
   abstract_int_vec4: () => {
     return generateVectorVectorToI64Cases(sparseVectorI64Range(4), sparseVectorI64Range(4), ai_dot);
+  },
+  i32_vec2: () => {
+    return generateVectorVectorToI32Cases(vectorI32Range(2), vectorI32Range(2), ci_dot);
+  },
+  i32_vec3: () => {
+    return generateVectorVectorToI32Cases(sparseVectorI32Range(3), sparseVectorI32Range(3), ci_dot);
+  },
+  i32_vec4: () => {
+    return generateVectorVectorToI32Cases(sparseVectorI32Range(4), sparseVectorI32Range(4), ci_dot);
+  },
+  u32_vec2: () => {
+    return generateVectorVectorToU32Cases(vectorU32Range(2), vectorU32Range(2), ci_dot);
+  },
+  u32_vec3: () => {
+    return generateVectorVectorToU32Cases(sparseVectorU32Range(3), sparseVectorU32Range(3), ci_dot);
+  },
+  u32_vec4: () => {
+    return generateVectorVectorToU32Cases(sparseVectorU32Range(4), sparseVectorU32Range(4), ci_dot);
   },
 };
 
