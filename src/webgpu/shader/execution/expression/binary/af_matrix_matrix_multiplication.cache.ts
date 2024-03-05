a2b2b9f8b3ac8ec2dd1f90b6e83ee526d037f530
Ryan Harrison <rharrison@google.com>
2024-03-05 13:23:21 -0500

    Add stochastic filtering to AF matrix * matrix cases

--- src/webgpu/shader/execution/expression/binary/af_matrix_matrix_multiplication.cache.ts
+++ src/webgpu/shader/execution/expression/binary/af_matrix_matrix_multiplication.cache.ts
@@ -1,18 +1,22 @@
 import { FP } from '../../../../util/floating_point.js';
 import { sparseMatrixF64Range } from '../../../../util/math.js';
+import { StochasticFilter } from '../../../../util/stochastic_filter.js';
 import { makeCaseCache } from '../case_cache.js';
 
+const sf = new StochasticFilter(0);
 // Cases: matKxR_matCxK
 const mat_mat_cases = ([2, 3, 4] as const)
   .flatMap(k =>
     ([2, 3, 4] as const).flatMap(cols =>
       ([2, 3, 4] as const).map(rows => ({
         [`mat${k}x${rows}_mat${cols}x${k}`]: () => {
-          return FP.abstract.generateMatrixPairToMatrixCases(
-            sparseMatrixF64Range(k, rows),
-            sparseMatrixF64Range(cols, k),
-            'finite',
-            FP.abstract.multiplicationMatrixMatrixInterval
+          return sf.filter(
+            FP.abstract.generateMatrixPairToMatrixCases(
+              sparseMatrixF64Range(k, rows),
+              sparseMatrixF64Range(cols, k),
+              'finite',
+              FP.abstract.multiplicationMatrixMatrixInterval
+            )
           );
         },
       }))
