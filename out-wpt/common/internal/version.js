f5b3011fa3a52f10fb3258bdc6ab0669e3cf0f1d
dj2 <dj2@everburning.com>
2024-03-06 18:43:20 +0000

    Deploying to gh-pages from @ gpuweb/cts@3dcdba7fe0dced1f7332387627345cb5a7e311c4 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '70ac878b39ada939f225f7dd0d01134e1d7de691';
+export const version = '3dcdba7fe0dced1f7332387627345cb5a7e311c4';
--- out/webgpu/shader/validation/expression/call/builtin/abs.spec.js
+++ out/webgpu/shader/validation/expression/call/builtin/abs.spec.js
@@ -56,4 +56,108 @@ fn((t) => {
     t.params.stage
   );
 });
+
+const kTests = {
+  valid: {
+    src: `_ = abs(1);`,
+    pass: true
+  },
+  alias: {
+    src: `_ = abs(i32_alias(1));`,
+    pass: true
+  },
+
+  bool: {
+    src: `_ = abs(false);`,
+    pass: false
+  },
+  vec_bool: {
+    src: `_ = abs(vec2<bool>(false, true));`,
+    pass: false
+  },
+  matrix: {
+    src: `_ = abs(mat2x2(1, 1, 1, 1));`,
+    pass: false
+  },
+  atomic: {
+    src: ` _ = abs(a);`,
+    pass: false
+  },
+  array: {
+    src: `var a: array<u32, 5>;
+          _ = abs(a);`,
+    pass: false
+  },
+  array_runtime: {
+    src: `_ = abs(k.arry);`,
+    pass: false
+  },
+  struct: {
+    src: `var a: A;
+          _ = abs(a);`,
+    pass: false
+  },
+  enumerant: {
+    src: `_ = abs(read_write);`,
+    pass: false
+  },
+  ptr: {
+    src: `var<function> a = 1u;
+          let p: ptr<function, u32> = &a;
+          _ = abs(p);`,
+    pass: false
+  },
+  ptr_deref: {
+    src: `var<function> a = 1u;
+          let p: ptr<function, u32> = &a;
+          _ = abs(*p);`,
+    pass: true
+  },
+  sampler: {
+    src: `_ = abs(s);`,
+    pass: false
+  },
+  texture: {
+    src: `_ = abs(t);`,
+    pass: false
+  },
+  no_params: {
+    src: `_ = abs();`,
+    pass: false
+  },
+  too_many_params: {
+    src: `_ = abs(1, 2);`,
+    pass: false
+  }
+};
+
+g.test('parameters').
+desc(`Test that ${builtin} is validated correctly.`).
+params((u) => u.combine('test', keysOf(kTests))).
+fn((t) => {
+  const src = kTests[t.params.test].src;
+  const code = `
+alias i32_alias = i32;
+
+@group(0) @binding(0) var s: sampler;
+@group(0) @binding(1) var t: texture_2d<f32>;
+
+var<workgroup> a: atomic<u32>;
+
+struct A {
+  i: u32,
+}
+struct B {
+  arry: array<u32>,
+}
+@group(0) @binding(3) var<storage> k: B;
+
+
+@vertex
+fn main() -> @builtin(position) vec4<f32> {
+  ${src}
+  return vec4<f32>(.4, .2, .3, .1);
+}`;
+  t.expectCompileResult(kTests[t.params.test].pass, code);
+});
 //# sourceMappingURL=abs.spec.js.map
