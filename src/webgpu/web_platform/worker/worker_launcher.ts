465dd6453ff075511a9850bd3cd53ba5491792c9
Kai Ninomiya <kainino@chromium.org>
2024-03-07 15:53:17 -0800

    Skip worker tests when run in a worker that can't support them
    DedicatedWorker can be created from DedicatedWorker, but none of the
    other nested worker pairs are allowed.

--- src/webgpu/web_platform/worker/worker_launcher.ts
+++ src/webgpu/web_platform/worker/worker_launcher.ts
@@ -1,3 +1,4 @@
+import { SkipTestCase } from '../../../common/framework/fixture.js';
 import { getDefaultRequestAdapterOptions } from '../../../common/util/navigator_gpu.js';
 
 export type TestResult = {
@@ -5,6 +6,10 @@ export type TestResult = {
 };
 
 export async function launchDedicatedWorker() {
+  if (typeof Worker === 'undefined') {
+    throw new SkipTestCase(`Worker undefined in context ${globalThis.constructor.name}`);
+  }
+
   const selfPath = import.meta.url;
   const selfPathDir = selfPath.substring(0, selfPath.lastIndexOf('/'));
   const workerPath = selfPathDir + '/worker.js';
@@ -18,6 +23,10 @@ export async function launchDedicatedWorker() {
 }
 
 export async function launchSharedWorker() {
+  if (typeof SharedWorker === 'undefined') {
+    throw new SkipTestCase(`SharedWorker undefined in context ${globalThis.constructor.name}`);
+  }
+
   const selfPath = import.meta.url;
   const selfPathDir = selfPath.substring(0, selfPath.lastIndexOf('/'));
   const workerPath = selfPathDir + '/worker.js';
@@ -35,6 +44,12 @@ export async function launchSharedWorker() {
 }
 
 export async function launchServiceWorker() {
+  if (typeof navigator === 'undefined' || typeof navigator.serviceWorker === 'undefined') {
+    throw new SkipTestCase(
+      `navigator.serviceWorker undefined in context ${globalThis.constructor.name}`
+    );
+  }
+
   const selfPath = import.meta.url;
   const selfPathDir = selfPath.substring(0, selfPath.lastIndexOf('/'));
   const serviceWorkerPath = selfPathDir + '/worker.js';
