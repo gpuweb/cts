b7486e7f9c5e54a462acb2a92b41d106293d3c52
Brandon Jones <tojiro@gmail.com>
2024-03-18 09:10:36 -0700

    Make use of existing quantize functions

--- src/webgpu/shader/validation/expression/call/builtin/normalize.spec.ts
+++ src/webgpu/shader/validation/expression/call/builtin/normalize.spec.ts
@@ -10,7 +10,9 @@ import {
   kConcreteIntegerScalarsAndVectors,
   kConvertableToFloatVectors,
   scalarTypeOf,
+  ScalarType,
 } from '../../../../../util/conversion.js';
+import { QuantizeFunc, quantizeToF16, quantizeToF32 } from '../../../../../util/math.js';
 import { ShaderValidationTest } from '../../../shader_validation_test.js';
 
 import {
@@ -24,27 +26,15 @@ export const g = makeTestGroup(ShaderValidationTest);
 
 const kValidArgumentTypes = objectsToRecord(kConvertableToFloatVectors);
 
-// These should probably go in a utilities file somewhere?
-const f32_cast_array = new Float32Array(1);
-function toF32(v: number): number {
-  f32_cast_array[0] = v;
-  return f32_cast_array[0];
-}
-
-const f16_max = 65504;
-const f16_min = -65504;
-const f16_epsilon = 0.0000000596046;
-function toF16(v: number): number {
-  if (Math.abs(v) < f16_epsilon) {
-    return 0;
+function quantizeFunctionForScalarType(type: ScalarType): QuantizeFunc<number> {
+  switch (type) {
+    case Type.f32:
+      return quantizeToF32;
+    case Type.f16:
+      return quantizeToF16;
+    default:
+      return (v: number) => v;
   }
-  if (v > f16_max) {
-    return Infinity;
-  }
-  if (v < f16_min) {
-    return -Infinity;
-  }
-  return v;
 }
 
 g.test('values')
@@ -70,27 +60,15 @@ Validates that constant evaluation and override evaluation of ${builtin}() rejec
     let expectedResult = true;
 
     const scalarType = scalarTypeOf(kValidArgumentTypes[t.params.type]);
-
-    let castFn;
-    switch (scalarType) {
-      case Type.f32:
-        castFn = toF32;
-        break;
-      case Type.f16:
-        castFn = toF16;
-        break;
-      default:
-        castFn = (v: number) => v;
-        break;
-    }
+    const quantizeFn = quantizeFunctionForScalarType(scalarType);
 
     // Should be invalid if the normalization calculations result in intermediate
     // values that exceed the maximum representable float value for the given type,
     // or if the length is smaller than the smallest representable float value.
     const v = Number(t.params.value);
-    const vv = castFn(v * v);
-    const dp = castFn(vv * kValidArgumentTypes[t.params.type].width);
-    const len = castFn(Math.sqrt(dp));
+    const vv = quantizeFn(v * v);
+    const dp = quantizeFn(vv * kValidArgumentTypes[t.params.type].width);
+    const len = quantizeFn(Math.sqrt(dp));
     if (vv === Infinity || dp === Infinity || len === 0) {
       expectedResult = false;
     }
@@ -164,5 +142,5 @@ g.test('must_use')
   .params(u => u.combine('use', [true, false]))
   .fn(t => {
     const use_it = t.params.use ? '_ = ' : '';
-    t.expectCompileResult(false, `fn f() { ${use_it}${builtin}${kArgCases['good']}; }`);
+    t.expectCompileResult(t.params.use, `fn f() { ${use_it}${builtin}${kArgCases['good']}; }`);
   });
