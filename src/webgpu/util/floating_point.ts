f44b0fa161940706d59ba179c65c0c1473b5a7b1
Ryan Harrison <rharrison@google.com>
2024-03-21 16:41:54 -0400

    wgsl: Implement AbstractFloat `fract` execution tests (#3530)
    Implementing recommendation from https://github.com/gpuweb/gpuweb/issues/4523
    
    Fixes #1297

--- src/webgpu/util/floating_point.ts
+++ src/webgpu/util/floating_point.ts
@@ -5148,7 +5148,7 @@ class FPAbstractTraits extends FPTraits {
   public readonly faceForwardIntervals = this.unimplementedFaceForward.bind(this);
   public readonly floorInterval = this.floorIntervalImpl.bind(this);
   public readonly fmaInterval = this.unimplementedScalarTripleToInterval.bind(this, 'fmaInterval');
-  public readonly fractInterval = this.unimplementedScalarToInterval.bind(this, 'fractInterval');
+  public readonly fractInterval = this.fractIntervalImpl.bind(this);
   public readonly inverseSqrtInterval = this.unimplementedScalarToInterval.bind(
     this,
     'inverseSqrtInterval'
