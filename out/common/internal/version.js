b240983ecb0916b525e0b2a2f235161026a874b7
zoddicus <rharrison@google.com>
2024-03-28 19:56:22 +0000

    Deploying to gh-pages from @ gpuweb/cts@916580420fa306db53e7934c172b1a94f42aba16 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'fa37932c812f8214a0ea241b98ad122a11b37f8c';
+export const version = '916580420fa306db53e7934c172b1a94f42aba16';
