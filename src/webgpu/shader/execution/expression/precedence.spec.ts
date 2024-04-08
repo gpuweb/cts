459828662d29a91d4b53578266045698c1077214
James Price <jrprice@google.com>
2024-04-08 11:42:36 -0400

    Add literal and no-whitespace parameterizations

--- src/webgpu/shader/execution/expression/precedence.spec.ts
+++ src/webgpu/shader/execution/expression/precedence.spec.ts
@@ -14,25 +14,25 @@ interface Expression {
   result: number;
 }
 const kExpressions: Record<string, Expression> = {
-  add_mul: { expr: 'three + seven * eleven', result: 80 },
-  mul_add: { expr: 'three * seven + eleven', result: 32 },
-  sub_neg: { expr: 'three--seven', result: 10 },
-  neg_shl: { expr: '- three << u32(seven)', result: -384 },
-  neg_shr: { expr: '- three >> u32(seven)', result: -1 },
-  neg_add: { expr: '- three + seven', result: 4 },
-  neg_mul: { expr: '- three * seven', result: -21 },
-  neg_and: { expr: '- three & seven', result: 5 },
-  neg_or: { expr: '- three | seven', result: -1 },
-  neg_xor: { expr: '- three ^ seven', result: -6 },
-  comp_add: { expr: '~ three + seven', result: 3 },
-  mul_deref: { expr: 'three**ptr_five', result: 15 },
+  add_mul: { expr: 'kThree + kSeven * kEleven', result: 80 },
+  mul_add: { expr: 'kThree * kSeven + kEleven', result: 32 },
+  sub_neg: { expr: 'kThree - - kSeven', result: 10 },
+  neg_shl: { expr: '- kThree << u32(kSeven)', result: -384 },
+  neg_shr: { expr: '- kThree >> u32(kSeven)', result: -1 },
+  neg_add: { expr: '- kThree + kSeven', result: 4 },
+  neg_mul: { expr: '- kThree * kSeven', result: -21 },
+  neg_and: { expr: '- kThree & kSeven', result: 5 },
+  neg_or: { expr: '- kThree | kSeven', result: -1 },
+  neg_xor: { expr: '- kThree ^ kSeven', result: -6 },
+  comp_add: { expr: '~ kThree + kSeven', result: 3 },
+  mul_deref: { expr: 'kThree * * ptr_five', result: 15 },
   not_and: { expr: 'i32(! kFalse && kFalse)', result: 0 },
   not_or: { expr: 'i32(! kTrue || kTrue)', result: 1 },
   eq_and: { expr: 'i32(kFalse == kTrue && kFalse)', result: 0 },
   and_eq: { expr: 'i32(kFalse && kTrue == kFalse)', result: 0 },
   eq_or: { expr: 'i32(kFalse == kFalse || kTrue)', result: 1 },
   or_eq: { expr: 'i32(kTrue || kFalse == kFalse)', result: 1 },
-  add_swizzle: { expr: '(vec + vec . y).z', result: 8 },
+  add_swizzle: { expr: '(vec + vec . y) . z', result: 8 },
 };
 
 g.test('precedence')
@@ -42,27 +42,44 @@ g.test('precedence')
     `
   )
   .params(u =>
-    u.combine('expr', keysOf(kExpressions)).combine('decl', ['const', 'override', 'var<private>'])
+    u
+      .combine('expr', keysOf(kExpressions))
+      .combine('decl', ['literal', 'const', 'override', 'var<private>'])
+      .combine('strip_spaces', [false, true])
   )
   .fn(t => {
     const expr = kExpressions[t.params.expr];
+
+    let decl = t.params.decl;
+    let expr_wgsl = expr.expr;
+    if (t.params.decl === 'literal') {
+      decl = 'const';
+      expr_wgsl = expr_wgsl.replace(/kThree/g, '3');
+      expr_wgsl = expr_wgsl.replace(/kSeven/g, '7');
+      expr_wgsl = expr_wgsl.replace(/kEleven/g, '11');
+      expr_wgsl = expr_wgsl.replace(/kFalse/g, 'false');
+      expr_wgsl = expr_wgsl.replace(/kTrue/g, 'true');
+    }
+    if (t.params.strip_spaces) {
+      expr_wgsl = expr_wgsl.replace(/ /g, '');
+    }
     const wgsl = `
       @group(0) @binding(0) var<storage, read_write> buffer : i32;
 
-      ${t.params.decl} kFalse = false;
-      ${t.params.decl} kTrue = true;
+      ${decl} kFalse = false;
+      ${decl} kTrue = true;
 
-      ${t.params.decl} three = 3;
-      ${t.params.decl} seven = 7;
-      ${t.params.decl} eleven = 11;
+      ${decl} kThree = 3;
+      ${decl} kSeven = 7;
+      ${decl} kEleven = 11;
 
       @compute @workgroup_size(1)
       fn main() {
         var five = 5;
-        var vec = vec4(1, three, 5, seven);
+        var vec = vec4(1, kThree, 5, kSeven);
         let ptr_five = &five;
 
-        buffer = ${expr.expr};
+        buffer = ${expr_wgsl};
       }
     `;
     const pipeline = t.device.createComputePipeline({
