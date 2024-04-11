2da606a7d01855d104588bdb223e5ca763b5d330
toji <tojiro@gmail.com>
2024-04-11 21:30:52 +0000

    Deploying to gh-pages from @ gpuweb/cts@14fed352f516d644d1b3dd10317461d044f753ea 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '05f7e5b5a2c585b6581fa1056165b84d43ffa964';
+export const version = '14fed352f516d644d1b3dd10317461d044f753ea';
