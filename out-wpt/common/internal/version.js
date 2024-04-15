e075aa3b725e935b5b70c5fcbc64cb54b93cff56
lokokung <lokokung@gmail.com>
2024-04-15 18:09:49 +0000

    Deploying to gh-pages from @ gpuweb/cts@e272d4892bd098c81c326cc0ad9d5d1238d9aa87 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'f8ecbdefc4285f539702cdfa9156c65f349a3d67';
+export const version = 'e272d4892bd098c81c326cc0ad9d5d1238d9aa87';
