8cc95e45081256252307549d9b8981666d657a6b
dneto0 <dneto@google.com>
2024-04-15 14:54:42 +0000

    Deploying to gh-pages from @ gpuweb/cts@18241e5ece5b0e970862fc4e9d1c1f0a739c6fd8 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '539e362781cf4026163fecff58934602fac3095e';
+export const version = '18241e5ece5b0e970862fc4e9d1c1f0a739c6fd8';
