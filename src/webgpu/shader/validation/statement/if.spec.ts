ed81245f215511831e955e2ff59ee09e46c75032
Ben Clayton <bclayton@google.com>
2024-04-18 19:33:03 +0100

    shader/validation: Expand tested statement condition types (#3690)

--- src/webgpu/shader/validation/statement/if.spec.ts
+++ src/webgpu/shader/validation/statement/if.spec.ts
@@ -1,42 +1,30 @@
 export const description = `Validation tests for 'if' statements'`;
 
 import { makeTestGroup } from '../../../../common/framework/test_group.js';
-import { scalarTypeOf, Type } from '../../../util/conversion.js';
+import { keysOf } from '../../../../common/util/data_tables.js';
 import { ShaderValidationTest } from '../shader_validation_test.js';
+
+import { kTestTypes } from './test_types.js';
 
 export const g = makeTestGroup(ShaderValidationTest);
 
-const kTestTypes = [
-  'bool',
-  'i32',
-  'u32',
-  'f32',
-  'f16',
-  'vec2f',
-  'vec3h',
-  'vec4u',
-  'vec3b',
-  'mat2x3f',
-  'mat4x2h',
-  'abstract-int',
-  'abstract-float',
-] as const;
-
 g.test('condition_type')
   .desc(`Tests that an 'if' condition must be a bool type`)
-  .params(u => u.combine('type', kTestTypes))
+  .params(u => u.combine('type', keysOf(kTestTypes)))
   .beforeAllSubcases(t => {
-    if (scalarTypeOf(Type[t.params.type]).kind === 'f16') {
+    if (kTestTypes[t.params.type].requires === 'f16') {
       t.selectDeviceOrSkipTestCase('shader-f16');
     }
   })
   .fn(t => {
-    const type = Type[t.params.type];
+    const type = kTestTypes[t.params.type];
     const code = `
-${scalarTypeOf(type).kind === 'f16' ? 'enable f16;' : ''}
+${type.requires ? `enable ${type.requires};` : ''}
+
+${type.header ?? ''}
 
 fn f() -> bool {
-  if ${type.create(1).wgsl()} {
+  if ${type.value} {
     return true;
   }
   return false;
@@ -49,21 +37,23 @@ fn f() -> bool {
 
 g.test('else_condition_type')
   .desc(`Tests that an 'else if' condition must be a bool type`)
-  .params(u => u.combine('type', kTestTypes))
+  .params(u => u.combine('type', keysOf(kTestTypes)))
   .beforeAllSubcases(t => {
-    if (scalarTypeOf(Type[t.params.type]).kind === 'f16') {
+    if (kTestTypes[t.params.type].requires === 'f16') {
       t.selectDeviceOrSkipTestCase('shader-f16');
     }
   })
   .fn(t => {
-    const type = Type[t.params.type];
+    const type = kTestTypes[t.params.type];
     const code = `
-${scalarTypeOf(type).kind === 'f16' ? 'enable f16;' : ''}
+${type.requires ? `enable ${type.requires};` : ''}
+
+${type.header ?? ''}
 
 fn f(c : bool) -> bool {
   if (c) {
     return true;
-  } else if (${type.create(1).wgsl()}) {
+  } else if (${type.value}) {
     return true;
   }
   return false;
--- src/webgpu/shader/validation/statement/loop.spec.ts
+++ src/webgpu/shader/validation/statement/loop.spec.ts
@@ -1,44 +1,32 @@
 export const description = `Validation tests for 'loop' statements'`;
 
 import { makeTestGroup } from '../../../../common/framework/test_group.js';
-import { scalarTypeOf, Type } from '../../../util/conversion.js';
+import { keysOf } from '../../../../common/util/data_tables.js';
 import { ShaderValidationTest } from '../shader_validation_test.js';
+
+import { kTestTypes } from './test_types.js';
 
 export const g = makeTestGroup(ShaderValidationTest);
 
-const kTestTypes = [
-  'bool',
-  'i32',
-  'u32',
-  'f32',
-  'f16',
-  'vec2f',
-  'vec3h',
-  'vec4u',
-  'vec3b',
-  'mat2x3f',
-  'mat4x2h',
-  'abstract-int',
-  'abstract-float',
-] as const;
-
 g.test('break_if_type')
   .desc(`Tests that a 'break if' condition must be a bool type`)
-  .params(u => u.combine('type', kTestTypes))
+  .params(u => u.combine('type', keysOf(kTestTypes)))
   .beforeAllSubcases(t => {
-    if (scalarTypeOf(Type[t.params.type]).kind === 'f16') {
+    if (kTestTypes[t.params.type].requires === 'f16') {
       t.selectDeviceOrSkipTestCase('shader-f16');
     }
   })
   .fn(t => {
-    const type = Type[t.params.type];
+    const type = kTestTypes[t.params.type];
     const code = `
-${scalarTypeOf(type).kind === 'f16' ? 'enable f16;' : ''}
+${type.requires ? `enable ${type.requires};` : ''}
+
+${type.header ?? ''}
 
 fn f() {
   loop {
     continuing {
-      break if ${type.create(1).wgsl()};
+      break if ${type.value};
     }
   }
 }
