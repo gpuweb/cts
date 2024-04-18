4b96a783814136afbe69430759bb2ca429676784
jrprice <jamesprice.dev@gmail.com>
2024-04-18 13:37:25 +0000

    Deploying to gh-pages from @ gpuweb/cts@f64f6c0345ed4c66b6a96fa757910fb0421eabf7 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '17f39c9877e4efbc404c6536c608e2418dee3644';
+export const version = 'f64f6c0345ed4c66b6a96fa757910fb0421eabf7';
