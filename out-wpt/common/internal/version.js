698b7ec1d9c8d00ed0e2c74c33b6f0d0067fc49f
zoddicus <rharrison@google.com>
2024-04-24 19:56:47 +0000

    Deploying to gh-pages from @ gpuweb/cts@5bd2353b5651103cc5ea1534f55c9cbbbb5defe9 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'e18bcdbb80772a576f12185e1e7e1752c409ec02';
+export const version = '5bd2353b5651103cc5ea1534f55c9cbbbb5defe9';
