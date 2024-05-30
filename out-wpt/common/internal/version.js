9e02acf0fda7bf6670f2f80cc3c861896c3d58dd
sudonatalie <chouinard.nm@gmail.com>
2024-05-30 16:52:13 +0000

    Deploying to gh-pages from @ gpuweb/cts@3f9ae5e9a7961b7de91cc3d5eee2029634248a48 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '704b6a320501a6888250d3acc93b339631eac4cd';
+export const version = '3f9ae5e9a7961b7de91cc3d5eee2029634248a48';
