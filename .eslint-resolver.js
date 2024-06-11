c8b7341edcd65e01c36a531c01d380c74eba5b37
Kai Ninomiya <kainino@chromium.org>
2024-06-10 18:32:26 -0700

    Autoformat JS files

--- .eslint-resolver.js
+++ .eslint-resolver.js
@@ -3,21 +3,22 @@ const resolve = require('resolve'); // eslint-disable-line node/no-extraneous-re
 
 // Implements the following resolver spec:
 // https://github.com/benmosher/eslint-plugin-import/blob/master/resolvers/README.md
-exports.interfaceVersion = 2
+exports.interfaceVersion = 2;
 
 exports.resolve = function (source, file, config) {
-  if (resolve.isCore(source)) return { found: true, path: null }
+  if (resolve.isCore(source)) return { found: true, path: null };
 
   source = source.replace(/\.js$/, '.ts');
   try {
     return {
-      found: true, path: resolve.sync(source, {
+      found: true,
+      path: resolve.sync(source, {
         extensions: [],
         basedir: path.dirname(path.resolve(file)),
         ...config,
-      })
-    }
+      }),
+    };
   } catch (err) {
-    return { found: false }
+    return { found: false };
   }
-}
+};
