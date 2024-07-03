a65854f7d984ed67be7fd5e2ff333d9b43584d3b
alan-baker <alanbaker@google.com>
2024-07-03 14:17:19 +0000

    Deploying to gh-pages from @ gpuweb/cts@652daec42219c0b4f8df7441e98493eb81d3821d 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'bea483d1c8d9db96f585b79e9ae1fa0ee56c6e9d';
+export const version = '652daec42219c0b4f8df7441e98493eb81d3821d';
--- out-wpt/webgpu/shader/validation/expression/binary/bitwise_shift.spec.js
+++ out-wpt/webgpu/shader/validation/expression/binary/bitwise_shift.spec.js
@@ -203,7 +203,16 @@ const kLeftShiftCases = [
 // Shift by negative is an error
 { lhs: `1`, rhs: `-1`, pass: false },
 { lhs: `1i`, rhs: `-1`, pass: false },
-{ lhs: `1u`, rhs: `-1`, pass: false }];
+{ lhs: `1u`, rhs: `-1`, pass: false },
+
+// Signed overflow (sign change) for abstract
+{ lhs: `1`, rhs: `63`, pass: false },
+{ lhs: `2`, rhs: `62`, pass: false },
+{
+  lhs: `${0b0100000000000000000000000000000000000000000000000000000000000000}`,
+  rhs: `1u`,
+  pass: false
+}];
 
 
 g.test('shift_left_concrete').
