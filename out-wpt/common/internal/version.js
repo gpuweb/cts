f25c676429c1f002683bad588f9cbacd2fc8bef8
shaoboyan <shaobo.yan@intel.com>
2024-08-02 08:06:17 +0000

    Deploying to gh-pages from @ gpuweb/cts@8ab018e2919dbc34b3ea37de46f0ca8ffd4b32d7 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '921a5107f9848f229dad20c78531e98b83050dd3';
+export const version = '8ab018e2919dbc34b3ea37de46f0ca8ffd4b32d7';
