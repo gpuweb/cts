5cdb23901199f2d0a0f7820c6a178748006cf7ec
dneto0 <dneto@google.com>
2024-08-14 19:41:38 +0000

    Deploying to gh-pages from @ gpuweb/cts@5c571490b4c077f0319cb6a9b06d77ed39e205dd 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'a17d71da10b513be9f42342337f1ac07d9f17431';
+export const version = '5c571490b4c077f0319cb6a9b06d77ed39e205dd';
--- out/webgpu/shader/validation/expression/call/builtin/clamp.spec.js
+++ out/webgpu/shader/validation/expression/call/builtin/clamp.spec.js
@@ -127,6 +127,8 @@ filter((t) => {
   const scalar = scalarTypeOf(ty);
   return scalar !== Type.abstractInt && scalar !== Type.abstractFloat;
 })
+// in_shader: Is the function call statically accessed by the entry point?
+.combine('in_shader', [false, true])
 ).
 beforeAllSubcases((t) => {
   const ty = kValuesTypes[t.params.type];
@@ -176,7 +178,10 @@ fn foo() {
   const shader_error =
   error && t.params.lowStage === 'constant' && t.params.highStage === 'constant';
   const pipeline_error =
-  error && t.params.lowStage !== 'runtime' && t.params.highStage !== 'runtime';
+  t.params.in_shader &&
+  error &&
+  t.params.lowStage !== 'runtime' &&
+  t.params.highStage !== 'runtime';
   t.expectCompileResult(!shader_error, wgsl);
   if (!shader_error) {
     const constants = {};
@@ -187,7 +192,7 @@ fn foo() {
       code: wgsl,
       constants,
       reference: ['o_low', 'o_high'],
-      statements: ['foo();']
+      statements: t.params.in_shader ? ['foo();'] : []
     });
   }
 });
