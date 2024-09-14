2a5500f8cfb52f3bb54b96cedaef3a8362d58780
Jiawei-Shao <jiawei.shao@intel.com>
2024-09-14 06:40:43 +0000

    Deploying to gh-pages from @ gpuweb/cts@383aa2812b79acf86d474722c7ca40ea8599760b 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'e321371e5b69077524fb309cea435c07730793af';
+export const version = '383aa2812b79acf86d474722c7ca40ea8599760b';
