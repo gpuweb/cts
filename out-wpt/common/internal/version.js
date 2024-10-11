75959c0f7e7451a1b8912c16942d029085dcdc5a
greggman <github@greggman.com>
2024-10-11 23:40:05 +0000

    Deploying to gh-pages from @ gpuweb/cts@3cf7402ce18325f20faa9380bbd2d6c4b13bd65c 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '8a2916436238386d6465ec2cab9735e65ad14070';
+export const version = '3cf7402ce18325f20faa9380bbd2d6c4b13bd65c';
