4a330e1997b5a8138ae5386cc546bdc5b09c3cc5
Kai Ninomiya <kainino@chromium.org>
2024-10-14 23:53:26 -0400

    Use batching for shader,validation,expression,call,builtin,*:values:
    Many of these tests have too many subcases, which run in parallel and
    can run out of resources. We can use batching to break tests into more
    cases (without having to break them up arbitrarily by the numeric values
    passed to builtins).
    
    Not all of the :values: tests actually have more than 125 subcases; if
    they don't, then `batch` has no effect. I added `batch` to all of them
    anyway so that when tests are added in the future, they'll serve as an
    example and get copied over.
    
    Hopefully helps with https://crbug.com/373478528
    and https://crbug.com/373485785

--- src/webgpu/shader/validation/expression/call/builtin/modf.spec.ts
+++ src/webgpu/shader/validation/expression/call/builtin/modf.spec.ts
@@ -25,6 +25,7 @@ export const g = makeTestGroup(ShaderValidationTest);
 const kValuesTypes = objectsToRecord(kConvertableToFloatScalarsAndVectors);
 
 g.test('values')
+  .batch(125)
   .desc(
     `
 Validates that constant evaluation and override evaluation of ${builtin}() rejects invalid values
