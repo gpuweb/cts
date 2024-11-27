97fc4f6b4feb45b3bcf80b1f3be7e292501d14cb
Alan Baker <alanbaker@google.com>
2024-11-27 13:10:36 -0500

    Add validation of subgroup_uniformity diagnostic

--- src/webgpu/shader/validation/parse/diagnostic.spec.ts
+++ src/webgpu/shader/validation/parse/diagnostic.spec.ts
@@ -6,7 +6,7 @@ import { ShaderValidationTest } from '../shader_validation_test.js';
 
 export const g = makeTestGroup(ShaderValidationTest);
 
-const kSpecDiagnosticRules = ['derivative_uniformity'];
+const kSpecDiagnosticRules = ['derivative_uniformity', 'subgroup_uniformity'];
 const kSpecDiagnosticSeverities = ['off', 'info', 'warning', 'error'];
 const kDiagnosticTypes = ['attribute', 'directive'];
 
@@ -80,6 +80,11 @@ g.test('valid_params')
       .combine('rule', kSpecDiagnosticRules)
       .combine('type', kDiagnosticTypes)
   )
+  .beforeAllSubcases(t => {
+    if (t.params.rule === 'subgroup_uniformity') {
+      t.selectDeviceOrSkipTestCase('subgroups' as GPUFeatureName);
+    }
+  })
   .fn(t => {
     const diag = generateDiagnostic(t.params.type, t.params.severity, t.params.rule);
     let code = ``;
@@ -124,9 +129,19 @@ g.test('warning_unknown_rule')
 g.test('valid_locations')
   .specURL('https://gpuweb.github.io/gpuweb/wgsl/#diagnostics')
   .desc(`Tests valid locations`)
-  .params(u => u.combine('type', kDiagnosticTypes).combine('location', keysOf(kValidLocations)))
+  .params(u =>
+    u
+      .combine('type', kDiagnosticTypes)
+      .combine('location', keysOf(kValidLocations))
+      .combine('rule', kSpecDiagnosticRules)
+  )
+  .beforeAllSubcases(t => {
+    if (t.params.rule === 'subgroup_uniformity') {
+      t.selectDeviceOrSkipTestCase('subgroups' as GPUFeatureName);
+    }
+  })
   .fn(t => {
-    const diag = generateDiagnostic(t.params.type, 'info', 'derivative_uniformity');
+    const diag = generateDiagnostic(t.params.type, 'info', t.params.rule);
     const code = kValidLocations[t.params.location](diag);
     let res = true;
     if (t.params.type === 'directive') {
@@ -143,9 +158,19 @@ g.test('valid_locations')
 g.test('invalid_locations')
   .specURL('https://gpuweb.github.io/gpuweb/wgsl/#diagnostics')
   .desc(`Tests invalid locations`)
-  .params(u => u.combine('type', kDiagnosticTypes).combine('location', keysOf(kInvalidLocations)))
+  .params(u =>
+    u
+      .combine('type', kDiagnosticTypes)
+      .combine('location', keysOf(kInvalidLocations))
+      .combine('rule', kSpecDiagnosticRules)
+  )
+  .beforeAllSubcases(t => {
+    if (t.params.rule === 'subgroup_uniformity') {
+      t.selectDeviceOrSkipTestCase('subgroups' as GPUFeatureName);
+    }
+  })
   .fn(t => {
-    const diag = generateDiagnostic(t.params.type, 'info', 'derivative_uniformity');
+    const diag = generateDiagnostic(t.params.type, 'info', t.params.rule);
     t.expectCompileResult(true, kInvalidLocations[t.params.location](''));
     t.expectCompileResult(false, kInvalidLocations[t.params.location](diag));
   });
