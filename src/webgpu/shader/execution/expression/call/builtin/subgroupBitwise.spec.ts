08731e9049bd8c70662a04778821c50a9b301b4e
David Neto <dneto@google.com>
2024-12-05 15:35:55 -0500

    Reapply "Use subgroupMinSize, subgroupMaxSize from GPUAdapterInfo" (#4079) (#4080)
    This reverts commit ed1b78a79ad5220701330e7311b528f174097dfe.
    
    Also: Test adapter.info.subgroupMinSize, subgroupMaxSize

--- src/webgpu/shader/execution/expression/call/builtin/subgroupBitwise.spec.ts
+++ src/webgpu/shader/execution/expression/call/builtin/subgroupBitwise.spec.ts
@@ -547,12 +547,12 @@ g.test('fragment,all_active')
   .fn(async t => {
     const numInputs = t.params.size[0] * t.params.size[1];
 
-    interface SubgroupLimits extends GPUSupportedLimits {
-      minSubgroupSize: number;
+    interface SubgroupProperties extends GPUAdapterInfo {
+      subgroupMinSize: number;
     }
-    const { minSubgroupSize } = t.device.limits as SubgroupLimits;
+    const { subgroupMinSize } = t.device.adapterInfo as SubgroupProperties;
     const innerTexels = (t.params.size[0] - 1) * (t.params.size[1] - 1);
-    t.skipIf(innerTexels < minSubgroupSize, 'Too few texels to be reliable');
+    t.skipIf(innerTexels < subgroupMinSize, 'Too few texels to be reliable');
 
     const inputData = generateInputData(t.params.case, numInputs, identity(t.params.op));
 
