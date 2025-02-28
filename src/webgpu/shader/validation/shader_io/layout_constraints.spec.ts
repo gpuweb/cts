340998711c9fff30879ddf780bbe8c8c7c760550
Gregg Tavares <github@greggman.com>
2025-02-28 15:42:23 -0800

    Refactor shader/validation/shader_io/*
    Issue #4178

--- src/webgpu/shader/validation/shader_io/layout_constraints.spec.ts
+++ src/webgpu/shader/validation/shader_io/layout_constraints.spec.ts
@@ -490,14 +490,11 @@ g.test('layout_constraints')
       .beginSubcases()
       .combine('aspace', ['storage', 'uniform', 'function', 'private', 'workgroup'] as const)
   )
-  .beforeAllSubcases(t => {
-    const testcase = kLayoutCases[t.params.case];
-    if (testcase.f16) {
-      t.selectDeviceOrSkipTestCase('shader-f16');
-    }
-  })
   .fn(t => {
     const testcase = kLayoutCases[t.params.case];
+    if (testcase.f16) {
+      t.skipIfDeviceDoesNotHaveFeature('shader-f16');
+    }
     const decls = testcase.decls !== undefined ? testcase.decls : '';
     let code = `
 ${testcase.f16 ? 'enable f16;' : ''}
