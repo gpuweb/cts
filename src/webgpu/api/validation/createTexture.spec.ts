1b0ea8482fa4f3ddccb7aac5921f9dd40816f93f
François Beaufort <beaufort.francois@gmail.com>
2025-04-15 09:17:35 +0200

    Check packed texture formats features for 3d texture creations (#4352)
    * Check packed texture formats features for 3d texture creations
    
    * Update src/webgpu/format_info.ts
    
    Co-authored-by: Kai Ninomiya <kainino@chromium.org>
    
    ---------
    
    Co-authored-by: Kai Ninomiya <kainino@chromium.org>

--- src/webgpu/api/validation/createTexture.spec.ts
+++ src/webgpu/api/validation/createTexture.spec.ts
@@ -21,6 +21,7 @@ import {
   isTextureFormatPossiblyStorageReadable,
   isColorTextureFormat,
   textureFormatsAreViewCompatible,
+  textureDimensionAndFormatCompatibleForDevice,
 } from '../../format_info.js';
 import { maxMipLevelCount } from '../../util/texture/base.js';
 
@@ -100,7 +101,8 @@ g.test('zero_size_and_usage')
 
 g.test('dimension_type_and_format_compatibility')
   .desc(
-    `Test every dimension type on every format. Note that compressed formats and depth/stencil formats are not valid for 1D/3D dimension types.`
+    `Test every dimension type on every format. Note that compressed formats and depth/stencil formats are not valid
+    for 1D dimension types while it depends on the format for 3D types.`
   )
   .params(u =>
     u //
@@ -119,9 +121,12 @@ g.test('dimension_type_and_format_compatibility')
       usage: GPUTextureUsage.TEXTURE_BINDING,
     };
 
-    t.expectValidationError(() => {
-      t.createTextureTracked(descriptor);
-    }, !textureDimensionAndFormatCompatible(dimension, format));
+    t.expectValidationError(
+      () => {
+        t.createTextureTracked(descriptor);
+      },
+      !textureDimensionAndFormatCompatibleForDevice(t.device, dimension, format)
+    );
   });
 
 g.test('mipLevelCount,format')
