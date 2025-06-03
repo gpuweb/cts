baa305fa7a0418869286a7c661b0df69630a81d3
beaufortfrancois <beaufort.francois@gmail.com>
2025-06-03 07:08:09 +0000

    Deploying to gh-pages from @ gpuweb/cts@566aba1bde80492c08b07193bc37c124e450b2c2 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '75289e08465d568768ca5b04082e226d911c01f9';
+export const version = '566aba1bde80492c08b07193bc37c124e450b2c2';
