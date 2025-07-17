01351783d12faf2ce4175114a3a637f5260244e9
Gregg Tavares <github@greggman.com>
2025-07-17 15:32:57 -0700

    Add texture-component-swizzle tests
    I removed the 3 stages and am only testing the fragment stage.
    As for speed, with `debug` false it runs fast enough as is I
    think. About 25 seconds for all tests on M1 Mac.
    
    Note: One issue I ran into is the code that created textures
    would potentially create them with RENDER_ATTACHMENT if they
    needed to be rendered to to fill. A texture created with
    RENDER_ATTACHMENT can't be swizzled. My solution was to change
    the code that makes the texture such that, if the texture
    had RENDER_ATTACHMENT added, and it can be copied texture to texture,
    then make a copy without RENDER_ATTACHMENT
    
    Another issue I ran into and I'm not sure what solution is better,
    is testing depth-stencil formats. The existing code is not
    designed for that. All the depth-stencil formats are "unencodable".
    My solution was createTextureWithRandomDataAndGetTexelsForEachAspect
    that returns 2 sets of TexelViews, one for the depth aspect,
    another for the stencil aspec. It's possible, maybe we should
    make those formats encodable as Texels? I didn't chose that becuase
    depth-stencil has 2 types, f32 for depth, u32 for stencil. This seems
    like it would cause issues all over the place where code expects just
    one type.
    
    Another issue is depth24plus-stencil, the depth part is undefined.
    In the code, createTextureWithRandomDataAndGetTexelsForEachAspect
    on a depth format returns a TexelView with float data, not depth24plus
    data. I'm not sure all the encoders etc would handle this.
    
    So, like I mentioned above, my solution was to return 2 sets of
    TexelViews, one for the depth aspect and one for the stenci aspect.
    The code then choose one based on which aspect it's testing.

---
+++ src/webgpu/api/validation/capability_checks/features/texture_component_swizzle.spec.ts
@@ -0,0 +1,78 @@
+export const description = `
+Validation tests for the 'texture-component-swizzle' feature.
+
+Test that:
+* when the feature is off, swizzling is not allowed, even the identity swizzle.
+* swizzling is not allowed on textures with usage STORAGE_BINDING nor RENDER_ATTACHMENT
+  except the identity swizzle.
+`;
+
+import { makeTestGroup } from '../../../../../common/framework/test_group.js';
+import { GPUConst } from '../../../../constants.js';
+import { UniqueFeaturesOrLimitsGPUTest } from '../../../../gpu_test.js';
+
+import {
+  isIdentitySwizzle,
+  kSwizzleTests,
+  swizzleSpecToGPUTextureComponentSwizzle,
+} from './texture_component_swizzle_utils.js';
+
+export const g = makeTestGroup(UniqueFeaturesOrLimitsGPUTest);
+
+g.test('no_swizzle')
+  .desc(
+    `
+  Test that if texture-component-swizzle is not enabled, having a swizzle property generates a validation error.
+  `
+  )
+  .fn(t => {
+    const texture = t.createTextureTracked({
+      format: 'rgba8unorm',
+      size: [1],
+      usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.TEXTURE_BINDING,
+    });
+    t.expectValidationError(() => {
+      texture.createView({ swizzle: {} });
+    });
+  });
+
+g.test('no_render_nor_storage')
+  .desc(
+    `
+  Test that setting the swizzle on the texture with RENDER_ATTACHMENT or STORAGE_BINDING usage works
+  if the swizzle is the identity but generates a validation error otherwise.
+  `
+  )
+  .params(u =>
+    u
+      .combine('usage', [
+        GPUConst.TextureUsage.COPY_SRC,
+        GPUConst.TextureUsage.COPY_DST,
+        GPUConst.TextureUsage.TEXTURE_BINDING,
+        GPUConst.TextureUsage.RENDER_ATTACHMENT,
+        GPUConst.TextureUsage.STORAGE_BINDING,
+      ] as const)
+      .beginSubcases()
+      .combine('swizzleSpec', kSwizzleTests)
+  )
+  .beforeAllSubcases(t => {
+    // MAINTENANCE_TODO: Remove this cast once texture-component-swizzle is added to @webgpu/types
+    t.selectDeviceOrSkipTestCase('texture-component-swizzle' as GPUFeatureName);
+  })
+  .fn(t => {
+    const { swizzleSpec, usage } = t.params;
+    const swizzle = swizzleSpecToGPUTextureComponentSwizzle(swizzleSpec);
+    const texture = t.createTextureTracked({
+      format: 'rgba8unorm',
+      size: [1],
+      usage,
+    });
+    const badUsage =
+      (usage &
+        (GPUConst.TextureUsage.RENDER_ATTACHMENT | GPUConst.TextureUsage.STORAGE_BINDING)) !==
+      0;
+    const shouldError = badUsage && !isIdentitySwizzle(swizzle);
+    t.expectValidationError(() => {
+      texture.createView({ swizzle });
+    }, shouldError);
+  });
