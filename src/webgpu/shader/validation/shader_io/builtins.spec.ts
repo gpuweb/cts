c20df363e5a5e3d716cfd2b598f8fc07f2908282
Brandon Jones <bajones@chromium.org>
2025-07-29 13:12:08 -0700

    Add tests for the primitive_id builtin
    Adds some initial tests for the primitive_id WGSL builtin. Currently
    uses `enable chromium_experimental_primitive_id` to enable, but that
    can change once the spec is fleshed out and accepted by the group.

--- src/webgpu/shader/validation/shader_io/builtins.spec.ts
+++ src/webgpu/shader/validation/shader_io/builtins.spec.ts
@@ -38,6 +38,7 @@ export const kBuiltins = [
   { name: 'clip_distances', stage: 'vertex', io: 'out', type: 'array<f32,6>' },
   { name: 'clip_distances', stage: 'vertex', io: 'out', type: 'array<f32,7>' },
   { name: 'clip_distances', stage: 'vertex', io: 'out', type: 'array<f32,8>' },
+  { name: 'primitive_id', stage: 'fragment', io: 'in', type: 'u32' },
 ] as const;
 
 // List of types to test against.
@@ -338,6 +339,8 @@ g.test('reuse_builtin_name')
         code += 'enable subgroup;\n';
       } else if (t.params.name === 'clip_distances') {
         code += 'enable clip_distances;\n';
+      } else if (t.params.name === 'primitive_id') {
+        code += 'enable chromium_experimental_primitive_id;\n';
       }
     }
     if (t.params.use === 'alias') {
