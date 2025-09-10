c6a6038cbd8ad522ca8d3be798ed92bba74ebdc4
Kai Ninomiya <kainino@chromium.org>
2025-09-10 10:09:57 -0700

    fix

--- src/common/tools/version.ts
+++ src/common/tools/version.ts
@@ -1,6 +1,6 @@
-/* eslint-disable-next-line n/no-restricted-require */
 export const version = (() => {
   try {
+    /* eslint-disable-next-line n/no-restricted-require */
     return require('child_process')
       .execSync('git describe --always --abbrev=0 --dirty')
       .toString()
