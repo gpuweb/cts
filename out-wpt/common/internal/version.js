8f906728ac27a7ca5baadc8bef5e2702121e96af
dneto0 <dneto@google.com>
2025-10-17 15:57:15 +0000

    Deploying to gh-pages from @ gpuweb/cts@d8631014b7eae1f74a4084d40a5b5d6030b858f9 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'aabe8e85e5c8938f81cda8223c3efe9e56b7c889';
+export const version = 'd8631014b7eae1f74a4084d40a5b5d6030b858f9';
--- src/webgpu/shader/execution/expression/call/builtin/subgroupMul.spec.ts
+++ src/webgpu/shader/execution/expression/call/builtin/subgroupMul.spec.ts
@@ -330,7 +330,6 @@ g.test('compute,split')
 enable subgroups;
 
 diagnostic(off, subgroup_uniformity);
-diagnostic(off, subgroup_branching);
 
 @group(0) @binding(0)
 var<storage> input : array<u32>;
