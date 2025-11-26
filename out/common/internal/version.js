9c5279f34879cce9024bfdaec7aaf3595f9c4450
petermcneeleychromium <96925679+petermcneeleychromium@users.noreply.github.com>
2025-11-26 18:30:23 +0000

    Deploying to gh-pages from @ gpuweb/cts@a4a4a2bc7dbd2c76ebc1cc4ef77f03a8374ca59b 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '3314e4108692d0c201f0d381bf10ddc8dbbc7b69';
+export const version = 'a4a4a2bc7dbd2c76ebc1cc4ef77f03a8374ca59b';
