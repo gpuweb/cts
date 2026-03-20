69f4a14323a580e96f736b961a94db2b5a47a466
kainino0x <kainino1@gmail.com>
2026-03-20 09:39:07 +0000

    Deploying to gh-pages from @ gpuweb/cts@857b158c374adb87187816f717090c608c89de89 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'e135cc01eb4f93daceb040ad183500444033810a';
+export const version = '857b158c374adb87187816f717090c608c89de89';
